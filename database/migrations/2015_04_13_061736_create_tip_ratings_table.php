009d6dfa7183fe57c86cba58e72f363ee4e731d6
jayson1234 <jsuizo@nerubia.com>
2015-04-13 17:40:34 +0800

    mailservices.php,app/models,database/migrations migration updates

---
+++ database/migrations/2015_04_13_061736_create_tip_ratings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipRatingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tip_ratings', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('tip_id');
+            $table->tinyInteger('rating');
+            $table->TEXT('comments');
+            $table->bigInteger('created_by');
+            $table->bigInteger('updated_by');
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tip_ratings');
+	}
+
+}
---
+++ database/migrations/2015_04_13_062649_create_help_requests_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHelpRequestsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('help_requests', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('class_id');
+            $table->bigInteger('user_id');
+            $table->string('title',128);
+            $table->TEXT('content');
+            $table->bigInteger('module_code');
+            $table->bigInteger('subject_code');
+            $table->bigInteger('area_code');
+            $table->tinyInteger('is_verified');
+            $table->enum('status',['Enabled', 'Disabled']);
+            $table->enum('question_status',['Open', 'Answered', 'Cancelled']);
+            $table->timestamp('last_answered_at');
+            $table->bigInteger('created_by');
+            $table->bigInteger('updated_by');
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('help_requests');
+	}
+
+}
