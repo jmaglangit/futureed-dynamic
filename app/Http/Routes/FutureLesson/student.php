c1cd0679fc36dec45b49b2afd90eca37e5039b49
Jollie Mar <jgutang@nerubia.com>
2015-04-14 16:49:20 +0800

    Added angular component for enter image password

--- app/Http/Routes/FutureLesson/student.php
+++ app/Http/Routes/FutureLesson/student.php
@@ -2,18 +2,18 @@
 	
 	Routes::group(['prefix' => '/student'], function()
 	{
-		Routes::get('/', 'FutureLesson\Student\LoginController@index');
+		Routes::any('/', 'FutureLesson\Student\LoginController@index');
 	
 		Routes::group(['prefix' => '/login'], function()
 		{
-			Routes::get('/', [ 'as' => 'student.login', 'uses' => 'FutureLesson\Student\LoginController@index']);
+			Routes::any('/', [ 'as' => 'student.login', 'uses' => 'FutureLesson\Student\LoginController@index']);
 			Routes::any('/enter-password', [ 'as' => 'student.enter_password', 'uses' => 'FutureLesson\Student\LoginController@enter_password']);
 			Routes::get('/forgot-password', [ 'as' => 'student.login.forgot_password', 'uses' => 'FutureLesson\Student\LoginController@forgot_password']);
 			Routes::get('/forgot-password-success', [ 'as' => 'student.login.forgot_password_success', 'uses' => 'FutureLesson\Student\LoginController@forgot_password_success']);
 			Routes::get('/reset-password', [ 'as' => 'student.login.reset_password', 'uses' => 'FutureLesson\Student\LoginController@reset_password']);
 			Routes::get('/reset-confirm-password', [ 'as' => 'student.login.reset-confirm-password', 'uses' => 'FutureLesson\Student\LoginController@reset_confirm_password']);
 			Routes::get('/reset-password-success', [ 'as' => 'student.login.reset_password_success', 'uses' => 'FutureLesson\Student\LoginController@reset_password_success']);
-			
+			// Routes::get('/dashboard');
 		});
 		
 		Routes::get('/password/reset', [ 'as' => 'student.login.reset_password', 'uses' => 'FutureLesson\Student\LoginController@reset_password']);
--- resources/views/student/login/enter-password.blade.php
+++ resources/views/student/login/enter-password.blade.php
@@ -6,27 +6,45 @@
   <div class="container login">
     <div class="col-md-6 col-md-offset-3">
       <div class="form-style form-select-password">
-        <div class="title">Please Select Your Password</div>
-        <div class="error" ng-if="error">
-          <p>warning style 1</p>
+        <div ng-if="locked">
+          <div class="title">Account Locked</div>
+          <div class="form_content">
+            <div style="width:120px; margin:0 auto 30px;">
+            <i class="fa fa-lock fa-5x img-rounded text-center" style="background:#e8e8e8; border-radius:200px; padding:20px; width:120px;"></i>
+            </div>
+  
+            <p class="h4 text">Your account has been locked due to maximum attempt of invalid login.</p>
+            <small>Please <a href="#">click here</a> to redirect you to the steps to reset your password.</small>
+            <p style="padding-bottom: 40px;">
+              <a href="" class="btn btn-red">Reset Password</a>
+            </p>
+          </div>
         </div>
 
-        <form name="passwordForm">
-          <div class="form_content">
-            <ul class="form_password list-unstyled list-inline">
-              <li class="item" ng-repeat="item in imagePass">
-                 <!-- {!! Html::image('{! item.password_image_file !}', false) !!} -->
-                 <img ng-src="{! item.password_image_file !}" alt="{! item.name !}">
-              </li>
-              <p><button type="button" class="btn btn-red" ng-click="validatePassword()">Submit</button></p>
-            </ul>
+        <div ng-if="!locked">
+          <div class="title">Please Select Your Password</div>
+          <div class="error" ng-if="error">
+            <p>{! error !}</p>
           </div>
-          <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
-        </form>
+
+          <form id="passwordForm" action="/student/dashboard" name="passwordForm" method="POST">
+            <div class="form_content">
+              <ul class="form_password list-unstyled list-inline">
+                <li class="item" ng-repeat="item in imagePass" ng-click="highlight($event)">
+                   <!-- {!! Html::image('{! item.password_image_file !}', false) !!} -->
+                   <img ng-src="{! item.password_image_file !}" alt="{! item.name !}">
+                   <input type="hidden" id="image_id" name="image_id" value="{! item.id !}">
+                </li>
+                <p><button type="button" class="btn btn-red" ng-click="validatePassword()">Submit</button></p>
+              </ul>
+            </div>
+            <input type="hidden" name="id" id="id" value="<?php echo $id; ?>">
+            <input type="hidden" id="response" name="response">
+          </form>
+        </div>
       </div>
     </div>
   </div>
-  
 @stop
 
 @section('footer')
