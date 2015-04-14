f8eab89755de028b328a397b365bf925ae25524c
jayson1234 <jsuizo@nerubia.com>
2015-04-14 11:14:04 +0800

    invoicedetails migration update

--- database/migrations/2015_04_13_074133_create_invoice_details_table.php
+++ database/migrations/2015_04_13_074133_create_invoice_details_table.php
@@ -19,7 +19,7 @@ class CreateInvoiceDetailsTable extends Migration {
             $table->string('class_name',128);
             $table->bigInteger('grade_code');
             $table->smallInteger('seats_total');
-            $table->decimal('sub_total');
+            $table->decimal('sub_total',8,2);
             $table->bigInteger('created_by');
             $table->bigInteger('updated_by');
             $table->timestamps();
