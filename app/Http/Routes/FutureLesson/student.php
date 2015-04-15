54fa5a0889d4a345ef1ae125bdc9d1cc2a02cbfc
Jollie Mar <jgutang@nerubia.com>
2015-04-15 15:02:40 +0800

    for merge update

--- app/Http/Routes/FutureLesson/student.php
+++ app/Http/Routes/FutureLesson/student.php
@@ -10,15 +10,15 @@
 			Routes::get('/', [ 'as' => 'student.login', 'uses' => 'FutureLesson\Student\LoginController@index']);
 			Routes::match(['get','post'],'/enter-password', [ 'as' => 'student.enter_password', 'uses' => 'FutureLesson\Student\LoginController@enter_password']);
 			Routes::get('/forgot-password', [ 'as' => 'student.login.forgot_password', 'uses' => 'FutureLesson\Student\LoginController@forgot_password']);
-			Routes::get('/forgot-password-success', [ 'as' => 'student.login.forgot_password_success', 'uses' => 'FutureLesson\Student\LoginController@forgot_password_success']);
+			Routes::match(['get','post'], '/forgot-password-success', [ 'as' => 'student.login.forgot_password_success', 'uses' => 'FutureLesson\Student\LoginController@forgot_password_success']);
 			Routes::get('/reset-password', [ 'as' => 'student.login.reset_password', 'uses' => 'FutureLesson\Student\LoginController@reset_password']);
-			Routes::get('/reset-confirm-password', [ 'as' => 'student.login.reset-confirm-password', 'uses' => 'FutureLesson\Student\LoginController@reset_confirm_password']);
-			Routes::get('/reset-password-success', [ 'as' => 'student.login.reset_password_success', 'uses' => 'FutureLesson\Student\LoginController@reset_password_success']);
+			Routes::post('/reset-confirm-password', [ 'as' => 'student.login.reset-confirm-password', 'uses' => 'FutureLesson\Student\LoginController@reset_confirm_password']);
+			Routes::match(['get','post'], '/reset-password-success', [ 'as' => 'student.login.reset_password_success', 'uses' => 'FutureLesson\Student\LoginController@reset_password_success']);
 		
 			Routes::post('/process', [ 'as' => 'student.login.process', 'uses' => 'FutureLesson\Student\LoginController@process']);
 		});
 		
-		Routes::get('/password/reset', [ 'as' => 'student.login.reset_password', 'uses' => 'FutureLesson\Student\LoginController@reset_password']);
+		// Routes::get('/password/reset', [ 'as' => 'student.login.reset_password', 'uses' => 'FutureLesson\Student\LoginController@reset_password']);
 		Routes::get('/registration', ['as' => 'student.registration', 'uses' => 'FutureLesson\Student\LoginController@registration']);
 		Routes::get('/registration-success', ['as' => 'student.registration-success', 'uses' => 'FutureLesson\Student\LoginController@registration_success']);
 	});
--- public/js/futureed_controllers.js
+++ public/js/futureed_controllers.js
@@ -5,85 +5,143 @@ var controllers = angular.module('futureed.controllers', []);
         window.location.href = url;
       }
 
-    $scope.validateUser = function(username) {
-      $scope.username = angular.copy(username);
-      $scope.error = "";
+      $scope.validateUser = function(username) {
+        $scope.username = angular.copy(username);
+        $scope.error = "";
 
-      loginAPIService.validateUser($scope.username).success(function (response) {
-          if(response.status == 200) {
-            $scope.id = response.data.id;
-            $("input[name='id']").val($scope.id);
-            $("#login_form").submit();
-          } else {
-            var data = response.data;
-            if(data.error_code == 202) {
-              if(data.message == "Account Locked") {
-                $scope.locked = true;
+        loginAPIService.validateUser($scope.username).success(function (response) {
+            if(response.status == 200) {
+              $scope.id = response.data.id;
+              $("input[name='id']").val($scope.id);
+              $("#login_form").submit();
+            } else {
+              var data = response.data;
+              if(data.error_code == 202) {
+                if(data.message == "Account Locked") {
+                  $scope.locked = true;
+                } else {
+                  $scope.error = data.message;
+                }
               } else {
                 $scope.error = data.message;
               }
-            } else {
-              $scope.error = data.message;
             }
+        }).error(function(response) {
+            if(response.status == 422) {
+              $scope.error = "Username should not be empty."
+            } else {
+              $scope.error = response.data;
+            }
+        });
+      }
+
+      $scope.getImagePassword = function() {
+        // $scope.id = $("input[name='id']").val();
+        $scope.id = "3";
+
+        loginAPIService.getImagePassword($scope.id).success(function (response) {
+          $scope.imagePass = response.data
+        }).error(function(response) {
+
+        });
+      }
+
+      $scope.highlight = function($event) {
+        $("ul.form_password li").removeClass('selected');
+        $($event.currentTarget).addClass('selected');
+        $scope.image_id = $($event.currentTarget).find("#image_id").val();
+      }
+
+      $scope.validatePassword = function () {
+        loginAPIService.validatePassword($scope.id, $scope.image_id).success(function(response) {
+          if(response.status == 200) {
+            $("#response").val(response.data);
+            $("#password_form").submit();
+          } else if(response.status == 202) {
+            if(response.data.message == "Account Locked") {
+              $scope.locked = true;
+            } else {
+              $scope.error = "Password does not match.";
+            } 
           }
-      }).error(function(response) {
-          if(response.status == 422) {
-            $scope.error = "Username should not be empty."
+        }).error(function(response) {
+          
+        });
+      }
+
+      $scope.forgotPassword = function(username) {
+        $scope.error = "";
+        $scope.username = angular.copy(username);
+
+        loginAPIService.forgotPassword($scope.username).success(function(response) {
+          if(response.status == 200) {
+            $("input[name='response']").val(response.data.email);
+            $("#redirect_form").submit();
           } else {
-            $scope.error = response.data;
+            $scope.error = response.data.message;
           }
-      });
-    }
+        }).error(function(response) {
+          $scope.error = response.data.message;
+        });
+      }
 
-    $scope.getImagePassword = function() {
-      $scope.id = $("input[name='id']").val();
-
-      loginAPIService.getImagePassword($scope.id).success(function (response) {
-        $scope.imagePass = response.data
-      }).error(function(response) {
-
-      });
-    }
-
-    $scope.highlight = function($event) {
-      $("ul.form_password li").removeClass('selected');
-      $($event.currentTarget).addClass('selected');
-      $scope.image_id = $($event.currentTarget).find("#image_id").val();
-    }
-
-    $scope.validatePassword = function () {
-      loginAPIService.validatePassword($scope.id, $scope.image_id).success(function(response) {
-        if(response.status == 200) {
-          $("#response").val(response.data);
-          $("#password_form").submit();
-        } else if(response.status == 202) {
-          if(response.data.message == "Account Locked") {
-            $scope.locked = true;
-          } else {
-            $scope.error = "Password does not match.";
-          } 
+      $scope.setDisplay = function(email) {
+        if(email != '') {
+          $scope.title = "Enter Reset Code";
+          return false;
         }
-      }).error(function(response) {
+
+        $scope.title = "Email Sent";
+        return true;
+      }
+
+      $scope.validateCode = function(code) {
+        $scope.error = "";
+        $scope.code = angular.copy(code);
+        $scope.email = $("input[name='email']").val();
+
+        loginAPIService.validateCode($scope.code, $scope.email).success(function(response) {
+          if(response.status == 200) {
+            $("input[name='response']").val(response.data);
+            $("#success_form").submit();
+          } else {
+            $scope.error = response.data.message;
+          }
+        }).error(function(response) {
+          $scope.error = response.data.message;
+        });
+      }
+
+      $scope.storeNewPassword = function() {
+        $("input[name='selected_image_id']").val($scope.image_id);
+        $("#reset_password_form").submit();
+      }
+
+      $scope.updateBirthdate = function(reg, birthday) {
+        $scope.reg.birthday = $("input[name='birthday']").val();
+      }
+
+      $scope.validateRegistration = function(registration, terms) {
+        $scope.error = "";
+        $scope.terms = angular.copy(terms);
+        if($scope.terms) {
+          $scope.registration = angular.copy(registration);
         
-      });
-    }
-
-    $scope.forgotPassword = function(username) {
-      $scope.username = angular.copy(username);
-      loginAPIService.forgotPassword($scope.username).success(function(response) {
-
-      }).error(function(response) {
-
-      });
-    }
-
-    $scope.validateCode = function(code, email) {
-      $scope.code = angular.copy(code);
-      $scope.email = angular.copy(email);
-    }
-
-    $scope.validateRegistration = function(registration) {
-
-    }
+          loginAPIService.validateRegistration($scope.registration).success(function(response) {
+            if(response.status == 200) {
+              if(response.errors) {
+                $scope.error = response.errors.message;
+              }
+            } else {
+              $scope.error = response.data.message;
+            }
+          }).error(function(response) {
+            $scope.error = response.data.message;
+          });
+        } else {
+          $scope.error = "Please accept the terms and conditions.";
+        }
+        
+      }
 });
    
