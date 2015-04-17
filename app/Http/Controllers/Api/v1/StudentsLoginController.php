adc662ae554b976b0a1170709be3f78f0a1c71f5
jasonmag <jasonmaglangit@gmail.com>
2015-04-17 17:22:30 +0800

    fix changed method

--- app/Http/Controllers/Api/v1/StudentsLoginController.php
+++ app/Http/Controllers/Api/v1/StudentsLoginController.php
@@ -26,11 +26,11 @@ class StudentsLoginController extends StudentsController{
             }else{
 //                check if username exist, return id else nothing
                 $response = $this->user->checkLoginName($input['username'], 'Student');
-                $student_id = $this->student->getStudentReferences($response['data']);
+                $student_id = $this->student->getStudentId($response['data']);
 
                 if($response['status']==200){
                  return $this->setStatusCode($response['status'])
-                             ->respondWithData(['id'=>$student_id]);
+                             ->respondWithData(['id'=> $student_id]);
                 }
                 else{
                  return $this->setStatusCode($response['status'])
