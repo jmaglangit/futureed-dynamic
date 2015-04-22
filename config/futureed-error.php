5d8fe73b7f6f81e622ead793aea65b43cdd4ce16
jasonmag <jasonmaglangit@gmail.com>
2015-04-22 19:12:01 +0800

    added error messages

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -3,7 +3,12 @@
 
 return [
     'error_code' => [
-        '1007' => 'Not Found!',
+        //Field error codes
+        1001 => "Required field not found.",
+        1002 => "Required field empty.",
+
+
+        //Rules messages
         '1008' => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
     ],
 ];
