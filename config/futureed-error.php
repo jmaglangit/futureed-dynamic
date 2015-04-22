a03a01c23fadd85f15fb1fd9b31850222b356895
jayson1234 <jsuizo@nerubia.com>
2015-04-22 11:30:22 +0800

    change config

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -4,6 +4,6 @@
 return [
     'error_code' => [
         '1007' => 'Not Found!',
-        '1008' => 'The email you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
+        '1008' => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
     ],
 ];
