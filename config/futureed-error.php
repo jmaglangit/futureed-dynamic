4a5d04c961c203334957e6ad022ca40ce331db7f
jayson1234 <jsuizo@nerubia.com>
2015-04-23 17:01:27 +0800

    update

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -14,5 +14,6 @@ return [
         2008 => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
         
         2100 => 'Reset code invalid',
+        2101 => 'Password image invalid',
     ],
 ];
