4e0a2ba7acf6cd39788cc302ed62ae5053ac9e8f
jasonmag <jasonmaglangit@gmail.com>
2015-04-23 10:42:37 +0800

    update error messages on user/username

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -2,13 +2,14 @@
 
 
 return [
-    'error_code' => [
+    'error_messages' => [
         //Field error codes
         1001 => "Required field not found.",
         1002 => "Required field empty.",
 
 
         //Rules messages
-        '1008' => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
+        2001 => 'User does not exist.',
+        2008 => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
     ],
 ];
