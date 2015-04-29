39541f25a9a2485a881b112fdcb74e0ff539ca8b
Jayson Suizo <jayson@Jaysons-MacBook-Pro-3.local>
2015-04-29 20:09:28 +0800

    future error messages

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -3,10 +3,7 @@
 
 return [
 
-    'error_code' => [
-        '1007' => 'Not Found!',
-        '1008' => 'The email/username you entered is invalid Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
-    ],  'error_messages' => [
+     'error_messages' => [
         //Field error codes
         1001 => "Required field not found.",
         1002 => "Required field empty.",
