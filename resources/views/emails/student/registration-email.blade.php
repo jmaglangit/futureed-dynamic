e7ebc7158ba8b6ec88d9235ceb31422f63420c9c
Jayson Suizo <jayson@Jaysons-MacBook-Pro-4.local>
2015-04-29 11:07:33 +0800

    update for templates

--- resources/views/emails/student/registration-email.blade.php
+++ resources/views/emails/student/registration-email.blade.php
@@ -7,8 +7,10 @@
 <div>Hi there {{ $name }},</div>
 <br>
 <div>
-    <div>Thanks for joining us at Future Lesson and congratulations on becoming part of the coolest community of learners on the web!  We look forward to taking this incredible learning adventure with you and your child.
+    <div>Thanks for joining us at Future Lesson and congratulations on becoming part of the coolest community of learners on the web!  We look forward to taking this incredible learning adventure with you.
 </div>
+	<br>
+	<div>So tell us, who would you like to be when you grow up? A doctor, astronaut, scientist?  Pick your Avatar </div>
     <br>
     <div>Your confirmation code : {{ $code  }}</div>
     <br>
