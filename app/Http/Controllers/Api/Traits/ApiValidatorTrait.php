e90d7aadb6ee017bb47b6a9dffe18aa30bd24f29
jasonmag <jasonmaglangit@gmail.com>
2015-05-04 17:52:04 +0800

    added errorMessageCommon and MessageBagTrait

--- app/Http/Controllers/Api/Traits/ApiValidatorTrait.php
+++ app/Http/Controllers/Api/Traits/ApiValidatorTrait.php
@@ -5,29 +5,30 @@ use Illuminate\Support\Facades\Validator;
 trait ApiValidatorTrait {
 
     use ErrorMessageTrait;
+    use MessageBagTrait;
 
-    public $messageBag = [];
-
-    public function setMessageBag($message){
-        $this->messageBag = $message;
-        return $this;
-    }
-
-    public function getMessageBag(){
-        return $this->messageBag;
-    }
-
-    public function addMessageBag($message){
-
-        if(empty($this->messageBag) && !empty($message)){
-            $this->setMessageBag([$message]);
-        } elseif(!empty($message) ) {
-            $this->messageBag = array_merge(
-                $this->getMessageBag(),
-                [$message]
-            );
-        }
-    }
+//    public $messageBag = [];
+//
+//    public function setMessageBag($message){
+//        $this->messageBag = $message;
+//        return $this;
+//    }
+//
+//    public function getMessageBag(){
+//        return $this->messageBag;
+//    }
+//
+//    public function addMessageBag($message){
+//
+//        if(empty($this->messageBag) && !empty($message)){
+//            $this->setMessageBag([$message]);
+//        } elseif(!empty($message) ) {
+//            $this->messageBag = array_merge(
+//                $this->getMessageBag(),
+//                [$message]
+//            );
+//        }
+//    }
 
     //Check parameters of the fields.
     public function parameterCheck($input, $paramName){
---
+++ app/Http/Controllers/Api/Traits/MessageBagTrait.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 5/4/15
+ * Time: 5:28 PM
+ */
+
+namespace FutureEd\Http\Controllers\Api\Traits;
+
+
+trait MessageBagTrait {
+
+}
