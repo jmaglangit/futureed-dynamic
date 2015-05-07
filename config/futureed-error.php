6c2dd3a629a181f2ad34a9ffe72cd1b00ec9d883
Jayson Suizo <jayson@Jaysons-MacBook-Pro-3.local>
2015-05-07 13:16:53 +0800

    update error

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -11,9 +11,9 @@ return [
 
 
         //Rules messages
-        2001 => 'User does not exist.',
-        2002 =>  'Email does not exist.',
-        2003 => 'Account does not exist.',
+        2001 => 'User does not exists.',
+        2002 =>  'Email does not exists.',
+        2003 => 'Account does not exists.',
 
 
         //for student error message
@@ -22,8 +22,8 @@ return [
         2006 => 'Confirmation code does not match.',
         2007 => 'Confirmation code has expired.',
         2008 => 'The email/username you entered is invalid. Please try inputting again. If the account you are attempting to login is for a student less than 13 yrs old, please login via the parent site',
-        2009 => 'Avatar does not exist',
-        2010 => 'Parent does not exist',
+        2009 => 'Avatar does not exists',
+        2010 => 'Parent does not exists',
         2011 => 'Username and password is incorrect',
         2012 => 'Picture password is incorrect',
         2013 => 'Username or Email is invalid',
@@ -41,20 +41,20 @@ return [
         2101 => 'Invalid password image',
         2102 => 'Access token expired',
         2103 => 'Invalid confirmation code',
-        2104 => 'Username already exist',
-        2105 => "School Code doesn't exist",
+        2104 => 'Username already exists',
+        2105 => "School Code doesn't exists",
         2106 => 'Email does not match',
         2107 => 'Your current email and new email are the same.',
-        2108 => 'New Email does not exist',
+        2108 => 'New Email does not exists',
         2109 => 'Your account has already been confirmed',
         2110 => 'Your new password has already been confirmed',
         2111 => 'Your new email has already been confirmed',
 
 
         // Client Error messages
-        2200 => 'Email already exist',
-        2201 => 'Username already exist',
-        2202 => 'School Name already exist',
+        2200 => 'Email already exists',
+        2201 => 'Username already exists',
+        2202 => 'School Name already exists',
 
         //account status messages
         2230 => 'Account is inactive',
