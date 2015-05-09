be8b1576321c244d0d9807cd1bca27e77f30444e
jasonmag <jasonmaglangit@gmail.com>
2015-05-09 23:05:27 +0800

    added gitignore file

--- app/Http/Routes/Api/Test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//test routes
-
-//token routes to be remove after test
-Routes::get('/code','Api\v1\TokenController@getCode');
-Routes::get('/sendmail','Api\v1\TokenController@sendMail');
-Routes::post('/input','Api\v1\TokenController@input');
