ae0df0aae03bcbcbb9747845d26463785a4a9688
jasonmag <jasonmaglangit@gmail.com>
2015-05-09 22:18:34 +0800

    added Client route file

--- app/Http/Routes/Api/Client.php
+++ app/Http/Routes/Api/Client.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 5/9/15
- * Time: 9:36 PM
- */
+
+//Client
+Routes::resource('/client','Api\v1\ClientController',
+    ['except' => ['create','edit']]);
+
+//Parent
+Routes::resource('/client/parent','Api\v1\ClientParentController',
+    ['except' => ['create','edit']]);
+Routes::get('client/parent/student/list/{id}','Api\v1\ClientParentController@getStudentList');
+
+//Principal
+Routes::resource('/client/principal','Api\v1\ClientPrincipalController',
+    ['except' => ['create','edit']]);
+
+//Teacher
+Routes::resource('/client/teacher','Api\v1\ClientTeacherController',
+    ['except' => ['create','edit']]);
+
+//client
+Routes::post('/client/login','Api\v1\ClientLoginController@login');
+Routes::post('/client/register','Api\v1\ClientRegisterController@register');
+Routes::post('/client/password/{id}','Api\v1\ClientPasswordController@changePassword');
