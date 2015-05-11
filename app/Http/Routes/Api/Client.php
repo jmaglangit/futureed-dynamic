0218e9cf2a832cae37c6d8c7815010880821f267
jasonmag <jasonmaglangit@gmail.com>
2015-05-11 10:54:16 +0800

    update Client route

--- app/Http/Routes/Api/Client.php
+++ app/Http/Routes/Api/Client.php
@@ -1,23 +1,38 @@
 <?php
 
+
+Routes::group(['prefix' => '/client'], function()
+{
+
+    //Parent
+    Routes::resource('/parent','Api\v1\ClientParentController',
+        ['except' => ['create','edit']]);
+    Routes::get('/parent/student/list/{id}','Api\v1\ClientParentController@getStudentList');
+
+    //Principal
+    Routes::resource('/principal','Api\v1\ClientPrincipalController',
+        ['except' => ['create','edit']]);
+
+    //Teacher
+    Routes::resource('/teacher','Api\v1\ClientTeacherController',
+        ['except' => ['create','edit']]);
+
+    //client
+    Routes::post('/login','Api\v1\ClientLoginController@login');
+    Routes::post('/register','Api\v1\ClientRegisterController@register');
+    Routes::post('/password/{id}','Api\v1\ClientPasswordController@changePassword');
+
+
+
+});
+
+
 //Client
 Routes::resource('/client','Api\v1\ClientController',
     ['except' => ['create','edit']]);
 
-//Parent
-Routes::resource('/client/parent','Api\v1\ClientParentController',
-    ['except' => ['create','edit']]);
-Routes::get('client/parent/student/list/{id}','Api\v1\ClientParentController@getStudentList');
 
-//Principal
-Routes::resource('/client/principal','Api\v1\ClientPrincipalController',
-    ['except' => ['create','edit']]);
 
-//Teacher
-Routes::resource('/client/teacher','Api\v1\ClientTeacherController',
-    ['except' => ['create','edit']]);
 
-//client
-Routes::post('/client/login','Api\v1\ClientLoginController@login');
-Routes::post('/client/register','Api\v1\ClientRegisterController@register');
-Routes::post('/client/password/{id}','Api\v1\ClientPasswordController@changePassword');
+
+
