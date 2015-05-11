5acf3663cbb3d37335577121e7ffa016ea2e9ec8
Jayson Suizo <jayson@Jaysons-MacBook-Pro-3.local>
2015-05-11 15:58:29 +0800

    update for change password api

--- app/Models/Repository/Admin/AdminRepository.php
+++ app/Models/Repository/Admin/AdminRepository.php
@@ -40,7 +40,7 @@ class AdminRepository implements  AdminRepositoryInterface {
 
     public function verifyAdminId($id){
 
-        return Admin::where('id','=',$id)->pluck('id');
+        return Admin::select('id','user_id')->where('id','=',$id)->first();
 
     }
 
