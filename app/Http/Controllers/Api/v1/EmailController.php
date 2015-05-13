77d53042cb26e988e6f97642bf921c55ab5b5160
Jayson Suizo <jayson@Jaysons-MacBook-Pro-2.local>
2015-05-13 10:42:01 +0800

    update for callback_uri

--- app/Http/Controllers/Api/v1/EmailController.php
+++ app/Http/Controllers/Api/v1/EmailController.php
@@ -48,12 +48,12 @@ class EmailController extends ApiController {
 
         $userType = config('futureed.student');
 
-        $input = Input::only('new_email','password_image_id','url');
+        $input = Input::only('new_email','password_image_id','callback_uri');
 
         $this->addMessageBag($this->validateVarNumber($id));
         $this->addMessageBag($this->email($input,'new_email')); 
         $this->addMessageBag($this->validateNumber($input,'password_image_id'));
-        $this->addMessageBag($this->validateString($input,'url'));
+        $this->addMessageBag($this->validateString($input,'callback_uri'));
 
         $msg_bag = $this->getMessageBag();
 
@@ -91,7 +91,7 @@ class EmailController extends ApiController {
                     $this->user->addNewEmail($studentReferences['user_id'],$input['new_email']);
 
                     $new_user_details = $this->user->getUserDetail($studentReferences['user_id'],$userType);
-                    $this->mail->sendMailChangeEmail($new_user_details,$code['confirmation_code'],$input['url'],0);
+                    $this->mail->sendMailChangeEmail($new_user_details,$code['confirmation_code'],$input['callback_uri'],0);
                     $this->user->updateEmailCode($studentReferences['user_id'],$code);
                     
                     return $this->respondWithData(['id' => $id]);
@@ -221,12 +221,12 @@ class EmailController extends ApiController {
 
         $userType = config('futureed.client');
 
-        $input = Input::only('new_email','password','url');
+        $input = Input::only('new_email','password','callback_uri');
 
         $this->addMessageBag($this->validateVarNumber($id));
         $this->addMessageBag($this->email($input,'new_email')); 
         $this->addMessageBag($this->checkPassword($input,'password'));
-        $this->addMessageBag($this->validateString($input,'url'));
+        $this->addMessageBag($this->validateString($input,'callback_uri'));
 
         $msg_bag = $this->getMessageBag();
 
@@ -263,7 +263,7 @@ class EmailController extends ApiController {
                     $this->user->addNewEmail($idExist['user_id'],$input['new_email']);
 
                     $new_user_details = $this->user->getUserDetail($idExist['user_id'],$userType);
-                    $this->mail->sendMailChangeEmail($new_user_details,$code['confirmation_code'],$input['url'],0);
+                    $this->mail->sendMailChangeEmail($new_user_details,$code['confirmation_code'],$input['callback_uri'],0);
                     $this->user->updateEmailCode($studentReferences['user_id'],$code);
                     
                     return $this->respondWithData(['id' => $id]);
