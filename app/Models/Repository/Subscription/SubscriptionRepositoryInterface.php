b3fcbf52716f129a27230627834863d852662fbc
Aaron <aaugusto@nerubia.com>
2015-05-18 11:53:04 +0800

    Subsription CRUD

--- app/Models/Repository/Subscription/SubscriptionRepositoryInterface.php
+++ app/Models/Repository/Subscription/SubscriptionRepositoryInterface.php
@@ -6,4 +6,6 @@ interface SubscriptionRepositoryInterface {
     public function getSubscriptions($criteria = array(), $limit = 0, $offset = 0);
     public function getSubscription($id);
     public function updateSubscription($subscription);
+    public function addSubscription($subscription);
+    public function deleteSubscription($id);
 }
