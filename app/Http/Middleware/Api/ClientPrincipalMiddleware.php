39ad779fcd9e4f7c3a4e22265bde3f1c0431d1d8
jasonmag <jasonmaglangit@gmail.com>
2015-05-19 15:20:19 +0800

    added ClientPrincipalMiddleware

---
+++ app/Http/Middleware/Api/ClientPrincipalMiddleware.php
@@ -0,0 +1,19 @@
+<?php namespace FutureEd\Http\Middleware\Api;
+
+use Closure;
+
+class ClientPrincipalMiddleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		return $next($request);
+	}
+
+}
