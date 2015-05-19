639c406a7b547aa927bec66bbb9e0225945c85c5
Jayson Suizo <jayson@Jaysons-MacBook-Pro-2.local>
2015-05-19 10:49:58 +0800

    for verify client and reject client

---
+++ resources/views/emails/client/verify-client.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>Hi there {{ $name  }}</h2>
+
+<div>
+	<div>Your Future Lesson account is now active.  <br/><br/>Please go to <a href = "{{$link}}">Login Page</a> and login to begin your adventure on the Future Lesson platform.
+</div>
+    
+</div>
+</body>
+</html>
