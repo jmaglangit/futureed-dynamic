c8a09c0d9beee7e6fb2ba8164a4c5fb13159f8c0
jasonmag <jasonmaglangit@gmail.com>
2015-05-19 15:28:20 +0800

    updated JWTMiddleware

--- app/Http/Middleware/Api/JWTMiddleware.php
+++ app/Http/Middleware/Api/JWTMiddleware.php
@@ -1,19 +1,19 @@
-<?php namespace FutureEd\Http\Middleware;
+<?php namespace FutureEd\Http\Middleware\Api;
 
 use Closure;
-use FutureEd\Http\Controllers\Api\Traits\AccessTokenTrait;
 use FutureEd\Http\Controllers\Api\Traits\ErrorMessageTrait;
 use FutureEd\Http\Controllers\Api\Traits\MessageBagTrait;
 use FutureEd\Models\Repository\User\UserRepositoryInterface;
 use FutureEd\Services\TokenServices;
 use Illuminate\Contracts\Routing\Middleware;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class JWTMiddleware implements Middleware{
 
     use MessageBagTrait;
     use ErrorMessageTrait;
-    use AccessTokenTrait;
+
     /**
      * Token implementation
      *
@@ -22,14 +22,14 @@ class JWTMiddleware implements Middleware{
     protected $token;
 
     /**
-     * Create new token instance
-     *
      * @param TokenServices $tokenServices
+     * @param UserRepositoryInterface $userRepositoryInterface
      */
     public function __construct (TokenServices $tokenServices, UserRepositoryInterface $userRepositoryInterface){
 
         $this->token = $tokenServices;
         $this->user = $userRepositoryInterface;
+
     }
 
 	/**
@@ -41,8 +41,6 @@ class JWTMiddleware implements Middleware{
 	 */
 	public function handle($request, Closure $next)
 	{
-
-
         //get token if validated.
         $authorization = $request->header('authorization');
 
@@ -54,10 +52,76 @@ class JWTMiddleware implements Middleware{
 
            return $this->respondWithError($this->getMessageBag());
         }
-
+        dd($this->getPayload());
         return $next($request);
 
 	}
+
+    /**
+     * get token
+     * @return mixed
+     */
+    public function getToken(){
+
+        return $this->token->getToken();
+    }
+
+    public function getPayload(){
+
+        return $this->token->getPayload();
+    }
+
+
+    /**
+     * validate token
+     *
+     * @param $token
+     * @return int
+     */
+    public function validateToken($token){
+
+        $error_msg = config('futureed-error.error_messages');
+
+        $validator = Validator::make(
+            [
+                'authorization' => $token
+            ],
+            [
+                'authorization' => 'required'
+            ],
+            [
+                'required' => $error_msg[2030]
+            ]
+        );
+
+        if($validator->fails()){
+
+            $validator_msg = $validator->messages()->toArray();
+
+            $return = $this->setErrorCode(2030)
+                ->setField('authorization')
+                ->setMessage($validator_msg["authorization"][0])
+//                ->setMessage($error_msg[2030])
+                ->errorMessage();
+
+            $this->addMessageBag($return);
+
+            return 0;
+        }
+
+        $token_result = $this->token->validateToken($token);
+
+        if(!$token_result){
+
+            $return = $this->setErrorCode(2029)
+                ->setField('authorization')
+                ->setMessage($error_msg[2029])
+                ->errorMessage();
+
+            $this->addMessageBag($return);
+        }
+
+    }
 
 
     /**
@@ -86,6 +150,29 @@ class JWTMiddleware implements Middleware{
                 'errors' => $message
             ]
         );
+
+    }
+
+    /**
+     * return error message based on the error_code
+     *
+     * @param $error_code
+     * @return JWTMiddleware
+     */
+    public function respondErrorMessage($error_code){
+        $error_msg  = config('futureed-error.error_messages');
+
+        if(!is_null($error_code)){
+
+            $return = $this->setErrorCode($error_code)
+                ->setMessage($error_msg[$error_code])
+                ->errorMessageCommon();
+
+
+            $this->addMessageBag($return);
+
+            return $this->respondWithError($this->getMessageBag());
+        }
 
     }
 
