36e96ed0fc3af0dbea85ffeb3daa9310f42b8375
jasonmag <jasonmaglangit@gmail.com>
2015-05-19 14:51:16 +0800

    added ClientParentMiddleware

---
+++ app/Http/Middleware/Api/ClientParentMiddleware.php
@@ -0,0 +1,19 @@
+<?php namespace FutureEd\Http\Middleware\Api;
+
+use Closure;
+
+class ClientParentMiddleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		return $next($request);
+	}
+
+}
