99a18ec350900cefb873e45cc87f719295eaf83a
Jayson Suizo <jayson@Jaysons-MacBook-Pro-3.local>
2015-05-20 10:32:41 +0800

    minor change for admin login,and change in table mingration,seeders Grades

--- app/Models/Repository/Admin/AdminRepository.php
+++ app/Models/Repository/Admin/AdminRepository.php
@@ -68,7 +68,7 @@ class AdminRepository implements  AdminRepositoryInterface {
 
     public function verifyAdminId($id){
 
-        return Admin::select('id','user_id')->where('id','=',$id)->get();
+        return Admin::select('id','user_id')->where('id','=',$id)->first();
 
     }
 
