248b0b69970d11e3f61b58f4f6b1542e657a3867
jasonmag <jasonmaglangit@gmail.com>
2015-05-20 09:46:15 +0800

    updated usermiddleware

--- app/Http/Middleware/Api/UserMiddleware.php
+++ app/Http/Middleware/Api/UserMiddleware.php
@@ -9,10 +9,12 @@ class UserMiddleware extends JWTMiddleware{
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Closure  $next
+     * @param  array  $user_type
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next)
 	{
+        return $next($request);
 
         $authorization  = $request->header('authorization');
 
@@ -23,7 +25,31 @@ class UserMiddleware extends JWTMiddleware{
             return $this->respondWithError($this->getMessageBag());
         }
 
-		return $next($request);
+        //initialize futureed config
+        $futureed = config('futureed');
+
+        //get permissions
+        $action = $request->route()->getAction();
+
+        //get payload
+        $payload = $this->getPayload();
+
+        // check if payload type exist in permissions else respond unauthorized.
+        if(!in_array(strtolower($payload['type']),$action['permission'],true)){
+
+            return $this->respondErrorMessage(2032);
+        }
+
+        //get role
+       // if payload type is client get role
+        if($payload['type'] == $futureed['client'] && in_array(strtolower($payload['role']),$action['role'])){
+
+            dd('imba');
+        }
+        // compare payload role if exist in route role else respond unauthorized.
+
+
+
 	}
 
 }
