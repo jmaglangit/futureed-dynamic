54cd32aaed4dcf51baf29b9ab91bfd741a8fcd16
jasonmag <jasonmaglangit@gmail.com>
2015-05-20 09:46:55 +0800

    updated student routes

--- app/Http/Routes/Api/Student.php
+++ app/Http/Routes/Api/Student.php
@@ -4,18 +4,14 @@ Routes::group(['prefix' => '/student'], function()
 {
 
     //authenticated student routes
-    Routes::group(['middleware' => 'api_student'],function(){
+    Routes::group(['middleware' => 'api_user'],function(){
 
         //student password
         Routes::get('/password/image','Api\v1\StudentPasswordController@getPasswordImages');
         Routes::post('/password/reset','Api\v1\StudentPasswordController@passwordReset');
         Routes::post('/password/code','Api\v1\StudentPasswordController@confirmResetCode');
-        Routes::post('/password/new','Api\v1\StudentPasswordController@confirmNewImagePassword');
         Routes::post('/password/{id}','Api\v1\StudentPasswordController@changeImagePassword');
         Routes::post('/password/confirm/{id}','Api\v1\StudentPasswordController@confirmPassword');
-
-        //student registration
-        Routes::post('/invite','Api\v1\StudentRegistrationController@invite');
 
         //email
         Routes::put('/email/{id}','Api\v1\EmailController@updateStudentEmail');
@@ -23,13 +19,21 @@ Routes::group(['prefix' => '/student'], function()
 
     });
 
+
+
     //student login
     Routes::post('/login/username','Api\v1\StudentLoginController@login');
     Routes::post('/login/image','Api\v1\StudentLoginController@imagePassword');
     Routes::post('/login/password','Api\v1\StudentLoginController@loginPassword');
 
+    //student password
+    Routes::post('/password/new','Api\v1\StudentPasswordController@confirmNewImagePassword');
+
     //student registration
     Routes::post('/register','Api\v1\StudentRegistrationController@register');
+
+    //student registration
+    Routes::post('/invite','Api\v1\StudentRegistrationController@invite');
 
     //email
     Routes::post('/resend/email/{id}','Api\v1\EmailController@resendChangeEmail');
@@ -38,7 +42,7 @@ Routes::group(['prefix' => '/student'], function()
 
 
 //authenticated students
-Routes::group(['middleware' => 'api_student'],function(){
+Routes::group(['middleware' => 'api_user','permission' => ['admin','user','student']],function(){
 
     Routes::resource('/student', 'Api\v1\StudentController',
         ['except' => ['create', 'edit']]);
