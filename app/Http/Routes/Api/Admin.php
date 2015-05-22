0f168403cba258f9ff0abb246ad77372269daa2e
jasonmag <jasonmaglangit@gmail.com>
2015-05-22 16:57:04 +0800

    updated admin route

--- app/Http/Routes/Api/Admin.php
+++ app/Http/Routes/Api/Admin.php
@@ -1,13 +1,42 @@
 <?php 
 
-Routes::group(['middleware' => 'api_user','prefix' => '/admin'], function()
+Routes::group(['prefix' => '/admin'], function()
 {
 
-	Routes::post('/login','Api\v1\AdminLoginController@login');
-	Routes::post('/password/{id}','Api\v1\AdminPasswordController@changePassword');
+	Routes::post('/login',[
+        'uses' => 'Api\v1\AdminLoginController@login',
+        'as' => 'api.v1.admin.login'
+    ]);
+
+    /**
+     * Authenticated admin routes
+     */
+    Routes::group([
+        'middleware' => ['api_user','api_after'],
+        'permission' => ['admin'],
+        'role' => ['admin','super_admin']
+    ],function(){
+
+        Routes::post('/password/{id}',[
+            'uses' => 'Api\v1\AdminPasswordController@changePassword',
+            'as' => 'api.v1.admin.password'
+        ]);
+    });
+
 
 
 });
 
-Routes::resource('/admin','Api\v1\AdminController',
-    ['except' => ['create', 'edit']]);
+
+/**
+ * Admin resource
+ */
+Routes::group([
+    'middleware' => ['api_user','api_after'],
+    'permission' => ['admin'],
+    'role' => ['admin','super_admin']
+],function(){
+
+    Routes::resource('/admin','Api\v1\AdminController',
+        ['except' => ['create', 'edit']]);
+});
