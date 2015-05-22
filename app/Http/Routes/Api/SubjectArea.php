5acd4a32afce2b9f0a860c8775c7da1436f54cfa
jasonmag <jasonmaglangit@gmail.com>
2015-05-22 16:58:28 +0800

    updated SubjectArea route

--- app/Http/Routes/Api/SubjectArea.php
+++ app/Http/Routes/Api/SubjectArea.php
@@ -1,4 +1,15 @@
 <?php
 
-//Subjects
-Routes::resource('/subject-area','Api\v1\SubjectAreaController', ['except' => ['create','edit']]);
+/**
+ * Subject area resource
+ */
+Routes::group([
+    'middleware' => ['api_user','api_after'],
+    'permission' => ['admin','client','student'],
+    'role' => ['principal','teacher','parent','admin','super_admin']
+],function(){
+
+    Routes::resource('/subject-area','Api\v1\SubjectAreaController',
+        ['except' => ['create','edit']]);
+
+});
