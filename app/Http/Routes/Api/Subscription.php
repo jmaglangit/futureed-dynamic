07cec037d58e6671fa5790d9e3136e4204ddabc2
jasonmag <jasonmaglangit@gmail.com>
2015-05-22 16:59:09 +0800

    updated subscription route

--- app/Http/Routes/Api/Subscription.php
+++ app/Http/Routes/Api/Subscription.php
@@ -1,2 +1,14 @@
-<?php 
-Routes::resource('/subscription','Api\v1\SubscriptionController', ['except' => ['create','edit']]);
+<?php
+
+/**
+ * Subscription resource
+ */
+Routes::group([
+    'middleware' => ['api_user','api_after'],
+    'permission' => ['admin','client','student'],
+    'role' => ['principal','teacher','parent','admin','super_admin']
+],function(){
+
+    Routes::resource('/subscription','Api\v1\SubscriptionController',
+        ['except' => ['create','edit']]);
+});
