39c71602b9c28e39abf845bdfc53ef320da98998
jasonmag <jasonmaglangit@gmail.com>
2015-05-22 16:57:30 +0800

    updated ClientDiscount route

--- app/Http/Routes/Api/ClientDiscount.php
+++ app/Http/Routes/Api/ClientDiscount.php
@@ -1,2 +1,14 @@
-<?php 
-Routes::resource('/client-discount','Api\v1\ClientDiscountController', ['except' => ['edit']]);
+<?php
+
+/**
+ * Client discount resource.
+ */
+Routes::group([
+    'middleware' => ['api_user','api_after'],
+    'permission' => ['admin','client','student'],
+    'role' => ['principal','teacher','parent','admin','super_admin']
+],function(){
+
+    Routes::resource('/client-discount','Api\v1\ClientDiscountController',
+        ['except' => ['edit']]);
+});
