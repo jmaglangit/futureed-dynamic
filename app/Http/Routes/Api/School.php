913dc2a2d1dccb072363a1a10d45a5062ce04e28
jasonmag <jasonmaglangit@gmail.com>
2015-05-22 16:57:47 +0800

    updated school route

--- app/Http/Routes/Api/School.php
+++ app/Http/Routes/Api/School.php
@@ -1,14 +1,29 @@
 <?php
 
+Routes::group(['prefix' => '/school'], function()
+{
+
+    /**
+     * Authenticated school routes
+     */
+    Routes::group([
+        'middleware' => ['api_user','api_after'],
+        'permission' => ['admin','client','student'],
+        'role' => ['principal','teacher','parent','admin','super_admin']
+    ],function(){
+
+        /**
+         * School search
+         */
+        Routes::Post('/search',[
+            'uses' => 'Api\v1\SchoolSearchController@schoolSearch',
+            'as' => 'api.v1.school.search'
+        ]);
+
+    });
+
+});
+
 //schools
 Routes::resource('/school','Api\v1\SchoolController',
     ['except' => ['create','edit']]);
-
-
-Routes::group(['prefix' => '/school'], function()
-{
-//search school
-Routes::Post('/search','Api\v1\SchoolSearchController@schoolSearch');
-
-
-});
