75e3bbb1f833fd555e4bdf717d47627dde36a173
jasonmag <jasonmaglangit@gmail.com>
2015-05-26 22:30:48 +0800

    added classroom repository

--- app/Models/Repository/Classroom/ClassroomRepository.php
+++ app/Models/Repository/Classroom/ClassroomRepository.php
@@ -9,6 +9,58 @@
 namespace FutureEd\Models\Repository\Classroom;
 
 
-class ClassroomRepository {
+use FutureEd\Models\Core\Classroom;
+
+class ClassroomRepository implements ClassroomRepositoryInterface{
+
+    public function getClassrooms($criteria,$limit,$offset){
+
+        if(isset($criteria['name'])){
+
+            $classroom = Classroom::with('client','grade','order')->name($criteria['name']);
+        } else {
+
+            $classroom =  Classroom::with('client','grade','order');
+        }
+
+        $count = $classroom->count();
+
+        if($offset > 0 && $limit > 0){
+            $classroom = $classroom->skip($offset)->take($limit);
+        }
+
+        Return [
+            'total' => $count,
+            'record' => $classroom->get()
+        ];
+    }
+
+    public function getClassroom($id){
+
+        return Classroom::with('order','grade','client')->find($id);
+    }
+
+    public function addClassroom($data){
+
+        try{
+
+//            Classroom::create();
+
+        }catch(Exception $e){
+
+            return $e->getMessage();
+        }
+
+        return true;
+    }
+
+    public function updateClassroom($id){
+
+    }
+
+    public function deleteClassroom($id){
+
+    }
+
 
 }
