e40834f6aa0fd114c01205dcc4601fd15da8c2a1
jasonmag <jasonmaglangit@gmail.com>
2015-05-26 22:26:54 +0800

    added new route files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,5 +33,6 @@ Routes::group(['prefix' => 'api/v1'], function()
         include('Routes/Api/Subscription.php');
         include('Routes/Api/User.php');
         include('Routes/Api/VolumeDiscount.php');
+        include('Routes/Api/Classroom.php');
 
 });
