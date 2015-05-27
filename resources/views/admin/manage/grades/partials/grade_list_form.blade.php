3f9d42d43deee89d92a3625dbfc391081c239ced
Jollie Mar Gutang <mar@Nerubias-MacBook-Air.local>
2015-05-27 14:56:03 +0800

    [Admin] Fix ticket# 406

--- resources/views/admin/manage/grades/partials/grade_list_form.blade.php
+++ resources/views/admin/manage/grades/partials/grade_list_form.blade.php
@@ -5,30 +5,19 @@
 		</div>
 	</div>
 
-	<div class="form-content col-xs-12">
-		<div class="alert alert-success" ng-if="grade.delete.success">
+	<div class="form-content col-xs-12" ng-if="grade.delete.success">
+		<div class="alert alert-success">
 	    	<p>Successfully deleted the selected grade.</p>
 	    </div>
-
-	    <div class="col-xs-3" style="padding:0;">
-			<div class="btn btn-gold" ng-click="grade.setManageGradeActive('add_grade')">
-				<div class="row">
-					<i class="fa fa-plus-square"></i>
-				</div>
-				<div class="row">
-					Add Grade
-				</div>
-			</div>
-		</div>
 	</div>
 
-	<div class="col-xs-12">
+	<div class="col-xs-12 padding-0-30">
 		<div class="title-mid">
 			Search
 		</div>
 	</div>
 
-	<div class="col-xs-12">
+	<div class="col-xs-12 search-container">
 		<div class="form-search">
 			{!! Form::open(
 				array('id' => 'search_form'
@@ -72,7 +61,11 @@
 		</div>
 	</div>
 
-	<div class="col-xs-12">
+	<button class="btn btn-blue btn-small margin-0-30" ng-click="grade.setManageGradeActive('add_grade')">
+		<i class="fa fa-plus-square"></i> Add 
+	</button>
+
+	<div class="col-xs-12 padding-0-30">
 		<div class="title-mid">
 			Grade List
 		</div>
