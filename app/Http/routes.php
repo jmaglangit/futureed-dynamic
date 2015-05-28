9631c731e76f8d01209b8292a46120772079a27e
jasonmag <jasonmaglangit@gmail.com>
2015-05-28 11:44:51 +0800

    Revert "Beta hotfix 410"

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,7 +19,7 @@ include('Routes/FutureLesson/Futurelesson.php');
 Routes::group(['prefix' => 'api/v1'], function()
 {
     Routes::get('/','Api\v1\ApiController@index');
-       
+
 		include('Routes/Api/Admin.php');
 		include('Routes/Api/Announcement.php');
         include('Routes/Api/Client.php');
