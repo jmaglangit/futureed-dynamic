c9e7799ec48c1935f968925e4e25771db4f76ac2
jasonmag <jasonmaglangit@gmail.com>
2015-05-29 13:41:24 +0800

    added new class students repository

---
+++ app/Models/Repository/ClassStudents/ClassStudentsRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 5/29/15
+ * Time: 11:40 AM
+ */
+
+namespace FutureEd\Models\Repository\ClassStudents;
+
+
+class ClassStudentsRepository {
+
+}
---
+++ app/Models/Repository/ClassStudents/ClassStudentsRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 5/29/15
+ * Time: 11:39 AM
+ */
+
+interface ClassStudentsRepositoryInterface {
+
+}
