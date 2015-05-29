4fd77787e3cfacf93cc84c450110c99a433ff314
jasonmag <jasonmaglangit@gmail.com>
2015-05-29 13:43:00 +0800

    added new methods

--- app/Models/Repository/ClassStudents/ClassStudentsRepository.php
+++ app/Models/Repository/ClassStudents/ClassStudentsRepository.php
@@ -9,6 +9,15 @@
 namespace FutureEd\Models\Repository\ClassStudents;
 
 
-class ClassStudentsRepository {
+use FutureEd\Models\Core\ClassStudent;
+use FutureEd\Models\Repository\Classroom\ClassroomRepositoryInterface;
+
+class ClassStudentsRepository implements ClassStudentsRepositoryInterface{
+
+    public function getClassroomStudents($class_id){
+
+        return ClassStudent::with('user')->Classroom($class_id)->get();
+    }
+
 
 }
