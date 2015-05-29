6b91c6a19e13c5e402d236c7f35ff500b3588075
jasonmag <jasonmaglangit@gmail.com>
2015-05-29 10:04:25 +0800

    updated error message on lock

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -48,7 +48,7 @@ return [
         2032 => 'Unauthorized access.',
         2033 => 'Username/Email or password is invalid',
         2034 => 'Username/Email or password is invalid. Have you verified your email? Please check your inbox. Your account may have been locked, please click on forgot password to reset your password.',
-        2035 => 'Please click on forgot password to reset your password.',
+        2035 => 'Account has been locked. Please click on forgot password to reset your password.',
 
         2100 => 'Reset code is invalid.',
         2101 => 'Password image is invalid.',
