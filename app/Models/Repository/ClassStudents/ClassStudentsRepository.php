853eb9fe251d543806582fe271b9b000e5f73351
jasonmag <jasonmaglangit@gmail.com>
2015-06-03 16:43:26 +0800

    updated with search parameters

--- app/Models/Repository/ClassStudents/ClassStudentsRepository.php
+++ app/Models/Repository/ClassStudents/ClassStudentsRepository.php
@@ -14,9 +14,34 @@ use FutureEd\Models\Repository\Classroom\ClassroomRepositoryInterface;
 
 class ClassStudentsRepository implements ClassStudentsRepositoryInterface{
 
-    public function getClassroomStudents($class_id){
+    public function getClassroomStudents($class_id,$category,$offset,$limit){
 
-        return ClassStudent::with('user')->Classroom($class_id)->get();
+        $class_student = new ClassStudent();
+
+        $class_student = $class_student->with('user');
+
+        if(isset($category['username'])){
+
+            $class_student = $class_student->username($category['username']);
+        }
+
+        if(isset($category['email'])){
+
+            $class_student = $class_student->email($category['email']);
+        }
+
+        if($offset > 0 && $limit > 0 ){
+
+            $class_student = $class_student->with('user')->skip($offset)->take($limit);
+        }
+
+        $records = $class_student->get();
+        $count = $class_student->get()->count();
+
+        return [
+            'total' => $count,
+            'record' => $records
+        ];
     }
 
 
