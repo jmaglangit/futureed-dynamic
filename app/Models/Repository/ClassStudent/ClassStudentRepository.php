2cae53c4702bb7ac86c9d2d2426b81814440c8f8
jasonmag <jasonmaglangit@gmail.com>
2015-06-04 18:36:37 +0800

    added relationship model with student and user

--- app/Models/Repository/ClassStudent/ClassStudentRepository.php
+++ app/Models/Repository/ClassStudent/ClassStudentRepository.php
@@ -18,8 +18,8 @@ class ClassStudentRepository implements ClassStudentRepositoryInterface{
 
         $class_student = new ClassStudent();
 
-        $class_student = $class_student->with('user');
-
+        $class_student = $class_student->with('student');
+//        dd($class_student->get()->toArray());
         if(isset($criteria['class_id'])){
 
             $class_student = $class_student->classroom($criteria['class_id']);
