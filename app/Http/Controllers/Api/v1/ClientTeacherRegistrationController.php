86273e598379951c3a8684cd08376e9de12118af
jasonmag <jasonmaglangit@gmail.com>
2015-06-08 15:20:48 +0800

    added new controller for teacher registration

--- app/Http/Controllers/Api/v1/ClientTeacherRegistrationController.php
+++ app/Http/Controllers/Api/v1/ClientTeacherRegistrationController.php
@@ -3,13 +3,30 @@
 use FutureEd\Http\Requests;
 use FutureEd\Http\Controllers\Controller;
 
+use FutureEd\Models\Repository\Client\ClientRepositoryInterface;
 use Illuminate\Http\Request;
+use FutureEd\Http\Requests\Api\ClientTeacherRegistrationRequest;
 
 class ClientTeacherRegistrationController extends Controller {
 
+	/**
+	 * @var ClientRepositoryInterface
+	 */
+	protected $client;
 
-	public function byInvitation(){
+	/**
+	 * @param ClientRepositoryInterface $clientRepositoryInterface
+	 */
+	public function __construct(ClientRepositoryInterface $clientRepositoryInterface){
 
+		$this->client = $clientRepositoryInterface;
+
+	}
+
+	public function getTeacherInformation($id, ClientTeacherRegistrationRequest $request){
+
+		//return  teacher information
+		return $this->client->getTeacher($id,config('futureed.teacher'));
 
 	}
 
