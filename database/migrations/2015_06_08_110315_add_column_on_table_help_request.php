6aee76b83a8d1f167afce21766b1b4230fd557da
jayson1234 <jsuizo@nerubia.com>
2015-06-08 19:23:33 +0800

    ticket-536-update-table-help_request

---
+++ database/migrations/2015_06_08_110315_add_column_on_table_help_request.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnOnTableHelpRequest extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('help_requests', function(Blueprint $table) {
+
+			$table->bigInteger('module_id')->after('content');
+			$table->bigInteger('subject_id')->after('module_id');
+			$table->bigInteger('area_id')->after('subject_id');
+
+			$table->dropColumn('module_code','subject_code','area_code');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('help_requests', function(Blueprint $table) {
+
+			$table->dropColumn('module_id','subject_id','area_id');
+			$table->bigInteger('module_code');
+			$table->bigInteger('subject_code');
+			$table->bigInteger('area_code');
+
+		});
+
+	}
+
+}
