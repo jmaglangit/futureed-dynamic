c29ad0a7f4a9af38e40b04090d2a17a67b40906a
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-06-10 13:14:58 +0800

    KH#425 [Admin/Invoice] Partial commit for view invoice details

--- public/js/filters/date_filter.js
+++ public/js/filters/date_filter.js
@@ -1,17 +1,20 @@
 angular.module('futureed').filter('ddMMyyyy', function($filter) {
 	return function(input) {
 		var _date = "NA";
-		var datetime_parts = input.split(" ");
+		
+		if(angular.isDefined(input)) {
+			var datetime_parts = input.split(" ");
 
-		if(angular.isArray(datetime_parts)) {
-			var date_parts = datetime_parts[0].split("-");
+			if(angular.isArray(datetime_parts)) {
+				var date_parts = datetime_parts[0].split("-");
 
-			if(date_parts[0] == '0000' || date_parts[1] == '00' || date_parts[2] == '00') {
-				return _date;
-			}
+				if(date_parts[0] == '0000' || date_parts[1] == '00' || date_parts[2] == '00') {
+					return _date;
+				}
 
-			_date = date_parts[2] + "/" + date_parts[1] + "/" + date_parts[0];
-		} 
+				_date = date_parts[2] + "/" + date_parts[1] + "/" + date_parts[0];
+			} 
+		}
 
 		return _date;
 	};
