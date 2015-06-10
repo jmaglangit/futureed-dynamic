215a1ef316309ca17ff096f82a11ed71bc4d4673
jasonmag <jasonmaglangit@gmail.com>
2015-06-10 17:45:04 +0800

    updated grade seeder

--- database/seeds/GradeTableSeeder.php
+++ database/seeds/GradeTableSeeder.php
@@ -11,35 +11,13 @@ class GradeTableSeeder extends Seeder {
     {
         \DB::table('grades')->truncate();
         \DB::table('grades')->insert([
-            [
-                'name' => 'K1',
-                'description' => 'K One',
-                'country_id' => config('futureed.default_country'),
-                'code' =>101,
-                'status' => 'Enabled',
-                'created_by' => 1,
-                'updated_by' => 1,
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")
 
-            ],
-            [
-                'name' => 'K2',
-                'description' => 'K Two',
-                'country_id' => config('futureed.default_country'),
-                'code' =>102,
-                'status' => 'Enabled',
-                'created_by' => 1,
-                'updated_by' => 1,
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")
-
-            ],
+			//United States of America
             [
                 'name' => 'Grade 1',
                 'description' => 'Grade One',
                 'country_id' => config('futureed.default_country'),
-                'code' =>103,
+                'code' =>1,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -51,7 +29,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 2',
                 'description' => 'Grade Two',
                 'country_id' => config('futureed.default_country'),
-                'code' =>104,
+                'code' =>2,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -63,7 +41,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 3',
                 'description' => 'Grade Three',
                 'country_id' => config('futureed.default_country'),
-                'code' =>105,
+                'code' =>3,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -75,7 +53,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 4',
                 'description' => 'Grade Four',
                 'country_id' => config('futureed.default_country'),
-                'code' =>106,
+                'code' =>4,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -87,7 +65,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 5',
                 'description' => 'Grade Five',
                 'country_id' => config('futureed.default_country'),
-                'code' =>107,
+                'code' =>5,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -99,7 +77,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 6',
                 'description' => 'Grade Six',
                 'country_id' => config('futureed.default_country'),
-                'code' =>108,
+                'code' =>6,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -111,7 +89,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 7',
                 'description' => 'Grade Seven',
                 'country_id' => config('futureed.default_country'),
-                'code' =>109,
+                'code' =>7,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -123,7 +101,7 @@ class GradeTableSeeder extends Seeder {
                 'name' => 'Grade 8',
                 'description' => 'Grade Eight',
                 'country_id' => config('futureed.default_country'),
-                'code' =>110,
+                'code' =>8,
                 'status' => 'Enabled',
                 'created_by' => 1,
                 'updated_by' => 1,
@@ -131,7 +109,200 @@ class GradeTableSeeder extends Seeder {
                 'updated_at' => date("Y-m-d H:i:s")
 
             ],
+			[
+				'name' => 'Grade 9',
+				'description' => 'Grade Nine',
+				'country_id' => config('futureed.default_country'),
+				'code' =>9,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
 
+			],
+			[
+				'name' => 'Grade 10',
+				'description' => 'Grade Ten',
+				'country_id' => config('futureed.default_country'),
+				'code' =>10,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Grade 11',
+				'description' => 'Grade Elevent',
+				'country_id' => config('futureed.default_country'),
+				'code' =>11,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Grade 12',
+				'description' => 'Grade Twelve',
+				'country_id' => config('futureed.default_country'),
+				'code' =>12,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+
+			//Singapore
+			[
+				'name' => 'Primary 1',
+				'description' => 'Primary One',
+				'country_id' => 702,
+				'code' =>13,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Primary 2',
+				'description' => 'Primary Two',
+				'country_id' => 702,
+				'code' =>14,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Primary 3',
+				'description' => 'Primary Three',
+				'country_id' =>702,
+				'code' =>15,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Primary 4',
+				'description' => 'Primary Four',
+				'country_id' => 702,
+				'code' =>16,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Primary 5',
+				'description' => 'Primary Five',
+				'country_id' => 702,
+				'code' =>17,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Primary 6',
+				'description' => 'Primary Six',
+				'country_id' => 702,
+				'code' =>18,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Sec 1',
+				'description' => 'Sec One',
+				'country_id' => 702,
+				'code' =>19,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Sec 2',
+				'description' => 'Sec Two',
+				'country_id' => 702,
+				'code' =>20,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Sec 3',
+				'description' => 'Sec Three',
+				'country_id' => 702,
+				'code' =>21,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'Sec 4',
+				'description' => 'Sec Four',
+				'country_id' => 702,
+				'code' =>22,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'JC 1',
+				'description' => 'JC One',
+				'country_id' => 702,
+				'code' =>23,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
+			[
+				'name' => 'JC 2',
+				'description' => 'JC Two',
+				'country_id' => 702,
+				'code' =>24,
+				'status' => 'Enabled',
+				'created_by' => 1,
+				'updated_by' => 1,
+				'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s")
+
+			],
         ]);
     }
 
