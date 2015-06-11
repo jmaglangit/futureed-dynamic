01c36488f9fc8022d52ad5a596ba4404b2cc9646
jayson1234 <jsuizo@nerubia.com>
2015-06-11 09:26:22 +0800

    change indention

--- app/Http/Controllers/Api/v1/ClientParentController.php
+++ app/Http/Controllers/Api/v1/ClientParentController.php
@@ -7,49 +7,50 @@ use Illuminate\Http\Request;
 
 class ClientParentController extends ApiController {
 
-	public function getStudentList($id){
+	public function getStudentList($id)
+	{
 
-        $parent_role = config('futureed.parent');
+		$parent_role = config('futureed.parent');
 
-        $this->addMessageBag($this->validateVarNumber($id));
+		$this->addMessageBag($this->validateVarNumber($id));
 
-        if($this->getMessageBag()){
+		if ($this->getMessageBag()) {
 
-            return $this->respondWithError($this->getMessageBag());
-        }
-
-
-        $stud_list = $this->client->checkClient($id,$parent_role);
-
-        if(is_null($stud_list)){
-
-            return $this->respondErrorMessage(2010);
-        }
-
-        $students = $this->student->getStudentByParent($id);
-
-        //check if parent has student
-        if(!$students){
-
-           return $this->respondErrorMessage(2130);
-        }
-
-        foreach ($students as $key => $value) {
-
-            $avatar[] = $this->avatar->getAvatar($value['avatar_id']);
-
-        }
-
-        foreach ($avatar as $key => $value) {
-
-           $url = $this->avatar->getAvatarThumbnailUrl($value['avatar_image']);
-
-           $students[$key]['url'] = $url;
+			return $this->respondWithError($this->getMessageBag());
+		}
 
 
-        }      
+		$stud_list = $this->client->checkClient($id, $parent_role);
 
-        return $this->respondWithData($students);
-    }
+		if (is_null($stud_list)) {
+
+			return $this->respondErrorMessage(2010);
+		}
+
+		$students = $this->student->getStudentByParent($id);
+
+		//check if parent has student
+		if (!$students) {
+
+			return $this->respondErrorMessage(2130);
+		}
+
+		foreach ($students as $key => $value) {
+
+			$avatar[] = $this->avatar->getAvatar($value['avatar_id']);
+
+		}
+
+		foreach ($avatar as $key => $value) {
+
+			$url = $this->avatar->getAvatarThumbnailUrl($value['avatar_image']);
+
+			$students[$key]['url'] = $url;
+
+
+		}
+
+		return $this->respondWithData($students);
+	}
 
 }
