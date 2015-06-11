2d18dbcb83c4be8cc8d548d71d3d79033f19ccd5
fbriones <franz@nerubia.com>
2015-06-11 10:04:24 +0800

    #KH548[ALL]fix tab size

--- app/Http/Middleware/StudentMiddleware.php
+++ app/Http/Middleware/StudentMiddleware.php
@@ -15,10 +15,10 @@ class StudentMiddleware {
 
         $response = $next($request);
 
-        $response->headers->set("Cache-Control","no-cache,no-store, must-revalidate");
+		$response->headers->set("Cache-Control","no-cache,no-store, must-revalidate");
     	$response->headers->set("Pragma", "no-cache"); //HTTP 1.0
     	$response->headers->set("Expires"," Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-    	
+
         return $response;
     }
 }
