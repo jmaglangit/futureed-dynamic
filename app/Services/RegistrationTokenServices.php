cc70166eb9384edd5623380a9e2df45e261c2d10
jasonmag <jasonmaglangit@gmail.com>
2015-06-16 16:12:02 +0800

    added registration token services

--- app/Services/RegistrationTokenServices.php
+++ app/Services/RegistrationTokenServices.php
@@ -1,14 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 6/16/15
- * Time: 11:47 AM
- */
 
 namespace FutureEd\Services;
 
 
 class RegistrationTokenServices {
+
+	protected $key ;
+
+	public function getKey(){
+
+		$this->key = config('token.key');
+		return $this->key;
+	}
+
+	/**
+	 * Get Registration token
+	 * @param $data
+	 * @return string
+	 */
+	public function getRegistrationToken($data){
+
+		return hash_hmac('sha1', $data ,$this->key);
+
+	}
 
 }
