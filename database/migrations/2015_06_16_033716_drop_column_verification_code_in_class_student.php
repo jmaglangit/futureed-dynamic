225ce71f216ce154947f30dbff9738522f126e51
Aaron <aaugusto@nerubia.com>
2015-06-17 09:55:44 +0800

    modify class student request validation, implement proper indention on the migration class.

--- database/migrations/2015_06_16_033716_drop_column_verification_code_in_class_student.php
+++ database/migrations/2015_06_16_033716_drop_column_verification_code_in_class_student.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class DropColumnVerificationCodeInClassStudent extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::table('class_students', function(Blueprint $table)
         {
             $table->dropColumn('verification_code');
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::table('class_students', function(Blueprint $table)
         {
             $table->bigInteger('verification_code')->after('status');
         });
-	}
+    }
 
 }
