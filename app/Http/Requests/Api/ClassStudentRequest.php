44d24c03a88408ba925c646558cb54244a28fa06
jasonmag <jasonmaglangit@gmail.com>
2015-06-18 16:41:50 +0800

    updated ClassStudentsRequest name regex

--- app/Http/Requests/Api/ClassStudentRequest.php
+++ app/Http/Requests/Api/ClassStudentRequest.php
@@ -19,35 +19,37 @@ class ClassStudentRequest extends ApiRequest {
      * @return array
      */
     public function rules() {
-        switch($this->method){
-            case 'POST':
-                switch($this->route()->getName()){
-                    case 'class-student.add.existing.student':
-                        return ['email'        =>   'required|email',
-                            'client_id'    =>   'required',
-                            'class_id'     =>   'required'];
-                        break;
-                    case 'class-student.add.new.student':
-                        return ['first_name'    => 'required|regex:/^([a-z\x20])+$/i|max:64',
-                            'last_name'     => 'required|regex:/^([a-z\x20])+$/i|max:64',
-                            'gender'        => 'required|alpha|in:male,female',
-                            'birth_date'    => 'required|date_format:Ymd|before:-13 year',
-                            'grade_code'    => 'required|numeric',
-                            'country_id'    => 'required|integer',
-                            'state'         => 'required|string',
-                            'city'          => 'required|string',
-                            'email'         => 'required|email',
-                            'username'      => 'required|min:8|max:32|alpha_num',
-                            'callback_uri'  => 'required|string',
-                            'client_id'     => 'required',
-                            'class_id'      => 'required'];
-                        break;
-                    default:
-                        return ['user_id'     =>   'required|email',
-                            'class_id'    =>   'required',
-                            'status'      =>   'required|in:Enabled,Disabled'];
-                }
-                break;
+		switch ($this->method) {
+			case 'POST':
+				switch ($this->route()->getName()) {
+					case 'class-student.add.existing.student':
+						return [
+							'email' => 'required|email',
+							'client_id' => 'required',
+							'class_id' => 'required'];
+						break;
+					case 'class-student.add.new.student':
+						return [
+							'first_name' => 'required|regex:'. config('regex.name') .'|max:64',
+							'last_name' => 'required|regex:'. config('regex.name') .'|max:64',
+							'gender' => 'required|alpha|in:male,female',
+							'birth_date' => 'required|date_format:Ymd|before:-13 year',
+							'grade_code' => 'required|numeric',
+							'country_id' => 'required|integer',
+							'state' => 'required|string',
+							'city' => 'required|string',
+							'email' => 'required|email',
+							'username' => 'required|min:8|max:32|alpha_num',
+							'callback_uri' => 'required|string',
+							'client_id' => 'required',
+							'class_id' => 'required'];
+						break;
+					default:
+						return ['user_id' => 'required|email',
+							'class_id' => 'required',
+							'status' => 'required|in:Enabled,Disabled'];
+				}
+				break;
         }
 
 
