d6e28def791f0c02985cc7550c91697d6f3830c9
jayson1234 <jsuizo@nerubia.com>
2015-06-23 12:04:36 +0800

    ticket 606 update space

--- app/Models/Repository/Invoice/InvoiceRepositoryInterface.php
+++ app/Models/Repository/Invoice/InvoiceRepositoryInterface.php
@@ -3,10 +3,15 @@
 
 interface InvoiceRepositoryInterface {
 
-    public function getInvoiceDetails($criteria = [],$limit = 0, $offset = 0);
-    public function addInvoice($data);
-    public function getInvoice($id);
-    public function getClientInvoiceDiscount($client_id);
-    public function getNextInvoiceNo();
+	public function getInvoiceDetails($criteria = [], $limit = 0, $offset = 0);
+
+	public function addInvoice($data);
+
+	public function getInvoice($id);
+
+	public function getClientInvoiceDiscount($client_id);
+
+	public function getNextInvoiceNo();
+
 	public function getDetails($id);
 }
