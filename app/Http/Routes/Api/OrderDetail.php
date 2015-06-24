483d8ab25d79a5e6b682b0a9bcca3fa5b03e31a5
Aaron <aaugusto@nerubia.com>
2015-06-24 15:50:59 +0800

    Changed routes in order detail.

--- app/Http/Routes/Api/OrderDetail.php
+++ app/Http/Routes/Api/OrderDetail.php
@@ -3,5 +3,5 @@ Routes::group(['prefix' => '/order-detail'], function()
 {
     Routes::get('/get-students/{order_id}', [
         'uses' => 'Api\v1\ParentStudentController@getStudents',
-        'as' => 'parent-student.get.students']);
+        'as' => 'order-detail.get.students']);
 });
