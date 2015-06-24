f39980a23840b01055cf4987554959f67f464ce9
jasonmag <jasonmaglangit@gmail.com>
2015-06-24 13:51:58 +0800

    updated school_country_id error message

--- app/Http/Controllers/Api/Traits/ApiValidatorTrait.php
+++ app/Http/Controllers/Api/Traits/ApiValidatorTrait.php
@@ -218,7 +218,9 @@ trait ApiValidatorTrait {
                     "school_code.required" => $error_msg[2602],
                     "school_code.numeric" => $error_msg[2602],
                     "country_id.required" => $error_msg[2603],
-                    "country_id.numeric" => $error_msg[2604]
+                    "country_id.numeric" => $error_msg[2604],
+					"school_country_id.required" => $error_msg[2603],
+					"school_country_id.numeric" => $error_msg[2604]
                 ]
             );
 
