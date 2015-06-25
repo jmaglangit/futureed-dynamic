aeddbc759671a46c3d3edbdd37e241b9fcec3640
franzbrian <franz@nerubia.com>
2015-06-25 20:39:56 +0800

    KH#595[Client]Updated-zip-code-to-postal-code

--- resources/views/client/login/registration-invite-form.blade.php
+++ resources/views/client/login/registration-invite-form.blade.php
@@ -180,7 +180,7 @@
                 </div>   
             
                 <div class="form-group">
-                    <label class="col-md-2 control-label">Zip Code</label>
+                    <label class="col-md-2 control-label">Postal Code</label>
                     <div class="col-md-4">
                         {!! Form::text('zip', ''
                             , array(
