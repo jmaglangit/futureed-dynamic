7e4564fa42c14ad4b3ab54c7b0067b73bb26b9c5
franzbrian <franz@nerubia.com>
2015-06-26 13:18:14 +0800

    KH#674[Client]Parent-payment-enchancement

--- resources/views/client/parent/payment/partials/payment_form.blade.php
+++ resources/views/client/parent/payment/partials/payment_form.blade.php
@@ -118,21 +118,21 @@
 	    						<a href="" ng-click="payment.setActive('view', key.id)" title="view"><span><i class="fa fa-eye"></i></span></a>
 	    					</div>
 	    					<div class="col-xs-4">
-	    						<a href="" ng-click="payment.setActive('view', key.id)" title="cancel"><span><i class="fa fa-ban"></i></span></a>
+	    						<a href="" ng-if="key.payment_status == 'Pending'" ng-click="payment.confirmCancelInvoice(key.id)" title="cancel"><span><i class="fa fa-ban"></i></span></a>
 	    					</div>
 	    					<div class="col-xs-4">
-	    						<a href="" ng-click="payment.deleteInvoice(key.id)" title="delete"><span><i class="fa fa-trash"></i></span></a>
+	    						<a href="" ng-if="key.payment_status == 'Pending' || key.payment_status == 'Cancelled'" ng-click="payment.confirmRemoveInvoice(key.id)" title="delete"><span><i class="fa fa-trash"></i></span></a>
 	    					</div>
 		            	</div>
 		            </td>
 		        </tr>
 		        <tr class="odd" ng-if="!payment.records.length && !payment.table.loading">
-		        	<td valign="top" colspan="7" class="dataTables_empty">
+		        	<td valign="top" colspan="8" class="dataTables_empty">
 		        		No records found
 		        	</td>
 		        </tr>
 		        <tr class="odd" ng-if="payment.table.loading">
-		        	<td valign="top" colspan="7" class="dataTables_empty">
+		        	<td valign="top" colspan="8" class="dataTables_empty">
 		        		Loading...
 		        	</td>
 		        </tr>
@@ -154,4 +154,64 @@
 			</div>
 		</div>
 	</div>
+</div>
+<div id="remove_subscription_modal_invoice" ng-show="payment.confirm_delete" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+            Remove Invoice
+        </div>
+        <div class="modal-body">
+            Are you sure you want to remove this invoice?
+        </div>
+        <div class="modal-footer">
+        	<div class="btncon col-md-8 col-md-offset-4 pull-left">
+                {!! Form::button('Yes'
+                    , array(
+                        'class' => 'btn btn-blue btn-medium'
+                        , 'ng-click' => "payment.deleteInvoice()"
+                        , 'data-dismiss' => 'modal'
+                    )
+                ) !!}
+
+                {!! Form::button('No'
+                    , array(
+                        'class' => 'btn btn-gold btn-medium'
+                        , 'data-dismiss' => 'modal'
+                    )
+                ) !!}
+        	</div>
+        </div>
+    </div>
+  </div>
+</div>
+<div id="cancel_subscription_modal_invoice" ng-show="payment.confirm_delete" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+            Cancel Invoice
+        </div>
+        <div class="modal-body">
+            Are you sure you want to cancel this invoice?
+        </div>
+        <div class="modal-footer">
+        	<div class="btncon col-md-8 col-md-offset-4 pull-left">
+                {!! Form::button('Yes'
+                    , array(
+                        'class' => 'btn btn-blue btn-medium'
+                        , 'ng-click' => "payment.cancelInvoice()"
+                        , 'data-dismiss' => 'modal'
+                    )
+                ) !!}
+
+                {!! Form::button('No'
+                    , array(
+                        'class' => 'btn btn-gold btn-medium'
+                        , 'data-dismiss' => 'modal'
+                    )
+                ) !!}
+        	</div>
+        </div>
+    </div>
+  </div>
 </div>
