ca0d06c6a079c88a188092465e7cca9a3100a5d2
jasonmag <jasonmaglangit@gmail.com>
2015-06-29 11:30:38 +0800

    removed unnecessary middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,15 +26,9 @@ class Kernel extends HttpKernel {
 	protected $routeMiddleware = [
 		'auth' => 'FutureEd\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-		'student' => 'FutureEd\Http\Middleware\StudentMiddleware',				
-		'admin' => 'FutureEd\Http\Middleware\AdminMiddleware',		
-		'client' => 'FutureEd\Http\Middleware\ClientMiddleware',
         'jwt' => 'FutureEd\Http\Middleware\Api\JWTMiddleware',
         'api_user' => 'FutureEd\Http\Middleware\Api\UserMiddleware',
         'api_after' => 'FutureEd\Http\Middleware\Api\AfterMiddleware',
-		'parent' => 'FutureEd\Http\Middleware\ParentMiddleware',
-		'principal' => 'FutureEd\Http\Middleware\PrincipalMiddleware',
-		'teacher' => 'FutureEd\Http\Middleware\TeacherMiddleware',
 	];
 
 }
