0d5a94d3baea4c71b6381cedcbd73601544e452b
jasonmag <jasonmaglangit@gmail.com>
2015-07-01 16:09:52 +0800

    added relationships and scopes

--- app/Models/Core/HelpRequestAnswer.php
+++ app/Models/Core/HelpRequestAnswer.php
@@ -1,4 +1,4 @@
-<?php namespace FutureEd;
+<?php namespace FutureEd\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -26,5 +26,65 @@ class HelpRequestAnswer extends Model {
 	];
 
 	protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
+
+	//Relationships
+	public function helpRequest(){
+
+		return $this->belongsTo('FutureEd\Model\Core\HelpRequest');
+	}
+
+	public function module(){
+
+		return $this->belongsTo('FutureEd\Model\Core\Module');
+	}
+
+	public function subject(){
+
+		return $this->belongsTo('FutureEd\Model\Core\Subject');
+	}
+
+	public function subjectArea(){
+
+		return $this->belongsTo('FutureEd\Model\Core\SubjectArea');
+	}
+
+
+	//Scopes
+	public function scopeRequestTitle($query, $help_request_title){
+
+		return $query->whereHas('helpRequest', function($query) use ($help_request_title){
+			$query->where('title', 'like','%'. $help_request_title . '%');
+		});
+	}
+
+	public function scopeModuleName($query,$module_name){
+
+		return $query->whereHas('module', function($query) use ($module_name){
+			$query->where('name','like','%'.$module_name.'%');
+		});
+	}
+
+	public function scopeSubjectName($query,$subject_name){
+
+		return $query->whereHas('subject', function($query) use ($subject_name){
+			$query->where('name','like','%'.$subject_name.'%');
+		});
+	}
+
+	public function scopeSubjectAreaName($query,$subject_area_name){
+
+		return $query->whereHas('subjectArea', function($query) use ($subject_area_name){
+			$query->where('name','like','%'.$subject_area_name.'%');
+		});
+	}
+
+	public function scopeAnswerStatus($query,$answer_status){
+
+		return $query->where('request_answer_status', $answer_status);
+
+	}
+
+
+
 
 }
