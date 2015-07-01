abbdca8c2fe6d34253c2a3b1450a94a3cc10e6a3
jasonmag <jasonmaglangit@gmail.com>
2015-07-01 14:26:48 +0800

    updated helprequestanswer model

--- app/Models/Core/HelpRequestAnswer.php
+++ app/Models/Core/HelpRequestAnswer.php
@@ -1,9 +1,30 @@
 <?php namespace FutureEd;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HelpRequestAnswer extends Model {
 
-	//
+	use SoftDeletes;
+
+	protected $table = 'help_request_answers';
+
+	protected $dates = ['created_at','updated_at','deleted_at'];
+
+	protected $fillable = [
+		'user_id',
+		'content',
+		'help_request_id',
+		'module_id',
+		'subject_id',
+		'area_id',
+		'rating',
+		'seq_no',
+		'is_verified',
+		'status',
+		'points'
+	];
+
+	protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
 
 }
