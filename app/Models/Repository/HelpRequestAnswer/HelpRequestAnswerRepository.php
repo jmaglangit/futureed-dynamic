f111b07d7cdef0885c1f744ab0e2dbdc106bf7fa
jasonmag <jasonmaglangit@gmail.com>
2015-07-01 16:10:46 +0800

    added getHelpRequestAnswers

--- app/Models/Repository/HelpRequestAnswer/HelpRequestAnswerRepository.php
+++ app/Models/Repository/HelpRequestAnswer/HelpRequestAnswerRepository.php
@@ -1,14 +1,60 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 7/1/15
- * Time: 2:31 PM
- */
-
-namespace FutureEd\Models\Repository\HelpRequestAnswer;
+<?php namespace FutureEd\Models\Repository\HelpRequestAnswer;
 
 
-class HelpRequestAnswerRepository {
+use FutureEd\HelpRequestAnswer;
+
+class HelpRequestAnswerRepository implements HelpRequestAnswerRepositoryInterface{
+
+	/**
+	 * Gets list of Help Request Answers.
+	 * @param $criteria
+	 * @param $limit
+	 * @param $offset
+	 * @return array
+	 */
+	public function getHelpRequestAnswers($criteria,$limit,$offset){
+
+		$help_request_answer = new HelpRequestAnswer();
+
+		if(isset($criteria['help_request'])){
+
+			$help_request_answer = $help_request_answer->RequestTitle($criteria['help_request']);
+		}
+
+		if(isset($criteria['module'])){
+
+			$help_request_answer = $help_request_answer->ModuleName($criteria['module']);
+
+		}
+
+		if(isset($criteria['subject'])){
+
+			$help_request_answer = $help_request_answer->SubjectName($criteria['subject']);
+		}
+
+		if(isset($criteria['subject_area'])){
+
+			$help_request_answer = $help_request_answer->SubjectAreaName($criteria['subject_are']);
+		}
+
+		if(isset($criteria['request_answer_status'])){
+
+			$help_request_answer = $help_request_answer->AnswerStatus($criteria['status']);
+
+		}
+
+
+		$count = $help_request_answer->count();
+
+		if($limit > 0 && $offset >= 0) {
+			$help_request_answer = $help_request_answer->offset($offset)->limit($limit);
+		}
+
+		return [
+			'total' => $count,
+			'records' => $help_request_answer->get()
+		];
+
+	}
 
 }
