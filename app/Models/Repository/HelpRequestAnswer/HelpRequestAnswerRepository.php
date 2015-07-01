e346fded6200c4a096fb726ff4768a3c0f4b64e6
jasonmag <jasonmaglangit@gmail.com>
2015-07-01 15:52:49 +0800

    added HelpRequestAnswer Repository

---
+++ app/Models/Repository/HelpRequestAnswer/HelpRequestAnswerRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/1/15
+ * Time: 2:31 PM
+ */
+
+namespace FutureEd\Models\Repository\HelpRequestAnswer;
+
+
+class HelpRequestAnswerRepository {
+
+}
---
+++ app/Models/Repository/HelpRequestAnswer/HelpRequestAnswerRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/1/15
+ * Time: 2:29 PM
+ */
+
+namespace FutureEd\Models\Repository\HelpRequestAnswer;
+
+
+interface HelpRequestAnswerRepositoryInterface {
+
+}
