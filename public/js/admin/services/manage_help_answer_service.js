5ffed9c6bb0f8d444f937ee6123688390f1abd83
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-07-03 13:32:28 +0800

    KH#759, #760, #761, #762 [Admin] Help Request Answer List, View, Edit, and Delete

--- public/js/admin/services/manage_help_answer_service.js
+++ public/js/admin/services/manage_help_answer_service.js
@@ -5,10 +5,49 @@ ManageHelpAnswerService.$inject = ['$http'];
 
 function ManageHelpAnswerService($http) {
 	var service = {};
-	var serviceUrl = '/api/v1';
+	var serviceUrl = '/api/v1/';
 
-	service.list = function list(search, table) {
-		return true;
+	service.list = function(search, table) {
+		return $http({
+			method 	: Constants.METHOD_GET
+			, url   : serviceUrl + 'help-request-answer?help_request=' + search.help_request
+				+ '&request_answer_status=' + search.request_answer_status
+				+ '&module=' + search.module
+				+ '&subject_area=' + search.subject_area
+				+ '&subject=' + search.subject
+				+ '&limit=' + table.size
+				+ '&offset=' + table.offset
+		});
+	}
+
+	service.detail = function(id) {
+		return $http({
+			method 	: Constants.METHOD_GET
+			, url   : serviceUrl + 'help-request-answer/' + id
+		});
+	}
+
+	service.update = function(data) {
+		return $http({
+			method 	: Constants.METHOD_PUT
+			, data	: data
+			, url   : serviceUrl + 'help-request-answer/' + data.id
+		});
+	}
+
+	service.delete = function(id) {
+		return $http({
+			method 	: Constants.METHOD_DELETE
+			, url   : serviceUrl + 'help-request-answer/' + id
+		});
+	}
+
+	service.updateHelpAnswerStatus = function(data) {
+		return $http({
+			method 	: Constants.METHOD_POST
+			, data  : { request_answer_status : data.request_answer_status }
+			, url   : serviceUrl + 'help-request-answer/status/' + data.id
+		});
 	}
 
 	return service;
