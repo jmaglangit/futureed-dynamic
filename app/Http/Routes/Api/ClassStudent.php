95d68a65ffc4afa7b4ac63113aba59711c713de9
jasonmag <jasonmaglangit@gmail.com>
2015-07-03 15:53:13 +0800

    added new routes

--- app/Http/Routes/Api/ClassStudent.php
+++ app/Http/Routes/Api/ClassStudent.php
@@ -4,7 +4,14 @@ Routes::group(['prefix' => '/class-student'], function()
     Routes::post('/add-existing-student', [
                 'uses' => 'Api\v1\ClassStudentController@addExistingStudent',
                 'as' => 'class-student.add.existing.student']);
+
     Routes::post('/add-new-student', [
     'uses' => 'Api\v1\ClassStudentController@addNewStudent',
     'as' => 'class-student.add.new.student']);
+
+	Routes::post('/student-join-class',
+		[
+			'uses' => 'Api\v1\ClassStudentController@studentJoinClass',
+			'as' => 'api.v1.class-student.student-join-class'
+		]);
 });
