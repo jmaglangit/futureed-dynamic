8b4b24c8b4a95d34e5d195f79ba86bd1b0b1bb53
jasonmag <jasonmaglangit@gmail.com>
2015-07-03 14:16:33 +0800

    updated required fields

--- app/Http/Requests/Api/HelpRequestAnswerRequest.php
+++ app/Http/Requests/Api/HelpRequestAnswerRequest.php
@@ -27,19 +27,8 @@ class HelpRequestAnswerRequest extends ApiRequest {
 			case 'PUT':
 
 				return [
-					'student_id' => 'required|exists:students,id,deleted_at,NULL',
 					'content' => 'required|string',
-					'help_request_id' => 'required|exists:help_requests,id,deleted_at,NULL',
-					'module_id' => 'required|exists:modules,id,deleted_at,NULL',
-					'subject_id' => 'required|exists:subjects,id,deleted_at,NULL',
-					'subject_area_id' => 'required|exists:subject_areas,id,deleted_at,NULL',
-					'rating' => 'numeric',
-					'seq_no' => 'required|numeric',
-					'request_answer_status' => 'required|in:Pending,Accepted,Rejected',
 					'status' => 'required|in:Enabled,Disabled',
-					'points' => 'required|numeric',
-					'created_by' => 'required|exists:users,id,deleted_at,NULL'
-
 				];
 				break;
 		}
