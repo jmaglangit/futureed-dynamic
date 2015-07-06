68fdff8b3f69094223dde4073912cbb73f920bf5
jasonmag <jasonmaglangit@gmail.com>
2015-07-06 14:23:44 +0800

    added necessary Age Group files

---
+++ app/Http/Routes/Api/AgeGroup.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/6/15
+ * Time: 2:19 PM
+ */
---
+++ app/Models/Repository/AgeGroup/AgeGroupRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/6/15
+ * Time: 2:13 PM
+ */
+
+namespace FutureEd\Models\Repository\AgeGroup;
+
+
+interface AgeGroupRepositoryInterface {
+
+}
