4c644e8fcdf6d28228d06b0dee03dca7ab2cfbd8
jasonmag <jasonmaglangit@gmail.com>
2015-07-06 13:53:10 +0800

    added Country Grade Repository files

---
+++ app/Models/Repository/CountryGrade/CountryGradeRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/6/15
+ * Time: 11:12 AM
+ */
+
+namespace FutureEd\Models\Repository\CountryGrade;
+
+
+class CountryGradeRepository {
+
+}
---
+++ app/Models/Repository/CountryGrade/CountryGradeRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/6/15
+ * Time: 11:09 AM
+ */
+
+namespace FutureEd\Models\Repository\CountryGrade;
+
+
+interface CountryGradeRepositoryInterface {
+
+}
