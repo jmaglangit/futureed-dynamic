62bc966bffff623ba82174e23ffcdcca2ffa695c
jasonmag <jasonmaglangit@gmail.com>
2015-07-06 18:44:32 +0800

    updated model

--- app/Models/Core/TeachingContent.php
+++ app/Models/Core/TeachingContent.php
@@ -1,9 +1,38 @@
-<?php namespace FutureEd;
+<?php namespace FutureEd\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
 
 class TeachingContent extends Model {
 
-	//
+	protected $table = 'teaching_contents';
+
+	protected $date = ['created_at','updated_at','deleted_at'];
+
+	protected $hidden = [
+		'created_by',
+		'updated_by',
+		'created_at',
+		'updated_at'
+	];
+
+	protected $fillable = [
+		'module_id',
+		'subject_id',
+		'subject_area_id',
+		'code',
+		'teaching_module',
+		'description',
+		'learning_style_id',
+		'content_url',
+		'media_type_id'
+	];
+
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
+
+
 
 }
