5e00ff16ead7a011f66c495e7d62662a3b26a469
jasonmag <jasonmaglangit@gmail.com>
2015-07-06 13:54:48 +0800

    added mutator

--- app/Models/Core/CountryGrade.php
+++ app/Models/Core/CountryGrade.php
@@ -1,9 +1,33 @@
 <?php namespace FutureEd\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CountryGrade extends Model {
 
+	use SoftDeletes;
+
 	protected $table = 'country_grades';
+
+	protected $dates = ['created_at','updated_at','deleted_at'];
+
+	protected $fillable = [
+		'age_group_id',
+		'country_id',
+		'grade_id'
+	];
+
+	protected $hidden = [
+		'created_by',
+		'updated_by',
+		'created_at',
+		'updated_at',
+		'deleted_at'
+	];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
 
 }
