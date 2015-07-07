cd1c1af6a874abb0799bf06b607d23023dddff4f
jasonmag <jasonmaglangit@gmail.com>
2015-07-07 18:51:11 +0800

    updated country grade repository

--- app/Models/Repository/CountryGrade/CountryGradeRepository.php
+++ app/Models/Repository/CountryGrade/CountryGradeRepository.php
@@ -10,6 +10,7 @@ namespace FutureEd\Models\Repository\CountryGrade;
 
 
 use FutureEd\Models\Core\CountryGrade;
+use League\Flysystem\Exception;
 
 class CountryGradeRepository implements CountryGradeRepositoryInterface{
 
@@ -19,6 +20,31 @@ class CountryGradeRepository implements CountryGradeRepositoryInterface{
 			return CountryGrade::create($data);
 
 		} catch (Exception $e){
+
+			return $e->getMessage();
+		}
+	}
+
+	/**
+	 * Get Country Grade by grade_id
+	 * @param $grade_id
+	 * @return mixed
+	 */
+	public function getCountryGradeByGrade($grade_id){
+
+		return CountryGrade::gradeId($grade_id)->first();
+	}
+
+	public function updateAgeGroup($grade_id, $data){
+
+		try{
+
+			CountryGrade::gradeId($grade_id)
+				->update($data);
+
+			return $this->getCountryGradeByGrade($grade_id);
+
+		} catch(Exception $e){
 
 			return $e->getMessage();
 		}
