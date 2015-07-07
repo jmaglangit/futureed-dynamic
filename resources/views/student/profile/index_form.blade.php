d8b40396e99ae4ababcdd117f4e0912922be41c0
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-07-07 13:44:46 +0800

    KH#828, #829, #830 [Student] Updated labels, ui enhancements

--- resources/views/student/profile/index_form.blade.php
+++ resources/views/student/profile/index_form.blade.php
@@ -53,7 +53,7 @@
                     )
                 ) !!}
             </div>
-            <div class="col-xs-2">
+            <div class="col-xs-3">
                 <a href="" ng-click="profile.setStudentProfileActive('confirm_email')" class="edit-email">Confirm Email Address</a>
             </div>	
         </div>
