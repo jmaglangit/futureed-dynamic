5b75002dd72ffd42ba7048b46d583174054ad9a9
jasonmag <jasonmaglangit@gmail.com>
2015-07-08 16:17:56 +0800

    added interface for modulecontent

--- app/Models/Repository/ModuleContent/ModuleContentRepositoryInterface.php
+++ app/Models/Repository/ModuleContent/ModuleContentRepositoryInterface.php
@@ -1,14 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 7/8/15
- * Time: 1:54 PM
- */
-
-namespace FutureEd\Models\Repository\ModuleContent;
+<?php namespace FutureEd\Models\Repository\ModuleContent;
 
 
 interface ModuleContentRepositoryInterface {
+
+	public function getModuleContents();
+
+	public function getModuleContent($id);
+
+	public function getModuleContentByTeachingContent($content_id);
+
+	public function addModuleContent($data);
+
+	public function updateModuleContent($id,$data);
+
+	public function updateModuleContentByTeachingContent($content_id, $data);
 
 }
