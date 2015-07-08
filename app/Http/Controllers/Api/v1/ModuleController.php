a92ca82bf911a28dcb0431b4d8be73fb0a982664
jasonmag <jasonmaglangit@gmail.com>
2015-07-08 18:30:09 +0800

    created module controller

---
+++ app/Http/Controllers/Api/v1/ModuleController.php
@@ -0,0 +1,76 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Http\Controllers\Controller;
+
+use FutureEd\Models\Repository\Module\ModuleRepositoryInterface;
+use Illuminate\Http\Request;
+
+class ModuleController extends ApiController {
+
+	protected $module;
+
+	public function __construct(
+		ModuleRepositoryInterface $moduleRepositoryInterface
+	){
+
+		$this->module = $moduleRepositoryInterface;
+
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return $this->respondWithData(
+			$this->module->viewModule($id)
+		);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
