e7972b93e08f05d74b76268a35fd8c5cb14ab8fd
jayson1234 <jsuizo@nerubia.com>
2015-07-08 17:17:36 +0800

    891 change routes

--- app/Http/Routes/Api/Question.php
+++ app/Http/Routes/Api/Question.php
@@ -15,7 +15,7 @@ Routes::group(['prefix' => '/question'], function() {
 	Routes::resource('/answer/admin', 'Api\v1\AdminQuestionAnswerController',
 		['except' => ['create', 'edit']]);
 
-	Routes::post('/class/update-image/{id}',[
+	Routes::post('/answer/update-image/{id}',[
 		'as' => 'api.v1.admin.answer.image.update',
 		'uses' => 'Api\v1\QuestionAnswerController@updateQuestionAnswerImage']);
 
