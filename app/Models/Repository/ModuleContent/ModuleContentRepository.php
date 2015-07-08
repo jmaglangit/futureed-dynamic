7837dc4f106e42ce7af7691e2c6a7bb15b778d31
jasonmag <jasonmaglangit@gmail.com>
2015-07-08 16:13:42 +0800

    added new module content repository

---
+++ app/Models/Repository/ModuleContent/ModuleContentRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/8/15
+ * Time: 1:55 PM
+ */
+
+namespace FutureEd\Models\Repository\ModuleContent;
+
+
+class ModuleContentRepository {
+
+}
---
+++ app/Models/Repository/ModuleContent/ModuleContentRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 7/8/15
+ * Time: 1:54 PM
+ */
+
+namespace FutureEd\Models\Repository\ModuleContent;
+
+
+interface ModuleContentRepositoryInterface {
+
+}
