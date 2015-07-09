e65d674971c612f9bd625afec4f19518c7d76551
jasonmag <jasonmaglangit@gmail.com>
2015-07-09 15:00:34 +0800

    removed comments

--- database/migrations/2015_07_09_065100_create_system_settings_table.php
+++ database/migrations/2015_07_09_065100_create_system_settings_table.php
@@ -4,16 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateSystemSettingsTable extends Migration {
-//No	Field Name	Screen Label	Data Type	Length	Required	Values	Sample Data
-//1	id	 	BIGINT	20	Yes		1
-//2	key	Key	VARCHAR	256
-//3	value	Value	VARCHAR	256
-//4	description	Description	VARCHAR	256			This is a sample description
-//5	created_by	Created By	BIGINT	20	Yes		1
-//6	updated_by	Updated By	BIGINT	20	Yes		1
-//7	created_at	Created Date	TIMESTAMP		Yes		20150330 09:00:00
-//8	updated_at	Updated Date	TIMESTAMP		Yes		20150330 09:01:00
-//9	deleted_at	[internal field]	TIMESTAMP		Yes		00000000 00:00:00
+
 	/**
 	 * Run the migrations.
 	 *
