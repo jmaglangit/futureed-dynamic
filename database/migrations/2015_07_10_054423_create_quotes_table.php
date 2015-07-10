95f3596b542b911725400c36b0c13e978de7e407
jasonmag <jasonmaglangit@gmail.com>
2015-07-10 13:58:39 +0800

    removed comments

--- database/migrations/2015_07_10_054423_create_quotes_table.php
+++ database/migrations/2015_07_10_054423_create_quotes_table.php
@@ -4,17 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateQuotesTable extends Migration {
-//No	Field Name	Screen Label	Data Type	Length	Required	Values	Sample Data
-//1	id	 	BIGINT	20	Yes		1
-//2	quote	Quote	TEXT		Yes		3 month Subscription
-//3	percent	Percent	SMALLINT	3	Yes
-//4	answer_status	[internal field]	ENUM			Correct, Wrong
-//5	seq_no	Sequence No	BIGINT	20	Yes		1
-//6	created_by	Created By	BIGINT	20	Yes		1
-//7	updated_by	Updated By	BIGINT	20	Yes		1
-//8	created_at	Created Date	TIMESTAMP		Yes		20150330 09:00:00
-//9	updated_at	Updated Date	TIMESTAMP		Yes		20150330 09:01:00
-//10	deleted_at	[internal field]	TIMESTAMP		Yes		00000000 00:00:00
+
 	/**
 	 * Run the migrations.
 	 *
