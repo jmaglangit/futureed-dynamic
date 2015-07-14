f0d61663b415bce9ce37db7df16648f40de6b336
jayson1234 <jsuizo@nerubia.com>
2015-07-14 09:53:28 +0800

    dev-api-979-admin-add-edit-question

--- app/Http/Controllers/Api/v1/AdminQuestionAnswerController.php
+++ app/Http/Controllers/Api/v1/AdminQuestionAnswerController.php
@@ -84,7 +84,7 @@ class AdminQuestionAnswerController extends ApiController {
 			//get image_name
 			$image = $_FILES['image']['name'];
 
-			//uploads image file
+			//upload image file
 			$data['image']->move(config('futureed.question_answer_image_path'), $image);
 
 			//set value for answer_image
