e377b08a5cadcca2ca1ff0e90b9ed9c3f65fd2aa
jayson1234 <jsuizo@nerubia.com>
2015-07-15 19:50:52 +0800

    1028 admin modify upload image on question and question answer

--- app/Http/Controllers/Api/v1/QuestionAnswerController.php
+++ app/Http/Controllers/Api/v1/QuestionAnswerController.php
@@ -5,6 +5,7 @@ use FutureEd\Http\Controllers\Controller;
 use FutureEd\Models\Repository\QuestionAnswer\QuestionAnswerRepositoryInterface;
 use FutureEd\Http\Requests\Api\QuestionAnswerRequest;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Input;
 
 use Illuminate\Http\Request;
 
@@ -25,20 +26,35 @@ class QuestionAnswerController extends ApiController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function uploadQuestionAnswerImage(QuestionAnswerRequest $request)
+	public function uploadQuestionAnswerImage()
 	{
-		$data = $request->only('image');
-		$now = Carbon::now()->timestamp;
+		$input = Input::only('file');
 
+		$now = Carbon::now()->timestamp;
 		$return = NULL;
+		define('MB',1048576);
 
 		//check if has images uploaded
-		if($data['image']){
-			//get image_name
-			$image = $_FILES['image']['name'];
+		if($input['file'])
+		{
+			if($_FILES['file']['type'] != 'image/jpeg' && $_FILES['file']['type'] != 'image/png'){
 
-			//upload image file
-			$data['image']->move(config('futureed.answer_image_path').'/'.$now,$image);
+				return $this->respondErrorMessage(2142);
+
+			}
+
+
+			if($_FILES['file']['size'] > 2 * MB){
+
+				return $this->respondErrorMessage(2143);
+
+			}
+
+			//get image_name
+			$image = $_FILES['file']['name'];
+
+			//uploads image file
+			$input['file']->move(config('futureed.answer_image_path').'/'.$now,$image);
 
 			//return the original name of the image
 			$return['image_name'] = $now.'/'.$image;
