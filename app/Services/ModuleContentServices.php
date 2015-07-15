7353b9d5222ac613b6c6d98455f22107619ebe03
jasonmag <jasonmaglangit@gmail.com>
2015-07-15 22:13:42 +0800

    added new services

--- app/Services/ModuleContentServices.php
+++ app/Services/ModuleContentServices.php
@@ -3,7 +3,7 @@
 
 use FutureEd\Models\Repository\ModuleContent\ModuleContentRepositoryInterface;
 
-class ModuleContent
+class ModuleContentServices
 {
 
 	protected $module_content;
@@ -15,11 +15,18 @@ class ModuleContent
 		$this->module_content = $moduleContentRepositoryInterface;
 	}
 
+	/**
+	 * Sort sequence by adding.
+	 * @param $module_id
+	 * @param $seq_no
+	 * @return mixed
+	 */
 	public function updateSequence($module_id, $seq_no)
 	{
 
 		//get sequence number
-		$sequence = $this->question->getQuestionSequenceNos($module_id);
+		$sequence = $this->module_content->getModuleContentSequenceNos($module_id);
+
 
 		foreach ($sequence as $seq => $list) {
 
@@ -31,5 +38,29 @@ class ModuleContent
 
 		return $sequence;
 	}
+
+	public function pullSequenceNo($module_id,$seq_no, $id){
+
+		//get sequence number
+		$sequence = $this->module_content->getModuleContentSequenceNos($module_id);
+
+
+		foreach ($sequence as $seq => $list) {
+
+			//retrack sequence
+			if ($list->id == $id) {
+
+				$list->seq_no = 0;
+			}
+
+			if ($list->seq_no > $seq_no) {
+
+				$list->seq_no--;
+			}
+		}
+
+		return $sequence;
+	}
+
 
 }
