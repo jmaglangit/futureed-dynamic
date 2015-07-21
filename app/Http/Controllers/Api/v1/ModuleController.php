70a200ada5271eeaa16220dc176e61e97e528bdf
jayson1234 <jsuizo@nerubia.com>
2015-07-21 09:29:08 +0800

    1082 update modulecontroller

--- app/Http/Controllers/Api/v1/ModuleController.php
+++ app/Http/Controllers/Api/v1/ModuleController.php
@@ -5,7 +5,6 @@ use FutureEd\Http\Controllers\Controller;
 
 use FutureEd\Models\Repository\Module\ModuleRepositoryInterface;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
 
 class ModuleController extends ApiController {
 
@@ -26,7 +25,42 @@ class ModuleController extends ApiController {
 	 */
 	public function index()
 	{
+		$criteria = [];
+		$limit = 0 ;
+		$offset = 0;
 
+
+
+
+		//for module name
+		if(Input::get('name')){
+
+			$criteria['name'] = Input::get('name');
+		}
+
+		//for subject
+		if(Input::get('subject')){
+
+			$criteria['subject'] = Input::get('subject');
+		}
+
+		//Age Group
+		//for subject
+		if(Input::get('age_group_id')){
+
+			$criteria['age_group_id'] = Input::get('age_group_id');
+		}
+
+		if(Input::get('limit')) {
+					$limit = intval(Input::get('limit'));
+		}
+
+		if(Input::get('offset')) {
+					$offset = intval(Input::get('offset'));
+		}
+
+		//get module list with relation to subject,subject_area,grade
+		return $this->respondWithData($this->module->getModules($criteria , $limit, $offset ));
 	}
 
 	/**
