deac36fc12e66d2d5fd9bbad9b1055d29a23b2d3
franzbrian <franz@nerubia.com>
2015-07-21 15:42:56 +0800

    KH#921[student]indention changes

--- resources/views/student/class/module/index.blade.php
+++ resources/views/student/class/module/index.blade.php
@@ -68,42 +68,42 @@
 									<label class="control-label col-xs-2">Title</label>
 									<div class="col-xs-10">
 										{!! Form::text('title', ''
-							                , array(
-							                    'class' => 'form-control sidebar-input'
-							                    , 'placeholder' => 'Title' 
-							                    , 'ng-model' => 'class.help.title'
-							                    , 'autocomplete' => 'off')
-							            ) !!}
+											, array(
+											    'class' => 'form-control sidebar-input'
+											    , 'placeholder' => 'Title' 
+											    , 'ng-model' => 'class.help.title'
+											    , 'autocomplete' => 'off')
+										) !!}
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="control-label col-xs-2">Description</label>
 									<div class="col-xs-10">
 										{!! Form::textarea('content', ''
-						                , array(
-						                    'class' => 'form-control sidebar-input'
-						                    , 'placeholder' => 'Description' 
-						                    , 'ng-model' => 'class.help.content'
-						                    , 'autocomplete' => 'off')
-						            ) !!}
+							                , array(
+							                    'class' => 'form-control sidebar-input'
+							                    , 'placeholder' => 'Description' 
+							                    , 'ng-model' => 'class.help.content'
+							                    , 'autocomplete' => 'off')
+							            ) !!}
 									</div>
 								</div>
 							</div>
 							<div class="col-xs-12 btn-container">
 								{!! Form::button('Submit'
-					                , array(
-					                  'id' => 'validate_code_btn'
-					                  , 'class' => 'btn btn-maroon btn-medium'
-					                  , 'ng-click' => 'class.submitHelp()'
-					                )
-					            ) !!}
+									, array(
+									  'id' => 'validate_code_btn'
+									  , 'class' => 'btn btn-maroon btn-medium'
+									  , 'ng-click' => 'class.submitHelp()'
+									)
+								) !!}
 					            {!! Form::button('Back'
-					                , array(
-					                  'id' => 'validate_code_btn'
-					                  , 'class' => 'btn btn-gold btn-medium'
-					                  , 'ng-click' => 'class.submitHelp()'
-					                )
-					            ) !!}
+									, array(
+										'id' => 'validate_code_btn'
+										, 'class' => 'btn btn-gold btn-medium'
+										, 'ng-click' => 'class.submitHelp()'
+										)
+								) !!}
 							</div>
 							{!! Form::close() !!}
 						</div>
@@ -124,60 +124,8 @@
 								</div>
 							</div>
 						</div>
-						<div class="col-xs-10 col-xs-offset-1 margin-top-15">
-							<div class="alert alert-error" ng-if="mod.errors">
-					            <p ng-repeat="error in mod.errors track by $index" > 
-					                {! error !}
-					            </p>
-					        </div>
-					        <div class="alert alert-success" ng-if="mod.success">
-					        	<p>Successfully added your help request</p>
-					        </div>
-							<div class="col-xs-12">
-								<div class="clearfix"></div>
-								{!! Form::open(['class' => 'form-horizontal margin-top-15']) !!}
-								<div class="form-group">
-									<label class="control-label col-xs-2">Title</label>
-									<div class="col-xs-10">
-										{!! Form::text('title', ''
-							                , array(
-							                    'class' => 'form-control sidebar-input'
-							                    , 'placeholder' => 'Title' 
-							                    , 'ng-model' => 'mod.add.title'
-							                    , 'autocomplete' => 'off')
-							            ) !!}
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="control-label col-xs-2">Description</label>
-									<div class="col-xs-10">
-										{!! Form::textarea('content', ''
-						                , array(
-						                    'class' => 'form-control sidebar-input'
-						                    , 'placeholder' => 'Description' 
-						                    , 'ng-model' => 'mod.add.content'
-						                    , 'autocomplete' => 'off')
-						            ) !!}
-									</div>
-								</div>
-							</div>
-							<div class="col-xs-12 btn-container">
-								{!! Form::button('Submit'
-					                , array(
-					                  'id' => 'validate_code_btn'
-					                  , 'class' => 'btn btn-maroon btn-medium'
-					                  , 'ng-click' => 'mod.addHelp()'
-					                )
-					            ) !!}
-					            {!! Form::button('Back'
-					                , array(
-					                  'id' => 'validate_code_btn'
-					                  , 'class' => 'btn btn-gold btn-medium'
-					                  , 'ng-click' => 'class.submitHelp()'
-					                )
-					            ) !!}
-							</div>
-						</div>
+						<div template-directive template-url="{!! route('student.class.module.partials.add_help') !!}"></div>
+						<div template-directive template-url="{!! route('student.class.module.partials.list_help') !!}"></div>
 					</div>
 				</div>
 			</div>
