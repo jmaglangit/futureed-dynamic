82a991758ceec8f0aa144731931ea8c3b258cae8
jayson1234 <jsuizo@nerubia.com>
2015-07-22 11:15:19 +0800

    1084 student module get badges

--- app/Models/Core/Badge.php
+++ app/Models/Core/Badge.php
@@ -1,9 +1,18 @@
-<?php namespace FutureEd;
+<?php namespace FutureEd\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Badge extends Model {
 
-	//
+	use SoftDeletes;
+
+	protected $table = 'badges';
+
+	protected $dates = ['deleted_at'];
+
+	protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
+
+
 
 }
