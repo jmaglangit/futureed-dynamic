98b6c7903c202624ac78da23006eacdb2a98d254
jasonmag <jasonmaglangit@gmail.com>
2015-07-22 16:09:38 +0800

    added services

--- app/Services/FileServices.php
+++ app/Services/FileServices.php
@@ -1,16 +1,41 @@
 <?php namespace FutureEd\Services;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Filesystem\Filesystem;
 
-class ImageServices {
+
+class FileServices {
+
+	protected $file_system;
+
+	public function __construct(
+		Filesystem $filesystem
+	){
+
+		$this->file_system = $filesystem;
+
+	}
 
 	/**
 	 * Delete image file.
 	 * @param $file
 	 */
-	public function deleteImageFile($file){
+	public function deleteDirectory($file){
 
-		return File::delete($file);
+		//get file
+
+
+
+		if($this->file_system->exists($file)){
+
+			$file_dir = dirname($file);
+
+			$clean = $this->file_system->deleteDirectory($file_dir);
+
+			return $clean;
+		}
+
+		return false;
 
 	}
 
