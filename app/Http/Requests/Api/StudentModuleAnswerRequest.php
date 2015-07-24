641a85ea54939aa5dc8cd97b648f276a5e674f32
Aaron <aaugusto@nerubia.com>
2015-07-24 12:01:21 +0800

    KH#1105: Added logic on incrementing difficulty level and answering other types of questions.

--- app/Http/Requests/Api/StudentModuleAnswerRequest.php
+++ app/Http/Requests/Api/StudentModuleAnswerRequest.php
@@ -25,10 +25,11 @@ class StudentModuleAnswerRequest extends ApiRequest
             'module_id' => 'required|integer',
             'seq_no' => 'required|integer',
             'question_id' => 'required|integer',
-            'answer_id' => 'required|integer',
+            'answer_id' => 'integer',
             'answer_text' => 'string',
             'student_id' => 'required|integer',
-            'total_time' => 'required|integer'];
+            'total_time' => 'required|integer',
+            'answer_text' => 'string'];
     }
 
     public function messages()
--- app/Models/Core/StudentModule.php
+++ app/Models/Core/StudentModule.php
@@ -14,11 +14,11 @@ class StudentModule extends Model {
 
     protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
 
-    protected $fillable = ['class_id', 'student_id', 'module_id', 'module_status', 'last_viewed_content_id', 'progress', 'date_start'
-        ,'date_end', 'total_time', 'question_counter', 'wrong_counter','correct_counter', 'running_points', 'points_earned','last_answered_question_id'
-        ,'created_by','updated_by'];
+    protected $fillable = ['class_id', 'student_id', 'module_id', 'module_status', 'last_viewed_content_id', 'progress', 'date_start',
+        'date_end', 'total_time', 'question_counter', 'wrong_counter','correct_counter', 'running_points', 'points_earned','last_answered_question_id',
+        'total_correct_answer','current_difficulty_level', 'created_by','updated_by'];
 
     protected $attributes = ['progress' => 0,'total_time' => 0,'question_counter'=> 0,
-        'wrong_counter' => 0,'correct_counter'=> 0,
-        'running_points'=> 0,'points_earned'=> 0,'last_answered_question_id'=> 0];
+        'wrong_counter' => 0,'correct_counter'=> 0, 'running_points'=> 0,'points_earned'=> 0,
+        'last_answered_question_id'=> 0,'total_correct_answer' => 0,'current_difficulty_level'=> 1];
 }
