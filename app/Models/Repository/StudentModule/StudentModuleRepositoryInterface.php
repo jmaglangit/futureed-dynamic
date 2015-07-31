96fd82ed46f37147d01a493c1c8843d1181f3bfe
jasonmag <jasonmaglangit@gmail.com>
2015-07-31 15:49:20 +0800

    added method contract

--- app/Models/Repository/StudentModule/StudentModuleRepositoryInterface.php
+++ app/Models/Repository/StudentModule/StudentModuleRepositoryInterface.php
@@ -1,8 +1,11 @@
 <?php namespace FutureEd\Models\Repository\StudentModule;
 
 interface StudentModuleRepositoryInterface {
-    public function addStudentModule($data);
-    public function getStudentModule($id);
-    public function updateStudentModule($id,$data);
-    public function viewStudentModule($id);
+	public function addStudentModule($data);
+
+	public function getStudentModule($id);
+
+	public function updateStudentModule($id, $data);
+
+	public function viewStudentModule($id);
 }
