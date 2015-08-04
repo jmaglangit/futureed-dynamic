763b8ff58b10e74d5dbcf8a74137e143afafd945
jasonmag <jasonmaglangit@gmail.com>
2015-08-04 20:39:16 +0800

    implemented method repos

--- app/Models/Repository/Question/QuestionRepository.php
+++ app/Models/Repository/Question/QuestionRepository.php
@@ -270,7 +270,30 @@ class QuestionRepository implements QuestionRepositoryInterface{
 	 */
 	public function getQuestionsByModule($module_id){
 
-		return Question::whereModuleId($module_id)->get();
+		return Question::whereModuleId($module_id)
+			->orderBySeqNo()->get();
+	}
+
+	/**
+	 * Get questions by points to finish.
+	 * @param $module_id
+	 * @param $points_to_finish
+	 */
+	public function getQuestionByPointsToFinish($module_id, $points_to_finish){
+
+		return Question::whereModuleId($module_id)
+			->orderBySeqNo()->take($points_to_finish)->get();
+	}
+
+	/**
+	 * GEt first question.
+	 * @param $module_id
+	 * @return mixed
+	 */
+	public function getQuestionFirst($module_id){
+
+		return Question::whereModuleId($module_id)
+			->orderBySeqNo()->take(1)->get();
 	}
 
 
