db1f994e1f0cfb0bb694649c31ede9c3bcc461f9
jasonmag <jasonmaglangit@gmail.com>
2015-08-05 23:56:06 +0800

    added Avatar wiki routes and services

---
+++ app/Http/Routes/Api/AvatarWiki.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/5/15
+ * Time: 10:59 PM
+ */
---
+++ app/Services/WikipediaServices.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/5/15
+ * Time: 11:37 PM
+ */
+
+namespace FutureEd\Services;
+
+
+class WikipediaServices {
+
+}
