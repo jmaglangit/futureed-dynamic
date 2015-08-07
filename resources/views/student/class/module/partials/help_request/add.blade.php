2710540a6f79b58d096e2ed0327cfb67f7fd5fbe
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-08-07 14:55:04 +0800

    KH#1045 [Student] Help Request (tied to a Teaching content/question) - VIEW

--- resources/views/student/class/module/partials/help_request/add.blade.php
+++ resources/views/student/class/module/partials/help_request/add.blade.php
@@ -1,41 +1,44 @@
-<div class="col-xs-10 col-xs-offset-1 margin-top-15" ng-if="help.active_add">
-	<div class="alert alert-error" ng-if="help.errors">
-        <p ng-repeat="error in help.errors track by $index" > 
-            {! error !}
-        </p>
-    </div>
-    <div class="alert alert-success" ng-if="help.success">
-    	<p>Successfully added your help request</p>
-    </div>
-	<div class="col-xs-12">
-		<div class="clearfix"></div>
-		{!! Form::open(['class' => 'form-horizontal margin-top-15']) !!}
-		<div class="form-group">
-			<label class="control-label col-xs-2">Title</label>
-			<div class="col-xs-10">
-				{!! Form::text('title', ''
-					, array(
-					    'class' => 'form-control sidebar-input'
-					    , 'placeholder' => 'Title' 
-					    , 'ng-model' => 'help.add.title'
-					    , 'autocomplete' => 'off')
-				) !!}
+<div class="col-xs-12 margin-top-15" ng-if="help.active_add">
+	<div id="help_request_list" class="view-help-container">
+		<div class="alert alert-error" ng-if="help.errors">
+	        <p ng-repeat="error in help.errors track by $index" > 
+	            {! error !}
+	        </p>
+	    </div>
+	    <div class="alert alert-success" ng-if="help.success">
+	    	<p>Successfully added your help request. </p>
+	    </div>
+
+		<div class="col-xs-12">
+			<div class="clearfix"></div>
+			{!! Form::open(['class' => 'form-horizontal margin-top-15']) !!}
+			<div class="form-group">
+				<label class="control-label col-xs-2">Title</label>
+				<div class="col-xs-10">
+					{!! Form::text('title', ''
+						, array(
+						    'class' => 'form-control sidebar-input'
+						    , 'placeholder' => 'Title' 
+						    , 'ng-model' => 'help.record.title'
+						    , 'autocomplete' => 'off')
+					) !!}
+				</div>
 			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-xs-2">Description</label>
-			<div class="col-xs-10">
-				{!! Form::textarea('content', ''
-					, array(
-					    'class' => 'form-control sidebar-input'
-					    , 'placeholder' => 'Description' 
-					    , 'ng-model' => 'help.add.content'
-					    , 'autocomplete' => 'off')
-				) !!}
+			<div class="form-group">
+				<label class="control-label col-xs-2">Description</label>
+				<div class="col-xs-10">
+					{!! Form::textarea('content', ''
+		                , array(
+		                    'class' => 'form-control sidebar-input disabled-textarea'
+		                    , 'placeholder' => 'Description' 
+		                    , 'ng-model' => 'help.record.content'
+		                    , 'autocomplete' => 'off')
+		            ) !!}
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="col-xs-12 btn-container">
+	<div class="col-xs-10 col-xs-offset-2 btn-container">
 		{!! Form::button('Submit'
 			, array(
 			  'id' => 'validate_code_btn'
@@ -43,12 +46,13 @@
 			  , 'ng-click' => 'help.addHelp()'
 			)
 		) !!}
-        {!! Form::button('Back'
-            , array(
-              'id' => 'validate_code_btn'
-              , 'class' => 'btn btn-gold btn-medium'
-              , 'ng-click' => 'help.setCurrentActive(); help.toggleBtnHelp()'
-            )
-        ) !!}
+
+		{!! Form::button('Back'
+			, array(
+				 'class' => 'btn btn-gold btn-medium'
+				, 'ng-click' => 'help.setActive()'
+				)
+		) !!}
 	</div>
+	{!! Form::close() !!}
 </div>
--- resources/views/student/class/module/partials/help_request/view.blade.php
+++ resources/views/student/class/module/partials/help_request/view.blade.php
@@ -1,78 +1,57 @@
-<div class="col-xs-12 margin-top-15 list-container" ng-if="help.active_view">
-	<div class="content-box row margin-top-bot-5">
-		<div class="help-container">
-			<h3>{! help.details.title !}</h3>
-			<div class="row">
-				<p>{! help.details.created_at !}</p>
-			</div>
-			<div class="row">
-				<div class="col-xs-6">
-					<p>{! help.details.subject_area_name !} > {! help.details.subject_name !}</p>
+<div class="col-xs-12 margin-top-15" ng-if="help.active_view">
+	<div class="view-help-container">
+		<div class="content-box row margin-top-bot-5">
+			<div>
+				<div class="row">
+					<div class="col-xs-6">
+						<p class="tip-title">{! help.record.title !}</p class="title-mid">
+					</div>
 				</div>
-				<div class="col-xs-6">
-					<span><i class="fa fa-user"></i> By : {! help.details.name !}</span>
+
+				<div class="row">
+					<div class="col-xs-6">
+						<p><i class="fa fa-calendar-o"></i> {! help.record.created_at !}</p>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<hr/>
-				<p>{! help.details.content !}</p>
-			</div>
-		</div>
-		<div class="help-container" ng-if="help.ans_record.length == 0">
-			<div class="row">
-				<h4>No Answers Yet</h4>	
-			</div>
-		</div>
-		<div class="help-container" ng-repeat="rec in help.ans_record">
-			<div class="row">
-				<h4>{! rec.content !}</h4>
-			</div>
-			<div class="row">
-				<div class="col-xs-6 pull-right">
-					<span><i class="fa fa-user"></i> {! rec.student.first_name !} {! rec.student.last_name !}</span>
+
+				<div class="row">
+					<div class="col-xs-6" ng-if="help.record.subject_area_name && help.record.subject_name">
+						<p>{! help.record.subject_area_name !} > {! help.record.subject_name !}</p>
+					</div>
+					<div class="col-xs-6">
+						<span><i class="fa fa-user"></i> By : {! help.record.name !}</span>
+					</div>
+				</div>
+
+				<div class="row">
+					<hr/>
+					<p>{! help.record.content !}</p>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xs-6 pull-right">
-					<span ng-repeat="i in help.details.stars track by $index">
-						<!-- <img ng-if="!answer.rating" ng-mouseover="help.changeColor($index, answer.id)" ng-src="{! (help.hovered[answer.id][$index])  && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" /> -->
-						<!-- <img ng-if="answer.rating" ng-src="{! $index + 1 <= answer.rating && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" /> -->
-						<img ng-src="{! $index+1 <= rec.rating && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" />
-					</span>
+			<div class="clearfix"></div>
+			<div class="help-container" ng-repeat="rec in help.ans_record">
+				<div class="row">
+					<h4>{! rec.content !}</h4>
 				</div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-		<div class="col-xs-12">
-			{!! Form::open(['class' => 'form-horizontal']) !!}
-			{!! Form::textarea('content', ''
-                , array(
-                    'class' => 'form-control toggle-input'
-                    , 'placeholder' => 'Help Answer' 
-                    , 'ng-model' => 'help.answer.content'
-                    , 'autocomplete' => 'off'
-                    , 'rows' => 6)
-            ) !!}
-		</div>
-		<div class="row">
-			<div class="col-xs-8 pull-right">
-				<div class="btn-container">
-					{!! Form::button('Submit Answer'
-						, array(
-						  'id' => 'validate_code_btn'
-						  , 'class' => 'btn btn-maroon btn-medium'
-						  , 'ng-click' => 'help.submitAnswer()'
-						)
-					) !!}
-					{!! Form::button('Back'
-						, array(
-						  'id' => 'validate_code_btn'
-						  , 'class' => 'btn btn-gold btn-medium'
-						  , 'ng-click' => 'help.setCurrentActive()'
-						)
-					) !!}
+				<div class="row">
+					<div class="col-xs-6 pull-right">
+						<span><i class="fa fa-user"></i> {! rec.student.first_name !} {! rec.student.last_name !}</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-6 pull-right">
+						<span ng-repeat="i in help.record.stars track by $index">
+							<!-- <img ng-if="!answer.rating" ng-mouseover="help.changeColor($index, answer.id)" ng-src="{! (help.hovered[answer.id][$index])  && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" /> -->
+							<!-- <img ng-if="answer.rating" ng-src="{! $index + 1 <= answer.rating && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" /> -->
+							<img ng-src="{! $index+1 <= rec.rating && '/images/class-student/icon-star_yellow.png' || '/images/class-student/icon-star_white.png' !}" />
+						</span>
+					</div>
 				</div>
 			</div>
 		</div>
+	</div>
+	<div class="btn-container">
+		<button type="button" class="btn btn-gold btn-medium pull-right"
+			ng-click="help.setActive()"> Back </button>
 	</div>
 </div>
