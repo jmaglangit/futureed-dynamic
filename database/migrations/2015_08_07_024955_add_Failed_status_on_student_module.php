926eaf58f6a52fb397c2dc9a5dd623ecc80d4501
jasonmag <jasonmaglangit@gmail.com>
2015-08-07 11:42:01 +0800

    added new migration

---
+++ database/migrations/2015_08_07_024955_add_Failed_status_on_student_module.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFailedStatusOnStudentModule extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::statement("ALTER TABLE student_modules CHANGE module_status module_status ENUM('Available','On Going','Completed','Failed')");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement("ALTER TABLE student_modules CHANGE module_status module_status ENUM('Available','On Going','Completed')");
+	}
+
+}
