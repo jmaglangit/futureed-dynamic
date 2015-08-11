4fc009e175caebad7b1e8b13b1e25673e9b31eba
jasonmag <jasonmaglangit@gmail.com>
2015-08-11 12:01:30 +0800

    refactor UserMiddleware

--- app/Http/Middleware/Api/UserMiddleware.php
+++ app/Http/Middleware/Api/UserMiddleware.php
@@ -1,8 +1,36 @@
 <?php namespace FutureEd\Http\Middleware\Api;
 
 use Closure;
+use FutureEd\Models\Repository\Admin\AdminRepositoryInterface;
+use FutureEd\Models\Repository\Client\ClientRepositoryInterface;
+use FutureEd\Models\Repository\Student\StudentRepositoryInterface;
+use FutureEd\Services\TokenServices;
 
 class UserMiddleware extends JWTMiddleware{
+
+	protected $student;
+
+	protected $client;
+
+	protected $admin;
+
+	protected $token;
+
+	public function __construct(
+		StudentRepositoryInterface $studentRepositoryInterface,
+		ClientRepositoryInterface $clientRepositoryInterface,
+		AdminRepositoryInterface $adminRepositoryInterface,
+		TokenServices $tokenServices
+	){
+
+		$this->student = $studentRepositoryInterface;
+
+		$this->client = $clientRepositoryInterface;
+
+		$this->admin = $adminRepositoryInterface;
+
+		$this->token = $tokenServices;
+	}
 
 	/**
 	 * Handle an incoming request.
@@ -14,11 +42,10 @@ class UserMiddleware extends JWTMiddleware{
 	 */
 	public function handle($request, Closure $next)
 	{
-//        return $next($request);
 
         $authorization  = $request->header('authorization');
 
-        $this->validateToken($authorization);
+		$this->validateToken($authorization);
 
 
         if($this->getMessageBag()){
@@ -26,38 +53,64 @@ class UserMiddleware extends JWTMiddleware{
             return $this->respondWithError($this->getMessageBag());
         }
 
-        //initialize futureed config
-        $futureed = config('futureed');
+		//TODO: CHECK user if it has right access.
 
-        //get permissions
-        $action = $request->route()->getAction();
+		//check id by user type
+		//if client and admin, check role.
+		$payload_data = $this->token->getPayloadData();
 
-        //get payload
-        $payload = $this->getPayload();
+		if($this->checkUser($payload_data['id'],$payload_data['type'],$payload_data['role'])){
 
-        // check if payload type exist in permissions else respond unauthorized.
-        if(!in_array(strtolower($payload['type']),$action['permission'],true)){
+			//Authorized access.
+			return $next($request);
+		} else {
 
-            return $this->respondErrorMessage(2032);
-        }
+			//unauthorize access.
+			return $this->respondErrorMessage(2032);
+		}
 
-        //get role
-        // if payload type is client get role
-        // compare payload role if exist in route role else respond unauthorized.
-        if($payload['type'] == $futureed['client'] && !in_array(strtolower($payload['role']),$action['role'])){
+	}
 
-            return $this->respondErrorMessage(2032);
-        }
+	/**
+	 * Check User if has required access level.
+	 * @param $id
+	 * @param $user_type
+	 * @param $user_role
+	 * @return bool
+	 */
+	public function checkUser($id, $user_type, $user_role){
 
-        //if payload type is admin get role
-        if($payload['type'] == $futureed['admin'] && !in_array(strtolower($payload['role']),$action['role'])){
+		switch($user_type){
 
-            return $this->respondErrorMessage(2032);
-        }
+			case config('futureed.student'):
 
+				$response = $this->student->getStudent($id);
 
-        return $next($request);
+				$return = ($response <> null) ? true : false;
 
+				break;
+
+			case config('futureed.client'):
+
+				$response = $this->client->getClient($id,$user_role);
+
+				$return = ($response <> null) ? true : false;
+				break;
+
+			case config('futureed.admin'):
+
+				$response = $this->admin->getAdmin($id,$user_role);
+
+				$return = ($response <> null) ? true : false;
+				break;
+
+			default:
+
+				$return = false;
+				break;
+		}
+
+		return $return;
 	}
 
 
