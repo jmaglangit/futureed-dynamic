2ea8cc5a001367fb186e05ec4b3f995489bc085a
jasonmag <jasonmaglangit@gmail.com>
2015-08-12 16:56:11 +0800

    added auth on student point

--- app/Http/Routes/Api/StudentPoint.php
+++ app/Http/Routes/Api/StudentPoint.php
@@ -1,9 +1,17 @@
 <?php
 
-Routes::resource('/student-point', 'Api\v1\StudentPointController',
-	['except' => ['create', 'edit']]);
+Routes::group([
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['student','admin'],
+	'role' => ['admin','super admin']
+],function(){
+
+	Routes::resource('/student-point', 'Api\v1\StudentPointController',
+		['except' => ['create', 'edit']]);
 
 
-Routes::resource('admin/student/point', 'Api\v1\AdminStudentPointController',
-	['except' => ['create', 'edit']]);
+	Routes::resource('admin/student/point', 'Api\v1\AdminStudentPointController',
+		['except' => ['create', 'edit']]);
+});
+
 
