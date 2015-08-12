c52b0a3c394fe07db510bce7eaa69bed9afdd03f
jasonmag <jasonmaglangit@gmail.com>
2015-08-12 18:05:31 +0800

    added auth to assess

--- app/Http/Routes/Api/Assess.php
+++ app/Http/Routes/Api/Assess.php
@@ -5,6 +5,10 @@ Routes::group(['prefix' => '/assess'], function()
 
 	Routes::get('/get-test',[
         'uses' => 'Api\v1\AssessController@getTest',
-        'as' => 'api.v1.assess.get-test']);
+        'as' => 'api.v1.assess.get-test',
+		'middleware' => ['api_user','api_after'],
+		'permission' => ['student','admin'],
+		'role' => ['admin','super admin']
+	]);
     
 });
