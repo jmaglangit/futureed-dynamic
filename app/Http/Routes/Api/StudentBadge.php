5f845485e5b1de91e912856d250fb37b77bb95ca
jasonmag <jasonmaglangit@gmail.com>
2015-08-12 13:07:27 +0800

    added authorization on badge.

--- app/Http/Routes/Api/StudentBadge.php
+++ app/Http/Routes/Api/StudentBadge.php
@@ -2,10 +2,32 @@
 
 Routes::group(['prefix' => '/badge'], function() {
 
-	Routes::resource('/student', 'Api\v1\StudentBadgeController',
-		['except' => ['create', 'edit']]);
+	/**
+	 * Student Access
+	 */
+	Routes::group([
+		'middleware' => ['api_user','api_after'],
+		'permission' => ['student']
+	],function(){
 
-	Routes::resource('/admin', 'Api\v1\BadgeController',
-		['except' => ['create', 'edit']]);
+		Routes::resource('/student', 'Api\v1\StudentBadgeController',
+			['except' => ['create', 'edit']]);
+	});
+
+	/**
+	 * Admin Access
+	 */
+	Routes::group([
+		'middleware' => ['api_user','api_after'],
+		'permission' => ['admin'],
+		'role' => ['admin','super_admin']
+
+	],function(){
+
+		Routes::resource('/admin', 'Api\v1\BadgeController',
+			['except' => ['create', 'edit']]);
+	});
+
+
 
 });
