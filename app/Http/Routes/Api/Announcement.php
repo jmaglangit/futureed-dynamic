6292bda8f9911ca07ef4f37bfe8c9445b4a687e2
jasonmag <jasonmaglangit@gmail.com>
2015-08-13 11:22:49 +0800

    added auth on store announcement

--- app/Http/Routes/Api/Announcement.php
+++ app/Http/Routes/Api/Announcement.php
@@ -1,2 +1,10 @@
 <?php 
 Routes::resource('/announcement','Api\v1\AnnouncementController', ['only' => ['index','store']]);
+
+Routes::post('/announcement',[
+	'uses' => 'Api\v1\AnnouncementController@store',
+	'as' => 'api.v1.announcement.store',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin'],
+	'role' => ['admin','super admin']
+]);
