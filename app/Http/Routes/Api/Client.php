0dcf26ff875f068a4c4a67a6d297f1eb3df0eb1f
jayson1234 <jsuizo@nerubia.com>
2015-08-13 14:51:38 +0800

    client.php update space

--- app/Http/Routes/Api/Client.php
+++ app/Http/Routes/Api/Client.php
@@ -5,50 +5,50 @@ Routes::group(['prefix' => '/client'], function()
     /**
      * Authenticated client routes
      */
-    Routes::group([
-        'middleware' => ['api_user','api_after'],
-        'permission' => ['admin','client'],
-        'role' => ['principal','teacher','parent','admin','super admin'],
-    ], function(){
+	Routes::group([
+		'middleware' => ['api_user', 'api_after'],
+		'permission' => ['admin', 'client'],
+		'role' => ['principal', 'teacher', 'parent', 'admin', 'super admin'],
+	], function () {
 
-        /**
-         * Parent resource
-         */
-        Routes::resource('/parent','Api\v1\ClientParentController',
-            ['except' => ['create','edit']]);
+		/**
+		 * Parent resource
+		 */
+		Routes::resource('/parent', 'Api\v1\ClientParentController',
+			['except' => ['create', 'edit']]);
 
-        Routes::get('/parent/student/list/{id}',[
-            'uses' => 'Api\v1\ClientParentController@getStudentList',
-            'as' => 'api.v1.client.parent.student.list'
-        ]);
+		Routes::get('/parent/student/list/{id}', [
+			'uses' => 'Api\v1\ClientParentController@getStudentList',
+			'as' => 'api.v1.client.parent.student.list'
+		]);
 
-        /**
-         * Principal resource
-         */
-        Routes::resource('/principal','Api\v1\ClientPrincipalController',
-            ['except' => ['create','edit']]);
+		/**
+		 * Principal resource
+		 */
+		Routes::resource('/principal', 'Api\v1\ClientPrincipalController',
+			['except' => ['create', 'edit']]);
 
-        /**
-         * Teacher resource
-         */
-        Routes::resource('/teacher','Api\v1\ClientTeacherController',
-            ['except' => ['create','edit']]);
+		/**
+		 * Teacher resource
+		 */
+		Routes::resource('/teacher', 'Api\v1\ClientTeacherController',
+			['except' => ['create', 'edit']]);
 
-        Routes::post('/change-password/{id}',[
-            'uses' => 'Api\v1\ClientPasswordController@changePassword',
-            'as' => 'api.v1.client.change-password'
-        ]);
+		Routes::post('/change-password/{id}', [
+			'uses' => 'Api\v1\ClientPasswordController@changePassword',
+			'as' => 'api.v1.client.change-password'
+		]);
 
-        Routes::post('/new-password/{id}',[
-            'uses' => 'Api\v1\ClientPasswordController@setPassword',
-            'as' => 'api.v1.client.new-password'
-        ]);
+		Routes::post('/new-password/{id}', [
+			'uses' => 'Api\v1\ClientPasswordController@setPassword',
+			'as' => 'api.v1.client.new-password'
+		]);
 
-        Routes::post('/change-email/{id}',[
-            'uses' => 'Api\v1\EmailController@updateClientEmail',
-            'as' => 'api.v1.client.change-email'
-        ]);
-    });
+		Routes::post('/change-email/{id}', [
+			'uses' => 'Api\v1\EmailController@updateClientEmail',
+			'as' => 'api.v1.client.change-email'
+		]);
+	});
 
 	Routes::group([
 		'middleware' => ['api_after_client_login'],
@@ -68,31 +68,31 @@ Routes::group(['prefix' => '/client'], function()
 		]);
 	});
 
-	//NOTE: Token insert if login success.
-    /**
-     * Client login
-     */
-    Routes::post('/login',[
-        'uses' => 'Api\v1\ClientLoginController@login',
-        'as' => 'api.v1.client.login',
+//NOTE: Token insert if login success.
+	/**
+	 * Client login
+	 */
+	Routes::post('/login', [
+		'uses' => 'Api\v1\ClientLoginController@login',
+		'as' => 'api.v1.client.login',
 		'middleware' => ['api_after_client_login']
-    ]);
+	]);
 
-    Routes::post('/register',[
-        'uses' => 'Api\v1\ClientRegisterController@register',
-        'as' => 'api.v1.client.register',
-        'middleware' => ['api_after_client_login']
-    ]);
+	Routes::post('/register', [
+		'uses' => 'Api\v1\ClientRegisterController@register',
+		'as' => 'api.v1.client.register',
+		'middleware' => ['api_after_client_login']
+	]);
 
-    Routes::post('/reset-password/{id}',[
-        'uses' => 'Api\v1\ClientPasswordController@resetPassword',
-        'as' => 'api.v1.client.reset-password',
-        'middleware' => ['api_after_client_login']
-    ]);
+	Routes::post('/reset-password/{id}', [
+		'uses' => 'Api\v1\ClientPasswordController@resetPassword',
+		'as' => 'api.v1.client.reset-password',
+		'middleware' => ['api_after_client_login']
+	]);
 
-    /**
-     * Change client email
-     */
+	/**
+	 * Change client email
+	 */
 	Routes::group([
 		'middleware' => ['api_user', 'api_after'],
 		'permission' => ['admin', 'client', 'student'],
@@ -111,39 +111,39 @@ Routes::group(['prefix' => '/client'], function()
 		]);
 	});
 
-    /**
-     * Authenticated routes of the client for admin access only.
-     */
-    Routes::group([
-        'middleware' => ['api_user','api_after'],
-        'permission' => ['admin'],
-        'role' => ['admin','super admin']
-    ], function(){
+	/**
+	 * Authenticated routes of the client for admin access only.
+	 */
+	Routes::group([
+		'middleware' => ['api_user', 'api_after'],
+		'permission' => ['admin'],
+		'role' => ['admin', 'super admin']
+	], function () {
 
-        /**
-         * Client verification through admin
-         */
-        Routes::post('/verify-client/{id}',[
-            'uses' => 'Api\v1\EmailController@verifyClient',
-            'as' => 'api.v1.client.verify-client'
-        ]);
+		/**
+		 * Client verification through admin
+		 */
+		Routes::post('/verify-client/{id}', [
+			'uses' => 'Api\v1\EmailController@verifyClient',
+			'as' => 'api.v1.client.verify-client'
+		]);
 
-        /**
-         * Reject a client
-         */
-        Routes::post('/reject-client/{id}',[
-            'uses' => 'Api\v1\EmailController@rejectClient',
-            'as' => 'api.v1.client.reject-client'
-        ]);
+		/**
+		 * Reject a client
+		 */
+		Routes::post('/reject-client/{id}', [
+			'uses' => 'Api\v1\EmailController@rejectClient',
+			'as' => 'api.v1.client.reject-client'
+		]);
 
-        /**
-         * Change status of the client
-         */
-        Routes::post('/change-status/{id}',[
-            'uses' => 'Api\v1\AdminClientController@setClientStatus',
-            'as' => 'api.v1.client.change-status'
-        ]);
-    });
+		/**
+		 * Change status of the client
+		 */
+		Routes::post('/change-status/{id}', [
+			'uses' => 'Api\v1\AdminClientController@setClientStatus',
+			'as' => 'api.v1.client.change-status'
+		]);
+	});
 
 });
 
