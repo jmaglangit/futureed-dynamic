7c9bd9d2c6273a5a796e379c36b276d00c498af8
jasonmag <jasonmaglangit@gmail.com>
2015-08-14 14:46:18 +0800

    added auth

--- app/Http/Routes/Api/Payment.php
+++ app/Http/Routes/Api/Payment.php
@@ -1,11 +1,20 @@
 <?php
-Routes::post('payment', array(
-    'as' => 'payment',
-    'uses' => 'Api\v1\PaymentController@postPayment',
-));
+
+Routes::group([
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+],function(){
+
+	Routes::post('payment', array(
+		'as' => 'payment',
+		'uses' => 'Api\v1\PaymentController@postPayment',
+	));
 
 // this is after make the payment, PayPal redirect back to your site
-Routes::get('payment/status', array(
-    'as' => 'payment.status',
-    'uses' => 'Api\v1\PaymentController@getPaymentStatus',
-));
+	Routes::get('payment/status', array(
+		'as' => 'payment.status',
+		'uses' => 'Api\v1\PaymentController@getPaymentStatus',
+	));
+});
+
