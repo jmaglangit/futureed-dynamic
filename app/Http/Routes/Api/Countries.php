676f4fc8dd87d3c087b39fe20f58c49d9ed126d3
jasonmag <jasonmaglangit@gmail.com>
2015-08-14 10:59:41 +0800

    added auth; updated routes

--- app/Http/Routes/Api/Countries.php
+++ app/Http/Routes/Api/Countries.php
@@ -1,5 +1,22 @@
 <?php
 
-//countries
-Routes::resource('/countries','Api\v1\CountryController',
-    ['except' => ['create','edit']]);
+/**
+ * Get basic country information.
+ */
+Routes::group([
+	'prefix' => '/countries',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+],function(){
+
+	Routes::get('/',[
+		'uses' => 'Api\v1\CountryController@index',
+		'as' => 'api.v1.countries.index'
+	]);
+
+	Routes::get('/{id}',[
+		'uses' => 'Api\v1\CountryController@show',
+		'as' => 'api.v1.countries.show'
+	]);
+});
