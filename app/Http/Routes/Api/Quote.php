ed74b59c101e11ef306c9054dd87df50c0df223e
jasonmag <jasonmaglangit@gmail.com>
2015-08-14 15:03:50 +0800

    updated quote route and auth

--- app/Http/Routes/Api/Quote.php
+++ app/Http/Routes/Api/Quote.php
@@ -1,4 +1,9 @@
 <?php
 
-Routes::resource('/quote', 'Api\v1\QuoteController',
-	['only' => ['index']]);
+Routes::get('/quote',[
+	'uses' => 'Api\v1\QuoteController@index',
+	'as' => 'api.v1.quote.index',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+]);
