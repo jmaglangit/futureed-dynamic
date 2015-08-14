026c5e32f94ba22c7912d10cb4fab41bfb24bdf9
jasonmag <jasonmaglangit@gmail.com>
2015-08-14 11:06:46 +0800

    updated event routes and auth

--- app/Http/Routes/Api/Event.php
+++ app/Http/Routes/Api/Event.php
@@ -1,4 +1,9 @@
 <?php
 
-Routes::resource('/event','Api\v1\EventController',
-	['except' => ['create','edit']]);
+Routes::get('/event',[
+	'uses' => 'Api\v1\EventController@index',
+	'as' => 'api.v1.event.index',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+]);
