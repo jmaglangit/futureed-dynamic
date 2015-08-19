d339482a8ed9aa034ce7111596a6bf13be34c12a
jasonmag <jasonmaglangit@gmail.com>
2015-08-19 15:10:36 +0800

    added subject_id filter

--- app/Http/Requests/Api/StudentModuleRequest.php
+++ app/Http/Requests/Api/StudentModuleRequest.php
@@ -28,9 +28,12 @@ class StudentModuleRequest extends ApiRequest{
                 ];
                 break;
             default:
-                return ['class_id' => 'required|integer',
+                return [
+					'class_id' => 'required|integer',
                     'student_id' => 'required|integer',
-                    'module_id' => 'required|integer'];
+                    'module_id' => 'required|integer',
+					'subject_id' => 'required|integer'
+				];
         }
 
     }
@@ -44,6 +47,8 @@ class StudentModuleRequest extends ApiRequest{
             'student_id.integer' => 'Student must be a number.',
             'module_id.required' => 'Module is required.',
             'module_id.integer' => 'Module must be a number.',
+			'subject_id.required' => 'Subject is required.',
+			'subject_id.integer' => 'Subject must be a number.',
             'last_viewed_content_id.integer' => 'Content is invalid.',
             'last_answered_question_id.integer' => 'Question is invalid.',
         ];
