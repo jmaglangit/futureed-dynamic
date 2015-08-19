f9cb525c22824ee2d544e98e19361ad84fad312b
jasonmag <jasonmaglangit@gmail.com>
2015-08-19 15:47:45 +0800

    updated indentation

--- app/Services/StudentServices.php
+++ app/Services/StudentServices.php
@@ -21,147 +21,157 @@ use FutureEd\Models\Repository\Validator\ValidatorRepository;
 use FutureEd\Services\SchoolServices;
 use FutureEd\Services\AvatarServices;
 
-class StudentServices {
+class StudentServices
+{
 
-    public function __construct(
-            StudentRepositoryInterface $student,
-            PasswordImageServices $password,
-            UserServices $user,
-            ValidatorRepository $validator,
-            SchoolServices $school,
-            AvatarServices $avatar,
-            GradeRepositoryInterface $gradeRepositoryInterface,
-			ClassStudentRepositoryInterface $classStudentRepositoryInterface,
-			StudentModuleRepositoryInterface $studentModuleRepositoryInterface
-            ){
-        $this->student = $student;
-        $this->password = $password;
-        $this->user = $user;
-        $this->validator = $validator;
-        $this->school = $school;
-        $this->avatar = $avatar;
-        $this->grade = $gradeRepositoryInterface;
+	public function __construct(
+		StudentRepositoryInterface $student,
+		PasswordImageServices $password,
+		UserServices $user,
+		ValidatorRepository $validator,
+		SchoolServices $school,
+		AvatarServices $avatar,
+		GradeRepositoryInterface $gradeRepositoryInterface,
+		ClassStudentRepositoryInterface $classStudentRepositoryInterface,
+		StudentModuleRepositoryInterface $studentModuleRepositoryInterface
+	)
+	{
+		$this->student = $student;
+		$this->password = $password;
+		$this->user = $user;
+		$this->validator = $validator;
+		$this->school = $school;
+		$this->avatar = $avatar;
+		$this->grade = $gradeRepositoryInterface;
 		$this->class_student = $classStudentRepositoryInterface;
 		$this->student_module = $studentModuleRepositoryInterface;
-        }
+	}
 
-    public function getStudents($criteria , $limit , $offset ){
+	public function getStudents($criteria, $limit, $offset)
+	{
 
-        return $this->student->getStudents($criteria , $limit , $offset);
-    }
+		return $this->student->getStudents($criteria, $limit, $offset);
+	}
 
-    public function getStudent($id){
-        return $this->student->getStudent($id);
-    }
+	public function getStudent($id)
+	{
+		return $this->student->getStudent($id);
+	}
 
-    /*
-     * @desc Add new student
-     *  'first_name',
-            'last_name',
-            'gender',
-            'birthday',
-            'school_code',
-            'grade_code',
-            'country',
-            'state',
-            'city'
-     */
-    //TODO: Add more validations on the each data variables.
-    public function addStudent($student){
+	/*
+	 * @desc Add new student
+	 *  'first_name',
+			'last_name',
+			'gender',
+			'birthday',
+			'school_code',
+			'grade_code',
+			'country',
+			'state',
+			'city'
+	 */
+	//TODO: Add more validations on the each data variables.
+	public function addStudent($student)
+	{
 
-        $return = [];
+		$return = [];
 
-        if(empty(array_filter($return))){
-            //if existing add student
-            $student_response = $this->student->addStudent($student);
+		if (empty(array_filter($return))) {
+			//if existing add student
+			$student_response = $this->student->addStudent($student);
 
-            // if not add user and add student
+			// if not add user and add student
 
-            return [
-                'status' => 200,
-                'message' => $student_response
-            ];
-        }
+			return [
+				'status' => 200,
+				'message' => $student_response
+			];
+		}
 
-        return $return;
-    }
+		return $return;
+	}
 
-    public function updateStudent($student){
-        $this->student->updateStudent($student);
-    }
+	public function updateStudent($student)
+	{
+		$this->student->updateStudent($student);
+	}
 
-    public function deleteStudent($id){
-        $this->student->deleteStudent($id);
-    }
+	public function deleteStudent($id)
+	{
+		$this->student->deleteStudent($id);
+	}
 
-    public function getImagePassword($id){
+	public function getImagePassword($id)
+	{
 
-        //TODO: get image password.
-        $imgId= $this->student->getImagePassword($id);
-        if(is_null($imgId)){
+		//TODO: get image password.
+		$imgId = $this->student->getImagePassword($id);
+		if (is_null($imgId)) {
 
-            return false;
-        }
-        //mix password id with selections
-        $mix = $this->password->getMixImage($imgId);
-
-
-        shuffle($mix);
-
-        return [
-            'status' => 200,
-            'data' => $mix
-        ];
-    }
-
-    /*
-     * @param id
-     * @param image_id
-     * @param token
-     * @desc id is the student id, image_id is the image password, token is optional if it has a token.
-     * token is the indicator if it has login.
-     */
-    public function checkAccess($id,$image_id, $token = null){
-
-        $user_id = $this->student->getReferences($id);
-        $is_disabled =  $this->user->checkUserDisabled($user_id['user_id']);
-
-        if(!$is_disabled){
-            $password_image = $this->student->getImagePassword($id);
-
-            if($image_id == $password_image){
-                $this->user->resetLoginAttempt($user_id['user_id']);
-                return [
-                    'status' => 200,
-                    'data' => true
-                ];
-            } else {
-
-                //check if token has values.
-                if(!$token){
-
-                    $this->user->addLoginAttempt($user_id['user_id']);
-                }
-
-                if(!$this->user->exceedLoginAttempts($user_id['user_id'])){
-                    $this->user->lockAccount($user_id['user_id']);
-                    return [
-                        'status' => $this->user->checkUserDisabled($user_id['user_id'])
-                    ];
-                }
-                return [
-                    'status' => 2012
-                ];
-            }
-        } else {
-            return [
-                'status' => $is_disabled
-            ];
-        }
-    }
+			return false;
+		}
+		//mix password id with selections
+		$mix = $this->password->getMixImage($imgId);
 
 
-    public function getStudentDetails($id){
+		shuffle($mix);
+
+		return [
+			'status' => 200,
+			'data' => $mix
+		];
+	}
+
+	/*
+	 * @param id
+	 * @param image_id
+	 * @param token
+	 * @desc id is the student id, image_id is the image password, token is optional if it has a token.
+	 * token is the indicator if it has login.
+	 */
+	public function checkAccess($id, $image_id, $token = null)
+	{
+
+		$user_id = $this->student->getReferences($id);
+		$is_disabled = $this->user->checkUserDisabled($user_id['user_id']);
+
+		if (!$is_disabled) {
+			$password_image = $this->student->getImagePassword($id);
+
+			if ($image_id == $password_image) {
+				$this->user->resetLoginAttempt($user_id['user_id']);
+				return [
+					'status' => 200,
+					'data' => true
+				];
+			} else {
+
+				//check if token has values.
+				if (!$token) {
+
+					$this->user->addLoginAttempt($user_id['user_id']);
+				}
+
+				if (!$this->user->exceedLoginAttempts($user_id['user_id'])) {
+					$this->user->lockAccount($user_id['user_id']);
+					return [
+						'status' => $this->user->checkUserDisabled($user_id['user_id'])
+					];
+				}
+				return [
+					'status' => 2012
+				];
+			}
+		} else {
+			return [
+				'status' => $is_disabled
+			];
+		}
+	}
+
+
+	public function getStudentDetails($id)
+	{
 
 		$student = $this->getStudent($id)->toArray();
 		$student_reference = $this->student->getReferences($id)->toArray();
@@ -175,7 +185,6 @@ class StudentServices {
 		$avatar_url = '';
 		$avatar_url_background = '';
 		$avatar_thumbnail_url = '';
-
 
 
 		if ($student_reference['avatar_id']) {
@@ -198,11 +207,9 @@ class StudentServices {
 		}
 
 
-
-
 		$student = array_merge(array('id' => $id
-            ,'class_id' => $this->getCurrentClass($id)
-			,'class' => $this->getCurrentClass($id))
+			, 'class_id' => $this->getCurrentClass($id)
+			, 'class' => $this->getCurrentClass($id))
 			, $student
 			, $user,
 			array('age' => $age,
@@ -221,126 +228,139 @@ class StudentServices {
 		}
 		return $studentdetails;
 
-    }
-    
-    //get age
-    public function age($birth_date){
-         $interval = date_diff(date_create(),date_create($birth_date));
-         return $interval->format("%Y");
-    }
+	}
 
-    //get student birth_date
-    public function getAge($id){
+	//get age
+	public function age($birth_date)
+	{
+		$interval = date_diff(date_create(), date_create($birth_date));
+		return $interval->format("%Y");
+	}
 
-        $student = $this->student->getStudent($id);
+	//get student birth_date
+	public function getAge($id)
+	{
 
-        $age = Carbon::now();
+		$student = $this->student->getStudent($id);
 
-        return $age->diffInYears(Carbon::parse($student['birth_date']));
+		$age = Carbon::now();
 
-    }
-    
-    //udpate student_image_password
-    public function resetPasswordImage($data){
-        
-        $this->student->updateImagePassword($data);
-        $return = ['status'=>200,
-                    'data' =>$data['id']];
-        return $return;
-    }
+		return $age->diffInYears(Carbon::parse($student['birth_date']));
 
+	}
 
-    public function getStudentByParent($parent_id){
-        return $this->student->getStudentParent($parent_id);
-    }
-    
-    //save student avatar
-    public function saveStudentAvatar($data){
-        
-        return $this->student->saveStudentAvatar($data);
-        
-    }
-    
-    //validation email/username/firstname/lastname/gender
-    public function validateStudentDetails($input){
-        
-        if(!$this->validator->gender($input['gender'])){
-            
-            return['status'=>202,
-                    'data'=>'invalid gender'];
-        }else{
-           
-            if(!$this->validator->email($input['email'])){
-               
-                return['status'=>202,
-                    'data'=>'invalid email'];
-            }else{
-               
-                if(!$this->validator->username($input['username'])){
-                     
-                     return['status'=>202,
-                    'data'=>'invalid username'];  
-                }else{
-                    
-                    return['status'=>200];
-                }
-                
-            }
-            
-        }
-    }
-    
-    public function updateStudentDetails($id,$input){
-        
-        $student_reference = $this->student->getReferences($id)->toArray();
-        
-        //update user username and email
-        $this->user->updateUsernameEmail($student_reference['user_id'],$input);
-        
-        //update Student details
-        $this->student->updateStudentDetails($id,$input);
-    }
-    
-    
-     //format return for student reset code
-    public function resetCodeResponse($user_id){
-        $id = $this->student->getStudentId($user_id);
-        $return=['id'=>$id,
-                 'user_type'=>'Student',
-                ];
-        return $return;
-    }
+	//udpate student_image_password
+	public function resetPasswordImage($data)
+	{
+
+		$this->student->updateImagePassword($data);
+		$return = ['status' => 200,
+			'data' => $data['id']];
+		return $return;
+	}
 
 
-    //get student references
-    public function getStudentReferences($id){
+	public function getStudentByParent($parent_id)
+	{
+		return $this->student->getStudentParent($parent_id);
+	}
 
-        return $this->student->getReferences($id);
-    }
+	//save student avatar
+	public function saveStudentAvatar($data)
+	{
+
+		return $this->student->saveStudentAvatar($data);
+
+	}
+
+	//validation email/username/firstname/lastname/gender
+	public function validateStudentDetails($input)
+	{
+
+		if (!$this->validator->gender($input['gender'])) {
+
+			return ['status' => 202,
+				'data' => 'invalid gender'];
+		} else {
+
+			if (!$this->validator->email($input['email'])) {
+
+				return ['status' => 202,
+					'data' => 'invalid email'];
+			} else {
+
+				if (!$this->validator->username($input['username'])) {
+
+					return ['status' => 202,
+						'data' => 'invalid username'];
+				} else {
+
+					return ['status' => 200];
+				}
+
+			}
+
+		}
+	}
+
+	public function updateStudentDetails($id, $input)
+	{
+
+		$student_reference = $this->student->getReferences($id)->toArray();
+
+		//update user username and email
+		$this->user->updateUsernameEmail($student_reference['user_id'], $input);
+
+		//update Student details
+		$this->student->updateStudentDetails($id, $input);
+	}
 
 
-    //get student id
-    public function getStudentId($user_id){
+	//format return for student reset code
+	public function resetCodeResponse($user_id)
+	{
+		$id = $this->student->getStudentId($user_id);
+		$return = ['id' => $id,
+			'user_type' => 'Student',
+		];
+		return $return;
+	}
 
-        return $this->student->getStudentId($user_id);
-    }
-    
-    //change password images
-    public function changePasswordImage($id,$password_image_id){
-        
-        $this->student->changePasswordImage($id,$password_image_id);
-        
-    }
-    
-    //check if id exist
-    public function checkIdExist($id){
-        
-        return $this->student->checkIdExist($id);
-    }
 
-	public function updateSchool($id,$school_code){
+	//get student references
+	public function getStudentReferences($id)
+	{
 
-		return $this->student->updateSchool($id,$school_code);
+		return $this->student->getReferences($id);
+	}
+
+
+	//get student id
+	public function getStudentId($user_id)
+	{
+
+		return $this->student->getStudentId($user_id);
+	}
+
+	//change password images
+	public function changePasswordImage($id, $password_image_id)
+	{
+
+		$this->student->changePasswordImage($id, $password_image_id);
+
+	}
+
+	//check if id exist
+	public function checkIdExist($id)
+	{
+
+		return $this->student->checkIdExist($id);
+	}
+
+	public function updateSchool($id, $school_code)
+	{
+
+		return $this->student->updateSchool($id, $school_code);
 	}
 
 	/**
@@ -356,7 +376,6 @@ class StudentServices {
 
 		//mitigate to inactive
 		foreach ($active_class as $list => $class) {
-
 
 
 			if (!(Carbon::now()->between(
@@ -387,19 +406,16 @@ class StudentServices {
 		$current_class = $this->class_student->getStudentCurrentClassroom($student_id);
 
 		$class_id = [];
-		foreach($current_class as $class){
+		foreach ($current_class as $class) {
 
-			array_push($class_id,$class->class_id);
+			array_push($class_id, $class->class_id);
 		}
 
 		$class_student = $this->student_module->getStudentModuleByClass($student_id, $class_id);
-
 
 
 		return ($class_student) ? $class_student : null;
 	}
 
 
-    
-    
 }
