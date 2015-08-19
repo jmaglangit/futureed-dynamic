c9f2e8857697e94d5c5256f866c2856cb06f2cb9
jayson1234 <jsuizo@nerubia.com>
2015-08-19 10:21:11 +0800

    1238 update space

--- app/Http/Controllers/Api/v1/ParentStudentController.php
+++ app/Http/Controllers/Api/v1/ParentStudentController.php
@@ -408,7 +408,7 @@ class ParentStudentController extends ApiController {
 
         $parent_id = $request->only('parent_id');
 
-		$subject_id = $request->only('subject_id');
+        $subject_id = $request->only('subject_id');
 
         //check if user is associated to the parent.
        $check_parent_student = $this->parent_student->checkParentStudent($parent_id,$student_id);
@@ -417,13 +417,13 @@ class ParentStudentController extends ApiController {
             return $this->respondErrorMessage(2039);
         }
 
-		// check if student have current subscription of a subject
-		$class_student_subject = $this->classroom->getClassroomBySubjectId($subject_id, $student_id);
+        // check if student have current subscription of a subject
+        $class_student_subject = $this->classroom->getClassroomBySubjectId($subject_id, $student_id);
 
-		if($class_student_subject){
+        if($class_student_subject){
 
-			return $this->respondErrorMessage(2037);
-		}
+           return $this->respondErrorMessage(2037);
+        }
 
         $order_id = $request->only('order_id');
         $check_order_detail = $this->order_details->getOrderDetailByOrderIdAndStudentId($order_id['order_id'],$student_id);
