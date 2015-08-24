64aaf95983e4bcf8b09c95f25b68f79b02b1af7a
Francis Menguito <francis@nerubia.com>
2015-08-24 17:47:52 +0800

    fix indention

--- app/Http/Routes/Api/Assess.php
+++ app/Http/Routes/Api/Assess.php
@@ -1,13 +1,13 @@
 <?php 
 #TODO: Add Middleware api_user 
-Routes::group(['prefix' => '/assess'], function()
-{
+Routes::group(['prefix' => '/assess'], function() {
+
 	Routes::get('/get-test',[
 		'uses' => 'Api\v1\AssessController@getTest',
 		'as' => 'api.v1.assess.get-test']);
-        
-    Routes::post('/save-test',[
-    	'uses' => 'Api\v1\AssessController@saveTest',
-    	'as' => 'api.v1.assess.save-test']);
-    
+		
+	Routes::post('/save-test',[
+		'uses' => 'Api\v1\AssessController@saveTest',
+		'as' => 'api.v1.assess.save-test']);
+		
 });
--- app/Models/Core/StudentLsAnswer.php
+++ app/Models/Core/StudentLsAnswer.php
@@ -9,13 +9,13 @@ class StudentLsAnswer extends Model {
 
 	use SoftDeletes;
 	use TransactionTrait;
-
+	
 	protected $table = 'student_ls_answers';
-
+	
 	protected $dates = ['created_at', 'updated_at', 'deleted_at'];
-
+	
 	protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
-
+	
 	protected $fillable = [
 		'student_id', 
 		'ls_test_id', 
@@ -28,7 +28,7 @@ class StudentLsAnswer extends Model {
 		'ls_answer_text',
 		'ls_raw_score',
 	];
-
+	
 	protected $attributes = [
 		'created_by' => 1,
 		'updated_by' => 1
@@ -37,16 +37,16 @@ class StudentLsAnswer extends Model {
 	
 	//-- Scopes
 	public function scopeStudent_id($query, $student_id) {
-        return $query->where('student_id', '=', $student_id);
-    }
-    
-    public function scopeTest_id($query, $ls_test_id) {
-        return $query->where('ls_test_id', '=', $ls_test_id);
-    }
-    
-    public function scopeTest_question_id($query, $ls_test_question_id) {
-        return $query->whereLsTestQuestionId($ls_test_question_id);
-    }
+		return $query->where('student_id', '=', $student_id);
+	}
+	
+	public function scopeTest_id($query, $ls_test_id) {
+		return $query->where('ls_test_id', '=', $ls_test_id);
+	}
+	
+	public function scopeTest_question_id($query, $ls_test_question_id) {
+		return $query->whereLsTestQuestionId($ls_test_question_id);
+	}
     
 }
 
