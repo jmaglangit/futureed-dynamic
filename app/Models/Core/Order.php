bff6926eaa37b41a176ba9edf037fb49789eb8fe
jasonmag <jasonmaglangit@gmail.com>
2015-09-01 11:54:56 +0800

    Updated file indentation

--- app/Models/Core/Order.php
+++ app/Models/Core/Order.php
@@ -3,28 +3,32 @@
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Order extends Model {
+class Order extends Model
+{
 
-    use SoftDeletes;
+	use SoftDeletes;
 
-    protected $table = 'orders';
+	protected $table = 'orders';
 
-    protected $dates = ['deleted_at'];
+	protected $dates = ['deleted_at'];
 
-    protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
+	protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
 
-    protected $fillable = ['order_no','order_date','client_id','subscription_id','date_start','date_end','seats_total','seats_taken','total_amount','payment_status','student_id'];
+	protected $fillable = ['order_no', 'order_date', 'client_id', 'subscription_id', 'date_start', 'date_end', 'seats_total', 'seats_taken', 'total_amount', 'payment_status', 'student_id'];
 
 	protected $attributes = [
 		'client_id' => 0,
 		'created_by' => 1,
 		'updated_by' => 1
 	];
-	public function invoice(){
-		return $this->belongsTo('FutureEd\Models\Core\Invoice','order_no','order_no')->with('invoiceDetail');
+
+	public function invoice()
+	{
+		return $this->belongsTo('FutureEd\Models\Core\Invoice', 'order_no', 'order_no')->with('invoiceDetail');
 	}
 
-    public function scopeOrderNo($query,$order_no){
-        return $query->where('order_no',$order_no);
-    }
+	public function scopeOrderNo($query, $order_no)
+	{
+		return $query->where('order_no', $order_no);
+	}
 }
