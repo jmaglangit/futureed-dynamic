5360564c4f93fba9f616e52397a1d80dde54f8bf
jasonmag <jasonmaglangit@gmail.com>
2015-09-01 11:54:42 +0800

    Updated file indentation

--- app/Models/Core/Invoice.php
+++ app/Models/Core/Invoice.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Invoice extends Model {
 
 	use SoftDeletes;
-	
-    protected $table = 'invoices';
 
-    protected $dates = ['deleted_at'];
-    
-    protected $fillable = ['order_no', 'invoice_date', 'invoice_no', 'client_id','client_name','date_start','date_end','seats_total','discount_type','discount_id','discount','total_amount','subscription_id','payment_status','student_id','student_name'];
+	protected $table = 'invoices';
 
-    protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
+	protected $dates = ['deleted_at'];
+
+	protected $fillable = ['order_no', 'invoice_date', 'invoice_no', 'client_id', 'client_name', 'date_start', 'date_end', 'seats_total', 'discount_type', 'discount_id', 'discount', 'total_amount', 'subscription_id', 'payment_status', 'student_id', 'student_name'];
+
+	protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
 
 	protected $attributes = [
 		'client_id' => 0,
@@ -23,61 +23,60 @@ class Invoice extends Model {
 		'created_by' => 1,
 		'updated_by' => 1
 	];
-    
-    //-------------relationships
-    
+
+	//-------------relationships
+
 	public function client() {
 		return $this->belongsTo('FutureEd\Models\Core\Client');
 	}
-    
-    public function subscription() {
-        return $this->belongsTo('FutureEd\Models\Core\Subscription');
-    }
-    
-    public function order(){
-        return $this->belongsTo('FutureEd\Models\Core\Order','order_no','order_no');
-    }
-    
-    public function invoiceDetail(){
-        return $this->hasMany('FutureEd\Models\Core\InvoiceDetail')->with('classroom','grade');
-    }
 
-    //------------scopes    
-    public function scopeOrder($query, $order_no) {
+	public function subscription() {
+		return $this->belongsTo('FutureEd\Models\Core\Subscription');
+	}
 
-        return $query->where('order_no', '=' , $order_no);
+	public function order() {
+		return $this->belongsTo('FutureEd\Models\Core\Order', 'order_no', 'order_no');
+	}
 
-    }
+	public function invoiceDetail() {
+		return $this->hasMany('FutureEd\Models\Core\InvoiceDetail')->with('classroom', 'grade');
+	}
 
-    public function scopeSubscription($query, $name) {
+	//------------scopes
+	public function scopeOrder($query, $order_no) {
 
-		return $query->whereHas('subscription', function($query) use ($name) {
-			$query->where('name', 'like', '%'.$name.'%');
+		return $query->where('order_no', '=', $order_no);
+
+	}
+
+	public function scopeSubscription($query, $name) {
+
+		return $query->whereHas('subscription', function ($query) use ($name) {
+			$query->where('name', 'like', '%' . $name . '%');
 		});
 
-    }
+	}
 
-    public function scopePayment($query, $payment_status) {
+	public function scopePayment($query, $payment_status) {
 
-        return $query->where('payment_status', '=' , $payment_status);
+		return $query->where('payment_status', '=', $payment_status);
 
-   }
-   
-   public function scopeClientId($query,$client_id){
-	   return $query->where('client_id', $client_id);
-   }
+	}
 
-   public function scopeId($query,$id)
-   {
+	public function scopeClientId($query, $client_id) {
+		return $query->where('client_id', $client_id);
+	}
 
-	   return $query->where('id', $id);
+	public function scopeId($query, $id) {
 
-   }
+		return $query->where('id', $id);
 
-   public function scopeStudentId($query,$student_id){
+	}
 
-	   return $query->where('student_id', $student_id);
-   }
+	public function scopeStudentId($query, $student_id) {
 
-    
+		return $query->where('student_id', $student_id);
+	}
+
+
 }
