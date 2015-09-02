04b0c5134313e2a00e97c0839555b3b0ef6ca9ac
jasonmag <jasonmaglangit@gmail.com>
2015-09-02 15:37:05 +0800

    removed studentmodules

--- app/Models/Core/Classroom.php
+++ app/Models/Core/Classroom.php
@@ -59,7 +59,7 @@ class Classroom extends Model {
 
 	public function studentSubject(){
 
-		return $this->hasMany('FutureEd\Models\Core\Subject','id','subject_id')->with('studentModules');
+		return $this->hasMany('FutureEd\Models\Core\Subject','id','subject_id');
 	}
 
 
