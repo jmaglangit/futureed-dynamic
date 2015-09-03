0b3776820fbf489e9b2f9493daf47a9628d0fe74
jasonmag <jasonmaglangit@gmail.com>
2015-09-03 15:57:16 +0800

    added attributes and trait

--- app/Models/Core/AvatarWiki.php
+++ app/Models/Core/AvatarWiki.php
@@ -1,5 +1,6 @@
 <?php namespace FutureEd\Models\Core;
 
+use FutureEd\Models\Traits\TransactionTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -7,11 +8,18 @@ class AvatarWiki extends Model {
 
 	use SoftDeletes;
 
+	use TransactionTrait;
+
 	protected $table = 'avatar_wikis';
 
 	protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
 
 	protected $fillable = ['avatar_id','code','name','description_full','description_summary','title','source'];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
 
 	//scopes
 	public function scopeAvatarId($query, $avatar_id){
