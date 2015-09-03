708b3e20750749fdf85545b0b1934d49a28b2552
jasonmag <jasonmaglangit@gmail.com>
2015-09-03 15:55:11 +0800

    Added attributes and trait.

--- app/Models/Core/AvatarPose.php
+++ app/Models/Core/AvatarPose.php
@@ -1,5 +1,6 @@
 <?php namespace FutureEd\Models\Core;
 
+use FutureEd\Models\Traits\TransactionTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -7,12 +8,18 @@ class AvatarPose extends Model {
 
     use SoftDeletes;
 
+	use TransactionTrait;
+
     protected $table = 'avatar_poses';
 
     protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
 
     protected $fillable = ['avatar_id','code','name','pose_image','description'];
-    
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
 	
 	//-----scope
 	public function scopeAvatarId($query,$avatar_id){
