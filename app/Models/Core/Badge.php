f2b6099b6e75c4e88f4759b70f0ebf5322c5f5aa
jasonmag <jasonmaglangit@gmail.com>
2015-09-03 15:58:44 +0800

    added attributes and trait

--- app/Models/Core/Badge.php
+++ app/Models/Core/Badge.php
@@ -1,5 +1,6 @@
 <?php namespace FutureEd\Models\Core;
 
+use FutureEd\Models\Traits\TransactionTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -7,11 +8,18 @@ class Badge extends Model {
 
 	use SoftDeletes;
 
+	use TransactionTrait;
+
 	protected $table = 'badges';
 
 	protected $dates = ['deleted_at'];
 
 	protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
 
 	//-------------scopes
 	public function scopeName($query, $name) {
