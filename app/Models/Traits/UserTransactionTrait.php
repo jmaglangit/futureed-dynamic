f9f26783ebf78fcdee9f1b1a32e1ece3495b4b08
jasonmag <jasonmaglangit@gmail.com>
2015-09-04 17:04:19 +0800

    removed defined trait add new user trait.

--- app/Models/Traits/UserTransactionTrait.php
+++ app/Models/Traits/UserTransactionTrait.php
@@ -1,14 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/4/15
- * Time: 4:56 PM
- */
-
-namespace FutureEd\Models\Traits;
+<?php namespace FutureEd\Models\Traits;
 
 
 trait UserTransactionTrait {
 
+	protected static function boot() {
+
+		parent::boot();
+
+		static::creating(function($model){
+
+			$model->created_by = (session()->has('current_user'))? session('current_user') : 1;
+
+		});
+
+		static::updating(function($model){
+
+			$model->updated_by = (session()->has('current_user'))? session('current_user') : 1;
+
+		});
+
+		static::deleting(function ($admin) {
+
+			$admin->user->delete();
+
+		});
+	}
 }
