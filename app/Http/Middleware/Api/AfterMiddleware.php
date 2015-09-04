62391108ae8304dc1c2a2c65562641cb7dcd0fa6
jasonmag <jasonmaglangit@gmail.com>
2015-09-04 17:07:02 +0800

    added indentation

--- app/Http/Middleware/Api/AfterMiddleware.php
+++ app/Http/Middleware/Api/AfterMiddleware.php
@@ -3,56 +3,53 @@
 use Closure;
 use Illuminate\Support\Facades\Session;
 
-class AfterMiddleware extends JWTMiddleware{
+class AfterMiddleware extends JWTMiddleware {
 
 	/**
 	 * Handle an incoming request.
 	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
 	 * @return mixed
 	 */
-	public function handle($request, Closure $next)
-	{
+	public function handle($request, Closure $next) {
 
-        $response =  $next($request);
+		$response = $next($request);
 
 		//Remove session
-		if(Session::has('current_user')){
+		if (Session::has('current_user')) {
 
 			Session::forget('current_user');
 		}
 
-        $authorization  = $request->header('authorization');
+		$authorization = $request->header('authorization');
 
 
-        //if authorization exist then extract id, type, and role
-        if($authorization){
-            $this->token->parseToken($authorization);
+		//if authorization exist then extract id, type, and role
+		if ($authorization) {
+			$this->token->parseToken($authorization);
 
-            $payload_data = $this->getPayload();
+			$payload_data = $this->getPayload();
 
 			//parse payload data ang extract id, type, and role and insert to getToken()
-            $token = $this->getToken([
-                'id' => $payload_data['id'],
-                'type' => $payload_data['type'],
-                'role' => $payload_data['role'],
-            ]);
+			$token = $this->getToken([
+				'id' => $payload_data['id'],
+				'type' => $payload_data['type'],
+				'role' => $payload_data['role'],
+			]);
 
 
-            $collection = $response->headers;
+			$collection = $response->headers;
 
-            $collection->set('authorization',$token);
+			$collection->set('authorization', $token);
 
-            $response->headers = $collection;
+			$response->headers = $collection;
 
-        }
+		}
 
 
-        return $response;
+		return $response;
 	}
-
-
 
 
 }
