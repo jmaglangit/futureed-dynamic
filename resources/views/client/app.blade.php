3dcde620a039e4843ada4592c0d9e96ed8d2fbd7
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-09-10 11:29:32 +0800

    Removed unused angular files

--- resources/views/client/app.blade.php
+++ resources/views/client/app.blade.php
@@ -96,7 +96,6 @@
 		{!! Html::script('/js/datetimepicker.js') !!}
 		{!! Html::script('/js/ui-bootstrap-tpls-0.13.0.min.js') !!}
 		{!! Html::script('/js/common/filters.js') !!}
-		{!! Html::script('/js/angular-drag-and-drop-lists.min.js')!!}
 
 		{!! Html::script('/js/ng-file-upload-shim.min.js')!!}
 		{!! Html::script('/js/ng-file-upload.min.js')!!}
