ade842c4f4f9aba2980b22a0e9eb8d8f2b0ead69
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-09-10 11:21:26 +0800

    Removed parent and principal unused invoice controllers and services

--- public/js/client/controllers/manage_principal_invoice_controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('futureed.controllers')
-	.controller('ManagePrincipalInvoiceController', ManagePrincipalInvoiceController);
-
-ManagePrincipalInvoiceController.$inject = ['$scope', 'ManagePrincipalInvoiceService', 'apiService'];
-
-function ManagePrincipalInvoiceController($scope, ManagePrincipalInvoiceService, apiService) {
-	var self = this;
-	
-}
