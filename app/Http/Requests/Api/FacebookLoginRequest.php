5db03f626a2eedecb063393c2ff7486c16bcc088
jasonmag <jasonmaglangit@gmail.com>
2015-09-21 18:26:09 +0800

    Added filters for variables.

--- app/Http/Requests/Api/FacebookLoginRequest.php
+++ app/Http/Requests/Api/FacebookLoginRequest.php
@@ -23,17 +23,19 @@ class FacebookLoginRequest extends ApiRequest {
 
 			case 'POST' :
 
+				//Added checking if
+
 				switch($this->route()->getName()){
 
 					case 'api.v1.registration.facebook':
 
 						return [
-							'facebook_app_id' => 'required', //-- check with facebook if AppId exists.
-							'email' => 'required|email',
+							'facebook_app_id' => 'required|integer
+								|unique:users,facebook_app_id,NULL,id,user_type,' . $this->__get('user_type'),
+							'email' => 'required|email|unique:users,email,NULL,id,user_type,'.$this->__get('user_type'),
 							'user_type' => 'required|in:'. config('futureed.client') . ',' . config('futureed.student'),
 							'first_name' => 'required',
 							'last_name' => 'required',
-							'country_id' => 'required|exists:countries,id',
 
 							'gender' => 'required_if:user_type,' . config('futureed.student'),
 							'birth_date' => 'required_if:user_type,' . config('futureed.student'),
