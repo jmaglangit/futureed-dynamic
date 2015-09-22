299dad17ca4163a3349a6e7f088a195a3369e4a1
jasonmag <jasonmaglangit@gmail.com>
2015-09-22 14:43:50 +0800

    Added LoggerTrait.

--- app/Models/Traits/LoggerTrait.php
+++ app/Models/Traits/LoggerTrait.php
@@ -1,14 +1,67 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/22/15
- * Time: 2:15 PM
- */
+<?php namespace FutureEd\Models\Traits;
 
-namespace FutureEd\Models\Traits;
 
+use Illuminate\Support\Facades\Log;
 
 trait LoggerTrait {
+
+	//TODO: Place all logs to the database.
+
+
+	//Log Emergency
+	public function emergencyLog($error) {
+
+		Log::emergency($error);
+
+	}
+
+	//Log Alert
+	public function alertLog($error) {
+
+		Log::alert($error);
+
+	}
+
+	//Log Critical
+	public function criticalLog($error){
+
+		Log::critical($error);
+
+	}
+
+	//Log Error
+	public function errorLog($error){
+
+		Log::error('FUTUREED_ERROR: ' . $error);
+	}
+
+	//Log Warning
+	public function warningLog($error){
+
+		Log::warning($error);
+
+	}
+
+	//Log Notice
+	public function noticeLog($error){
+
+		Log::notice($error);
+
+	}
+
+	//Log Info
+	public function infoLog($error){
+
+		Log::info($error);
+
+	}
+
+	//Log Debug
+	public function debugLog($error){
+
+		Log::debug($error);
+
+	}
+
 
 }
