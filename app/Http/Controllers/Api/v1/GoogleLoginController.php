4f3dd04fa43c86fb45f52531a436c623c9b1d4d1
jasonmag <jasonmaglangit@gmail.com>
2015-09-22 13:28:19 +0800

    Added controller for google registration

--- app/Http/Controllers/Api/v1/GoogleLoginController.php
+++ app/Http/Controllers/Api/v1/GoogleLoginController.php
@@ -3,7 +3,9 @@
 use FutureEd\Http\Requests;
 use FutureEd\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
+use FutureEd\Models\Repository\Client\ClientRepositoryInterface;
+use FutureEd\Models\Repository\Student\StudentRepositoryInterface;
+use FutureEd\Http\Requests\Api\GoogleLoginRequest;
 
 class GoogleLoginController extends Controller {
 
@@ -15,7 +17,7 @@ class GoogleLoginController extends Controller {
 	 *
 	 *
 	 * USER
-	 * 	facebook_app_id
+	 * 	google_app_id
 	 *	email
 	 *	name -- combined with first_name and last_name
 	 * 	user_type
@@ -25,15 +27,71 @@ class GoogleLoginController extends Controller {
 	 * 	last_name
 	 * 	gender
 	 * 	birth_date
-	 * 	country_id
 	 *
 	 * CLIENT
 	 * 	first_name
 	 * 	last_name
 	 * 	client_role
-	 * 	country_id
 	 *
 	 */
 
+	protected $student;
+
+	protected $client;
+
+	public function __construct(
+		StudentRepositoryInterface $studentRepositoryInterface,
+		ClientRepositoryInterface $clientRepositoryInterface
+	){
+		$this->student = $studentRepositoryInterface;
+
+		$this->client = $clientRepositoryInterface;
+	}
+
+	public function googleRegistration(GoogleLoginRequest $request){
+
+		$user_type = $request->only('user_type');
+
+		$student = $request->only(
+			'google_app_id',
+			'email',
+			'user_type',
+			'first_name',
+			'last_name',
+			'gender',
+			'birth_date',
+			'country_id',
+			'state',
+			'city'
+		);
+
+		$client = $request->only(
+			'first_name',
+			'last_name',
+			'email',
+			'user_type',
+			'birth_date',
+			'client_role',
+			'street_address',
+			'city',
+			'state',
+			'country_id',
+			'zip'
+		);
+
+		switch($user_type['user_type']){
+
+			case config('futureed.student'):
+
+				//Registration of Student
+				break;
+
+			case config('futureed.client'):
+
+				//Registration of Client.
+				break;
+		}
+
+	}
 
 }
