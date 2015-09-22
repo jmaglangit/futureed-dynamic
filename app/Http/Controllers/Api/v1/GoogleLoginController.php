bb43441f93e4e174ff421bd0cbed05086b1c5886
jasonmag <jasonmaglangit@gmail.com>
2015-09-22 11:18:02 +0800

    Added controller for google login.

---
+++ app/Http/Controllers/Api/v1/GoogleLoginController.php
@@ -0,0 +1,39 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class GoogleLoginController extends Controller {
+
+	/**
+	 * REGISTRATION
+	 */
+	/*
+	 * Get required variables
+	 *
+	 *
+	 * USER
+	 * 	facebook_app_id
+	 *	email
+	 *	name -- combined with first_name and last_name
+	 * 	user_type
+	 *
+	 * STUDENT
+	 * 	first_name
+	 * 	last_name
+	 * 	gender
+	 * 	birth_date
+	 * 	country_id
+	 *
+	 * CLIENT
+	 * 	first_name
+	 * 	last_name
+	 * 	client_role
+	 * 	country_id
+	 *
+	 */
+
+
+}
