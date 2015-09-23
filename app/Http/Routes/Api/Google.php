27eb79672ceb5ff6b0bab698be74e1858c0fbc21
jasonmag <jasonmaglangit@gmail.com>
2015-09-23 18:04:23 +0800

    Added login routes for google

--- app/Http/Routes/Api/Google.php
+++ app/Http/Routes/Api/Google.php
@@ -7,3 +7,12 @@ Routes::post('/registration/google',[
 	'uses' => 'Api\v1\GoogleLoginController@googleRegistration',
 	'as' => 'api.v1.registration.google'
 ]);
+
+/**
+ * Google login
+ */
+Routes::post('/login/google',[
+	'uses' => 'Api\v1\GoogleLoginController@googleLogin',
+	'as' => 'api.v1.login.google',
+	'middleware' => 'api_after_auto_login'
+]);
