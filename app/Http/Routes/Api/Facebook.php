f5667cb0557647256ab330ec56a9b9b3d27e908b
jasonmag <jasonmaglangit@gmail.com>
2015-09-23 11:56:34 +0800

    added routes for facebook login.

--- app/Http/Routes/Api/Facebook.php
+++ app/Http/Routes/Api/Facebook.php
@@ -7,3 +7,14 @@ Routes::post('/registration/facebook',[
 	'uses' => 'Api\v1\FacebookLoginController@facebookRegistration',
 	'as' => 'api.v1.registration.facebook'
 ]);
+
+
+//TODO: Add Token for middleware.
+/**
+ * Facebook login
+ */
+Routes::post('/login/facebook',[
+	'uses' => 'Api\v1\FacebookLoginController@facebookLogin',
+	'as' => 'api.v1.login.facebook',
+	'middleware' => 'api_after_student_login'
+]);
