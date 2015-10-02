fbfcdebba11fea6a3305636bd0a45486ebbafd30
jasonmag <jasonmaglangit@gmail.com>
2015-10-02 19:10:01 +0800

    Added new student report progress

--- app/Http/Controllers/Api/Reports/StudentReportController.php
+++ app/Http/Controllers/Api/Reports/StudentReportController.php
@@ -1,14 +1,14 @@
 <?php namespace FutureEd\Http\Controllers\Api\Reports;
 
 use FutureEd\Http\Requests;
-use FutureEd\Http\Controllers\Controller;
 
-use FutureEd\Models\Core\ClassStudent;
 use FutureEd\Models\Repository\Avatar\AvatarRepositoryInterface;
 use FutureEd\Models\Repository\ClassStudent\ClassStudentRepositoryInterface;
 use FutureEd\Models\Repository\Grade\GradeRepositoryInterface;
+use FutureEd\Models\Repository\Module\ModuleRepositoryInterface;
 use FutureEd\Models\Repository\Student\StudentRepositoryInterface;
 use FutureEd\Http\Requests\Api\StudentReportRequest;
+use FutureEd\Models\Repository\StudentModule\StudentModuleRepository;
 
 class StudentReportController extends ReportController {
 
@@ -20,24 +20,35 @@ class StudentReportController extends ReportController {
 
 	protected $grade;
 
+	protected $module;
+
+	protected $student_module;
+
 
 	/**
 	 * StudentReportController constructor.
 	 * @param StudentRepositoryInterface $studentRepositoryInterface
 	 * @param AvatarRepositoryInterface $avatarRepositoryInterface
 	 * @param ClassStudentRepositoryInterface $classStudentRepositoryInterface
+	 * @param GradeRepositoryInterface $gradeRepositoryInterface
+	 * @param ModuleRepositoryInterface $moduleRepositoryInterface
+	 * @param StudentModuleRepository $studentModuleRepository
 	 * @internal param $student
 	 */
 	public function __construct(
 		StudentRepositoryInterface $studentRepositoryInterface,
 		AvatarRepositoryInterface $avatarRepositoryInterface,
 		ClassStudentRepositoryInterface $classStudentRepositoryInterface,
-		GradeRepositoryInterface $gradeRepositoryInterface
+		GradeRepositoryInterface $gradeRepositoryInterface,
+		ModuleRepositoryInterface $moduleRepositoryInterface,
+		StudentRepositoryInterface $studentRepositoryInterface
 	) {
 		$this->student = $studentRepositoryInterface;
 		$this->class_student = $classStudentRepositoryInterface;
 		$this->avatar = $avatarRepositoryInterface;
 		$this->grade = $gradeRepositoryInterface;
+		$this->module = $moduleRepositoryInterface;
+		$this->student_module = $studentRepositoryInterface;
 	}
 
 
@@ -95,22 +106,46 @@ class StudentReportController extends ReportController {
 			'carnival_rewards' => ''
 		];
 
-		//column headers
-		//get student country_Id
-		//get grade country_Id
-		$countries = $this->grade->getGradeCountries();
+		//COLUMN
+		//check if student countries.
+		$countries = [];
+		$grade_countries = $this->grade->getGradeCountries();
 
-//		dd($countries->toArray());
+		foreach($grade_countries as $grade => $k){
 
-		//parse header based on
+			$countries[] = $k->country_id;
+		}
 
-		$column_header = [
+		//check if student country if in the grade countries
+		$student_country = (in_array($student->country_id,$countries)) ?
+			$student->country_id : config('futureed.default_country');
 
+		$header = $this->module->getModuleGradeByStudentCountry($student_country);
 
-		];
-
-		//rows
+		$column_header = [];
 		$rows = [];
+
+		foreach($header as $column){
+
+			$column_header = array_add($column_header,$column->grade_id,$column->grade_name);
+			$rows = array_add($rows,$column->grade_id,0);
+		}
+
+		//ROWS
+		//get each completed on each grades.
+
+		$row_data = $this->class_student->getStudentModulesProgressByGrade($id,$student->country_id);
+
+
+		foreach($row_data as $data){
+
+			$status_data = [
+				'completed' => ($data->completed) ? round(($data->completed/$data->module_count) * 100): 0,
+				'on_going' => ($data->on_going) ? round(($data->on_going/$data->module_count) * 100) : 0
+			];
+
+			$rows[$data->grade_id] = $status_data;
+		}
 
 		return $this->respondReportData($additional_information, $column_header, $rows);
 
