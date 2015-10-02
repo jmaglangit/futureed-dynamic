5db0dcfd99432322ffdaf358dbdf8efa18e490bf
jasonmag <jasonmaglangit@gmail.com>
2015-10-02 13:30:53 +0800

    added request filter for reports

---
+++ app/Http/Requests/Api/StudentReportRequest.php
@@ -0,0 +1,43 @@
+<?php namespace FutureEd\Http\Requests\Api;
+
+use FutureEd\Http\Requests\Request;
+
+class StudentReportRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+
+		switch($this->method()){
+
+			case 'GET':
+
+				switch($this->route()->getName()){
+
+					case 'api.report.student.progress':
+
+						return [
+							'subject_id' => 'required|exists:subjects,id,deleted_at,NULL'
+						];
+						break;
+				}
+				break;
+		}
+
+	}
+
+}
