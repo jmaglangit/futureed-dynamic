469495548526209175086c2e661b180513964ebd
jasonmag <jasonmaglangit@gmail.com>
2015-10-06 17:27:08 +0800

    Added interface for userlog

--- app/Models/Repository/UserLog/UserLogRepositoryInterface.php
+++ app/Models/Repository/UserLog/UserLogRepositoryInterface.php
@@ -1,14 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 10/6/15
- * Time: 3:40 PM
- */
 
 namespace FutureEd\Models\Repository\UserLog;
 
 
 interface UserLogRepositoryInterface {
+
+	public function getUserLog($id);
+
+	public function getUserLogs($criteria, $offset, $limit);
 
 }
