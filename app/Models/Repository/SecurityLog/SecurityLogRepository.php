5357d8da85baa0c9cb7bd522e5abf03cc65d084a
jasonmag <jasonmaglangit@gmail.com>
2015-10-07 19:08:23 +0800

    added repository files for security logs

---
+++ app/Models/Repository/SecurityLog/SecurityLogRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 10/7/15
+ * Time: 6:10 PM
+ */
+
+namespace FutureEd\Models\Repository\SecurityLog;
+
+
+class SecurityLogRepository {
+
+}
