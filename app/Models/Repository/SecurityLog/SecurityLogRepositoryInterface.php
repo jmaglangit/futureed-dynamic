524a3ba7865246c6a6b6d5f69d4e4418c48c8198
jasonmag <jasonmaglangit@gmail.com>
2015-10-07 19:09:58 +0800

    Added method to retrieve security log

--- app/Models/Repository/SecurityLog/SecurityLogRepositoryInterface.php
+++ app/Models/Repository/SecurityLog/SecurityLogRepositoryInterface.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 10/7/15
- * Time: 6:09 PM
- */
 
 namespace FutureEd\Models\Repository\SecurityLog;
 
 
 interface SecurityLogRepositoryInterface {
+
+	public function getSecurityLogs($criteria, $offset, $limit);
 
 }
