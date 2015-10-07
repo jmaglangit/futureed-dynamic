f2a902c98b3d57043575ba9da6d086346413bb69
jasonmag <jasonmaglangit@gmail.com>
2015-10-07 17:17:22 +0800

    Added repository for adminlog

---
+++ app/Models/Repository/AdminLog/AdminLogRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 10/7/15
+ * Time: 4:28 PM
+ */
+
+namespace FutureEd\Models\AdminLog;
+
+
+class AdminLogRepository {
+
+}
---
+++ app/Models/Repository/AdminLog/AdminLogRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 10/7/15
+ * Time: 4:27 PM
+ */
+
+namespace FutureEd\Models\AdminLog;
+
+
+interface AdminLogRepositoryInterface {
+
+}
