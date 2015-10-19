7405517174256c1545779ffe4f5c63298c434f6f
jasonmag <jasonmaglangit@gmail.com>
2015-10-19 15:48:34 +0800

    Added token to record every after client login

--- app/Http/Middleware/Api/AfterClientLoginMiddleware.php
+++ app/Http/Middleware/Api/AfterClientLoginMiddleware.php
@@ -2,6 +2,7 @@
 
 use Closure;
 use FutureEd\Models\Repository\Client\ClientRepositoryInterface;
+use FutureEd\Services\SessionServices;
 use FutureEd\Services\TokenServices;
 
 class AfterClientLoginMiddleware extends JWTMiddleware {
@@ -10,13 +11,18 @@ class AfterClientLoginMiddleware extends JWTMiddleware {
 
 	protected $token;
 
+	protected $session;
+
 	public function __construct(
 		ClientRepositoryInterface $clientRepositoryInterface,
-		TokenServices $tokenServices
+		TokenServices $tokenServices,
+		SessionServices $sessionServices
 	){
 		$this->client = $clientRepositoryInterface;
 
 		$this->token = $tokenServices;
+
+		$this->session = $sessionServices;
 
 	}
 	/**
@@ -40,6 +46,22 @@ class AfterClientLoginMiddleware extends JWTMiddleware {
 				'type' => config('futureed.client'),
 				'role' => $this->client->getClientRole($content->data->id)
 			]);
+
+
+			//TODO: check if token has expired empty
+			//TODO: Add session token, last_activity to users table.
+
+			$user = $this->client->getClientDetails($content->data->id);
+
+			$user_data = [
+				'user_id' => $user->user_id,
+				'session_token' => session('_token')
+			];
+
+			if(!$this->session->addSessionToken($user_data)){
+
+				return $this->respondErrorMessage(2060);
+			}
 
 			$headers = $response->headers;
 
