807df7f6eb35749c9e9a441af8c5be4fc56fbf15
Jollie Mar Carino Gutang <jgutang@nerubia.com>
2015-10-20 15:25:44 +0800

    Admin updated manage module interface

--- resources/views/admin/manage/module/index.blade.php
+++ resources/views/admin/manage/module/index.blade.php
@@ -28,6 +28,7 @@
 @section('scripts')
 	{!! Html::script('/js/admin/controllers/manage_module_controller.js')!!}
 	{!! Html::script('/js/admin/services/manage_module_service.js')!!}
+
 	{!! Html::script('/js/admin/controllers/manage_age_group_controller.js')!!}
 	{!! Html::script('/js/admin/services/manage_age_group_service.js')!!}
 
@@ -38,7 +39,4 @@
 	{!! Html::script('/js/admin/controllers/manage_question_ans_controller.js')!!}
 	{!! Html::script('/js/admin/services/manage_question_ans_service.js')!!}
 	{!! Html::script('/js/admin/constants/manage_module_constants.js')!!}
-	
-	{!! Html::script('/js/common/table_service.js')!!}
-	{!! Html::script('/js/common/search_service.js')!!}
 @stop
