b53f1f17319abeaf280bc8143d4b8e2fde3ea03c
jasonmag <jasonmaglangit@gmail.com>
2015-10-29 13:21:24 +0800

    Added table of on students report

--- resources/views/student/profile/partials/reports_form.blade.php
+++ resources/views/student/profile/partials/reports_form.blade.php
@@ -8,6 +8,9 @@
         <li role="presentation" ng-class="{ 'active' : reports.active_current_learning }">
             <a href="javascript:void(0)" ng-click="reports.setActive(futureed.CURRENT_LEARNING)">Current Learning</a>
         </li>
+        <li role="presentation" ng-class="{ 'active' : reports.active_subject_area }">
+            <a href="javascript:void(0)" ng-click="reports.setActive(futureed.SUBJECT_AREA)">Subject Area</a>
+        </li>
     </ul>
 
     <div ng-if="reports.errors || reports.success">
@@ -183,9 +186,64 @@
 
                 </tbody>
             </table>
+        </div>
+    </div>
 
+    <div ng-if="reports.active_subject_area">
+        <div class="form-search">
+            {!! Form::open(
+                    [
+                        'id' => 'search_form',
+                        'class' => 'form-horizontal'
+                        , 'ng-submit' => 'reports.searchFnc($event)'
+                    ]
+            ) !!}
+            <div class="form-group">
+                <div class="col-xs-3"></div>
+                <div class="col-xs-6">
+                    <select ng-model="reports.search.subject_id"
+                            ng-change="reports.currentLearning()"
+                            ng-disabled="!reports.subjects.length"
+                            class="form-control">
+                        <option value="">-- Select Subject --</option>
+                        <option ng-selected="reports.search.subject_id == subject.id"
+                                ng-repeat="subject in reports.subjects" ng-value="subject.id">{! subject.name !}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+
+        <div class="reports-container" ng-cloak>
+            <table id="tip-list" class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th ng-repeat="column in reports.summary.columns">{! column.name !}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr ng-repeat="data in reports.summary.records ">
+
+                    <td>{! data.curriculum_name !}</td>
+
+                    <td ng-repeat="(key, value) in reports.summary.columns " ng-if="key > 0">
+                        <div ng-repeat="(dataKey, dataValue) in data.curriculum_data" ng-if="dataValue.grade_id == value.id">
+                            <div class="report-progress">
+                                <div ng-if="dataValue.progress" class="report-progress-bar report-progress-bar-success"
+                                     ng-style="{ 'width' : dataValue.progress + '%' }"></div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
         </div>
 
 
     </div>
+
+
+
 </div>
