6654b8723188f15b7f5c899094ce3369a5162a31
jasonmag <jasonmaglangit@gmail.com>
2015-11-09 18:45:44 +0800

    Added error messages for invalid file extension

--- config/futureed-error.php
+++ config/futureed-error.php
@@ -76,6 +76,7 @@ return [
 		2061 => 'Client is currently logged in.',
 		2062 => 'Admin is currently logged in.',
 		2063 => 'User is currently logged in.',
+		2063 => 'Invalid export file format',
 
 
 		2100 => 'Reset code is invalid.',
