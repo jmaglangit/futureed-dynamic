591c58bb2a1d90a32d3c5e21aa6bc1e5bd7814e6
jasonmag <jasonmaglangit@gmail.com>
2015-11-11 10:24:18 +0800

    Added comments on the function

--- app/Http/Controllers/Api/Reports/ClassReportExportController.php
+++ app/Http/Controllers/Api/Reports/ClassReportExportController.php
@@ -5,14 +5,26 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ClassReportExportController extends ReportController {
 
+	/**
+	 * @var ClassReportController
+	 */
 	protected $class_report;
 
+	/**
+	 * ClassReportExportController constructor.
+	 * @param ClassReportController $classReportController
+	 */
 	public function __construct(
 		ClassReportController $classReportController
 	) {
 		$this->class_report = $classReportController;
 	}
 
+	/**
+	 * @param $class_id
+	 * @param $file_type
+	 * @return mixed
+	 */
 	public function classReport($class_id, $file_type){
 
 		$report = $this->class_report->getClassReport($class_id);
