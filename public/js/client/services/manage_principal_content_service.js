8a2bcda27e49db524670082824f3559176f5162d
jasonmag <jasonmaglangit@gmail.com>
2015-11-17 14:41:11 +0800

    Added principal content service

--- public/js/client/services/manage_principal_content_service.js
+++ public/js/client/services/manage_principal_content_service.js
@@ -1,3 +1,19 @@
-/**
- * Created by jason on 11/17/15.
- */
+angular.module('futureed.services').factory('ManagePrincipalContentService',ManagePrincipalContentService);
+
+function ManagePrincipalContentService($http){
+
+    var apiUrl = '/api/v1/';
+    var reportUrl = '/api/report/';
+    var managePrincipalApi = {};
+
+    //get api report
+    managePrincipalApi.schoolReport = function(school_code){
+        return $http({
+                method : Constants.METHOD_GET
+            , url : reportUrl + 'school/' + school_code
+        });
+    }
+
+
+
+}
