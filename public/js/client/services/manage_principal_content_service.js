e3838f63538b62fa9f3ebfa7bd3c9cfdf52289d6
jasonmag <jasonmaglangit@gmail.com>
2015-11-18 19:16:30 +0800

    Added api callers

--- public/js/client/services/manage_principal_content_service.js
+++ public/js/client/services/manage_principal_content_service.js
@@ -1,19 +1,30 @@
-angular.module('futureed.services').factory('ManagePrincipalContentService',ManagePrincipalContentService);
+angular.module('futureed.services')
+    .factory('ManagePrincipalContentService', ManagePrincipalContentService);
 
-function ManagePrincipalContentService($http){
+ManagePrincipalContentService.$inject = ['$http'];
+
+function ManagePrincipalContentService($http) {
 
     var apiUrl = '/api/v1/';
     var reportUrl = '/api/report/';
     var managePrincipalApi = {};
 
-    //get api report
-    managePrincipalApi.schoolReport = function(school_code){
+    //get school report
+    managePrincipalApi.schoolReport = function (school_code) {
         return $http({
-                method : Constants.METHOD_GET
-            , url : reportUrl + 'school/' + school_code
+            method  :   Constants.METHOD_GET
+            , url   :   reportUrl + 'school/' + school_code
         });
     }
 
+    //get school teacher report
+    managePrincipalApi.schoolTeacherReport = function (school_code){
+        return $http({
+            method  :   Constants.METHOD_GET
+            ,url    :   reportUrl + 'school/' + school_code + '/teachers'
+        });
+    }
 
+    return managePrincipalApi;
 
 }
