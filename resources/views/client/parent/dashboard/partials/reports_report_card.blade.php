8504a467cfbf79d581b6f7b9f80212623423486e
ejingco <ejingco@nerubia.com>
2015-11-19 16:35:04 +0800

    -new partials page for report tabs

---
+++ resources/views/client/parent/dashboard/partials/reports_report_card.blade.php
@@ -0,0 +1,54 @@
+<div class="report-card" ng-if="reports.active_report_card">
+	<div class="form-search report-card-container">
+		{!! Form::open(
+				[
+					'id' => 'search_form',
+					'class' => 'form-horizontal'
+					, 'ng-submit' => 'reports.searchFnc($event)'
+				]
+		) !!}
+		<div class="form-group">
+			<div class="col-xs-3">
+				<img ng-src="{! reports.student.avatar_thumbnail !}"/>
+			</div>
+			<div class="col-xs-9">
+				<fieldset>
+					<legend>{! reports.student.student_name!}</legend>
+					<p>{! reports.student.grade_level!}</p>
+				</fieldset>
+			</div>
+		</div>
+		{!! Form::close() !!}
+	</div>
+
+	<br/>
+
+	<div class="list-container" ng-cloak>
+		<table id="tip-list" class="table table-striped table-bordered">
+			<thead>
+			<tr class="magenta">
+				<th>Subject</th>
+				<th>Module Status</th>
+				<th>Points Earned</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr ng-repeat="data in reports.records">
+				<td>{! data.name !}</td>
+				<td>{! data.module_status !}</td>
+				<td>{! data.points_earned !}</td>
+			</tr>
+			<tr class="odd" ng-if="!reports.records.length && !reports.table.loading">
+				<td valign="top" colspan="7">
+					No records found
+				</td>
+			</tr>
+			<tr class="odd" ng-if="reports.table.loading">
+				<td valign="top" colspan="7">
+					Loading...
+				</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
---
+++ resources/views/client/parent/dashboard/partials/reports_subject_area.blade.php
@@ -0,0 +1,54 @@
+<div class="subject-area" ng-if="reports.active_subject_area">
+	<div class="form-search magenta">
+		{!! Form::open(
+				[
+					'id' => 'search_form',
+					'class' => 'form-horizontal'
+					, 'ng-submit' => 'reports.searchFnc($event)'
+				]
+		) !!}
+		<div class="form-group">
+			<div class="col-xs-3"></div>
+			<div class="col-xs-6">
+				<select ng-model="reports.search.subject_id"
+						ng-change="reports.subjectArea()"
+						ng-disabled="!reports.subjects.length"
+						class="form-control">
+					<option value="">-- Select Subject --</option>
+					<option ng-selected="reports.search.subject_id == subject.id"
+							ng-repeat="subject in reports.subjects" ng-value="subject.id">{! subject.name !}
+					</option>
+				</select>
+			</div>
+		</div>
+		{!! Form::close() !!}
+	</div>
+
+	<div template-directive template-url="{!! route('client.parent.reports.progress_bar') !!}"></div>
+
+	<div class="reports-container" ng-cloak>
+		<table id="tip-list" class="table table-striped table-bordered">
+			<thead>
+			<tr>
+				<th ng-repeat="column in reports.summary.columns">{! column.name !}</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr ng-repeat="data in reports.summary.records ">
+
+				<td>{! data.curriculum_name !}</td>
+
+				<td ng-repeat="(key, value) in reports.summary.columns " ng-if="key > 0">
+					<div ng-repeat="(dataKey, dataValue) in data.curriculum_data" ng-if="dataValue.grade_id == value.id">
+						<div class="report-progress">
+							<div ng-if="dataValue.progress" class="report-progress-bar report-progress-bar-success"
+								 ng-style="{ 'width' : dataValue.progress + '%' }"></div>
+						</div>
+					</div>
+				</td>
+			</tr>
+
+			</tbody>
+		</table>
+	</div>
+</div>
