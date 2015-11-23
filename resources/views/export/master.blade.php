704d2cd1ac814b6312da5776fc946166cece3bd9
ejingco <ejingco@nerubia.com>
2015-11-23 16:19:39 +0800

    -html and css changes to PDFs exprot reports

--- resources/views/export/master.blade.php
+++ resources/views/export/master.blade.php
@@ -3,6 +3,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta charset="UTF-8">
     <title>Future Lesson</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ base_path().'/public/css/futureed-student.css' }}">
 </head>
 <body>
 
