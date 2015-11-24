af3906aa947232cb6a39ea809f92ae1a00716481
jasonmag <jasonmaglangit@gmail.com>
2015-11-24 10:24:38 +0800

    Added blade files

---
+++ resources/views/export/client/principal/school-teacher-progress-excel.blade.php
@@ -0,0 +1,25 @@
+@extends('export.client.principal')
+
+@section('content')
+    {{--Header--}}
+    <div>
+        {{--Logo--}}
+
+        {{--School info--}}
+
+    </div>
+
+    {{--Contents--}}
+    <div>
+        {{--Skills watch--}}
+        <table></table>
+        {{--Class watch--}}
+        <table>
+        </table>
+        {{--student watch--}}
+        <table>
+        </table>
+        {{--score--}}
+        <table></table>
+    </div>
+@stop
