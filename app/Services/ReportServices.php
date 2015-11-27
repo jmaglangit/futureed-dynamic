4adfee8c8e1ead3d81bd1df584186b5d886d9224
jasonmag <jasonmaglangit@gmail.com>
2015-11-27 13:52:45 +0800

    Added report services

--- app/Services/ReportServices.php
+++ app/Services/ReportServices.php
@@ -1,14 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 11/26/15
- * Time: 2:12 PM
- */
+<?php namespace FutureEd\Services;
 
-namespace FutureEd\Services;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class ReportServices {
+
+	//generate hash filename
+	public function createReportFileFolder($data){
+
+		$file_folder = sha1($data . Carbon::now()->timestamp);
+
+		Storage::makeDirectory(config('futureed.reports_folder').'/'.$file_folder);
+
+		return [
+			'path' => config('futureed.reports_folder').'/'.$file_folder,
+			'folder_name' => $file_folder
+		];;
+	}
+
+	public function saveReportFileFolder($file_location, $contents){
+
+		return Storage::put($file_location,$contents);
+	}
+
+	public function getReportFileURL($file_folder){
+
+		$route_name = route('api.report.folder.file',[
+			'folder_name' => $file_folder
+		]);
+
+		return $route_name;
+	}
+
 
 }
