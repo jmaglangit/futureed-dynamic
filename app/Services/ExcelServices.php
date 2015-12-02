48a9ffca48e608308bde7c43f9ef162df29cd596
jasonmag <jasonmaglangit@gmail.com>
2015-12-02 19:58:33 +0800

    Added comment on excel services

--- app/Services/ExcelServices.php
+++ app/Services/ExcelServices.php
@@ -4,7 +4,12 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ExcelServices extends Excel{
 
-
+	/**
+	 * Parse uploaded csv file and returns array.
+	 * @param $csv_file
+	 * @param array $headers
+	 * @return mixed
+	 */
 	public function importCsv($csv_file,$headers = []){
 
 		return Excel::load($csv_file,function ($reader){})->get($headers);
