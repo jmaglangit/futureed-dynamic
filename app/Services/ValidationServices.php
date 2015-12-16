4ce4e6883f535a40cedb3dec99658b5965563d33
jasonmag <jasonmaglangit@gmail.com>
2015-12-16 14:25:17 +0800

    Added validation services

--- app/Services/ValidationServices.php
+++ app/Services/ValidationServices.php
@@ -1,14 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 12/15/15
- * Time: 3:26 PM
- */
+<?php namespace FutureEd\Services;
 
-namespace FutureEd\Services;
+use Illuminate\Validation\Validator;
 
+class ValidationServices extends Validator{
 
-class ValidationServices {
+	/**
+	 * Validate json format.
+	 * @param $attribute
+	 * @param $value
+	 * @param $parameters
+	 * @return bool
+	 */
+	public function validateJson($attribute,$value,$parameters){
+
+		//check json format
+		return (json_decode($value) != NULL);
+	}
+
+	//check graph answer format if correct
+	public function validateGraphAnswer($attribute,$value,$parameters){
+
+		$response = true;
+		$graph = json_decode($value);
+
+		if($graph <> NULL && isset($graph->answer)){
+
+			foreach($graph->answer as $answer){
+				$count = 0;
+				foreach($answer as $data){
+					$count++;
+					//TODO: where it ended.
+				}
+
+			}
+			$response =  true;
+		}else {
+			$response = false;
+		}
+
+		return $response;
+	}
 
 }
