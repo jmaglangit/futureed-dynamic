64ae5b33c7aed2a11df482e0f1698fa346736374
jasonmag <jasonmaglangit@gmail.com>
2015-12-18 20:46:40 +0800

    Added admin permissions

--- app/Http/Routes/Api/StudentModuleAnswer.php
+++ app/Http/Routes/Api/StudentModuleAnswer.php
@@ -4,5 +4,6 @@ Routes::post('/student-module-answer',[
 	'uses' => 'Api\v1\StudentModuleAnswerController@store',
 	'as' => 'api.v1.student-module-answer.store',
 	'middleware' => ['api_user','api_after'],
-	'permission' => ['student']
+	'permission' => ['student','admin'],
+	'role' => ['admin','super admin']
 ]);
