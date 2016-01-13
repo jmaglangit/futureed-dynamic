0cb24b49190303a7268f982a420828e8d7449765
jasonmag <jasonmaglangit@gmail.com>
2016-01-13 15:47:07 +0800

    Added interface for deleted method

--- app/Models/Repository/StudentModuleAnswer/StudentModuleAnswerRepositoryInterface.php
+++ app/Models/Repository/StudentModuleAnswer/StudentModuleAnswerRepositoryInterface.php
@@ -6,4 +6,6 @@ interface StudentModuleAnswerRepositoryInterface
 	public function addStudentModuleAnswer($data);
 
 	public function getStudentModuleAnswer($student_module_id, $module_id);
+
+	public function deletedStudentModuleAnswer($student_module_id, $question_id);
 }
