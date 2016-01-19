867e460161403348aeeb2e180288906378af128d
jasonmag <jasonmaglangit@gmail.com>
2016-01-19 11:41:30 +0800

    Added error logger on UserLog repository

--- app/Models/Repository/UserLog/UserLogRepository.php
+++ app/Models/Repository/UserLog/UserLogRepository.php
@@ -39,74 +39,91 @@ class UserLogRepository implements UserLogRepositoryInterface{
 		 * date_end
 		 */
 
-		$log = new UserLog();
+		DB::beginTransaction();
 
-		if(isset($criteria['user_id'])){
+		try {
 
-			$log = $log->userId($criteria['user_id']);
+			$log = new UserLog();
 
+			if (isset($criteria['user_id'])) {
+
+				$log = $log->userId($criteria['user_id']);
+
+			}
+
+			if (isset($criteria['username'])) {
+
+				$log = $log->username($criteria['username']);
+			}
+
+			if (isset($criteria['email'])) {
+
+				$log = $log->email($criteria['email']);
+			}
+
+			if (isset($criteria['name'])) {
+
+				$log = $log->name($criteria['name']);
+			}
+
+			if (isset($criteria['user_type'])) {
+
+				$log = $log->userType($criteria['user_type']);
+			}
+
+			if (isset($criteria['page_accessed'])) {
+
+				$log = $log->pageAccessed($criteria['page_accessed']);
+			}
+
+			if (isset($criteria['api_accessed'])) {
+
+				$log = $log->apiAccessed($criteria['api_accessed']);
+			}
+
+			if (isset($criteria['result_response'])) {
+
+				$log = $log->resultResponse($criteria['result_response']);
+			}
+
+			if (isset($criteria['status'])) {
+
+				$log = $log->status($criteria['status']);
+			}
+
+			if (isset($criteria['date_start'])) {
+
+				$log = $log->dateStart($criteria['date_start']);
+			}
+
+			if (isset($criteria['date_end'])) {
+
+				$log = $log->dateEnd($criteria['date_end']);
+			}
+
+			$count = $log->count();
+
+			if ($limit > 0 && $offset >= 0) {
+				$log = $log->offset($offset)->limit($limit);;
+			}
+
+			$response = [
+				'total' => $count,
+				'record' => $log->get()->toArray()
+			];
+
+		} catch (\Exception $e) {
+
+			DB::rollback();
+
+			$this->errorLog($e->getMessage());
+
+			return false;
 		}
 
-		if(isset($criteria['username'])){
+		DB::commit();
 
-			$log = $log->username($criteria['username']);
-		}
-
-		if(isset($criteria['email'])){
-
-			$log = $log->email($criteria['email']);
-		}
-
-		if(isset($criteria['name'])){
-
-			$log = $log->name($criteria['name']);
-		}
-
-		if(isset($criteria['user_type'])){
-
-			$log = $log->userType($criteria['user_type']);
-		}
-
-		if(isset($criteria['page_accessed'])){
-
-			$log = $log->pageAccessed($criteria['page_accessed']);
-		}
-
-		if(isset($criteria['api_accessed'])){
-
-			$log = $log->apiAccessed($criteria['api_accessed']);
-		}
-
-		if(isset($criteria['result_response'])){
-
-			$log = $log->resultResponse($criteria['result_response']);
-		}
-
-		if(isset($criteria['status'])){
-
-			$log = $log->status($criteria['status']);
-		}
-
-		if(isset($criteria['date_start'])){
-
-			$log = $log->dateStart($criteria['date_start']);
-		}
-
-		if(isset($criteria['date_end'])){
-
-			$log = $log->dateEnd($criteria['date_end']);
-		}
-
-		$count = $log->count();
-
-		if($limit > 0 && $offset >= 0) {
-			$log = $log->offset($offset)->limit($limit);;
-		}
-
-		return [
-			'total' => $count,
-			'record' => $log->get()->toArray()
-		];
+		return $response;
 	}
 
 	/**
