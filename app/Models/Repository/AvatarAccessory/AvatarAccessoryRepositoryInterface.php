d87ff89161628e1aa43739f154b8934d611756c8
jmaglangit <jmaglangit@nerubia.com>
2016-02-29 11:24:41 +0800

    Update spacing of lines

--- app/Models/Repository/AvatarAccessory/AvatarAccessoryRepositoryInterface.php
+++ app/Models/Repository/AvatarAccessory/AvatarAccessoryRepositoryInterface.php
@@ -4,8 +4,12 @@ namespace FutureEd\Models\Repository\AvatarAccessory;
 
 
 interface AvatarAccessoryRepositoryInterface {
+
     public function getAvatarAccessories($student_id);
+
     public function buyAvatarAccessory($avatar_accessory);
+
     public function getStudentAvatarAccessories($student_id);
+
     public function canBuyAvatarAccessory($student_id, $avatar_accessories_id);
 }
