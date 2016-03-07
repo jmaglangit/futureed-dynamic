0026ced6e7099d22db2abfe264fe4d1350b2c476
jmaglangit <jmaglangit@nerubia.com>
2016-03-07 14:50:17 +0800

    Updated message display on successfull email confirmation

--- resources/views/client/login/registration-success.blade.php
+++ resources/views/client/login/registration-success.blade.php
@@ -9,27 +9,17 @@
 				{! error !}
 			</p>
 		</div>
-		
+
 		<div class="form_content">
-			<div ng-if="!login.resent && !login.linked">
+			<div ng-if="!login.linked">
 				<div class="roundcon">
 					<i class="fa fa-check fa-5x img-rounded text-center"></i>
 				</div>
 
-				<p class="text" >
-					A confirmation code has been sent to your email account.
-				</p>
-
-				<small>Please check your inbox or your spam folder for the email. The email contains a confirmation code that you need to input below.</small>
-			</div>
-			
-			<div ng-if="!login.resent && login.linked">
-				<div class="roundcon">
-					<i class="fa fa-check fa-5x img-rounded text-center"></i>
-				</div>
+				<small>You're almost done! We just need you to confirm you email address.</small>
 
 				<p class="text" >
-					Please enter the confirmation code to verify your email address and to setup your new password.
+					Please check your inbox and click on the link.
 				</p>
 			</div>
 
@@ -44,40 +34,9 @@
 				<small>Please check your inbox or your spam folder for the email. The email contains a confirmation code that you need to input below.</small>
 			</div>
 
-			{!! Form::open(array('id' => 'registration_success_form', 'ng-submit' => 'login.confirmClientRegistration($event)')) !!}
-				<div class="form-group">
-					{!! Form::label('', 'Enter Confirmation Code:') !!}
-					{!! Form::text('email_code', '', 
-						array(
-							'ng-model' => 'login.record.email_code'
-							, 'placeholder' => 'Confirmation Code'
-							, 'autocomplete' => 'off'
-							, 'class' => 'form-control')
-					) !!}
-				</div>
-				<div class="btn-container">
-					{!! Form::button('Confirm'
-							, array(
-										'id' => 'proceed_btn'
-									, 'class' => 'btn btn-blue btn-medium'
-									, 'ng-click' => 'login.confirmClientRegistration($event)'
-									, 'ng-if' => '!login.account_confirmed'
-							)
-					) !!}
-
-					{!! Form::button('Resend'
-							, array(
-										'class' => 'btn btn-gold btn-medium'
-									, 'ng-click' => 'login.resendClientConfirmation()'
-									, 'ng-if' => '!login.account_confirmed'
-							)
-					)!!}
-				</div>
-				<br />
-				<a href="{!! route('client.login') !!}"><i class="fa fa-home"></i> Home</a>
-			{!! Form::close() !!}
 		</div>
 	</div>
+
 
 	<div ng-if="login.confirmed">
 		<div class="title">
@@ -93,20 +52,7 @@
 				Your email account has been successfully confirmed.
 			</p>
 
-			<h4 class="title">
-					You will be receiving an email shortly by our Admin if your registration has been approved or not.
-			</h4>
-			<small> 
-					If you have not yet receive the email, please check your inbox or your spam folder.
-			</small>
 
-			<div class="form-group">
-				{!! Html::link(route('client.login'), 'Click here to Login'
-						, array(
-								'class' => 'btn btn-blue'
-						) 
-				) !!}
-			</div>
-		</div> 
-	</div> 
+		</div>
+	</div>
 </div>
