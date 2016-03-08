2b57e0e9ba6c46f6a102328e9750abaa13270843
jmaglangit <jmaglangit@nerubia.com>
2016-03-08 14:16:52 +0800

    updated confirmation messages and structures

--- resources/views/student/login/register/confirmation-success.blade.php
+++ resources/views/student/login/register/confirmation-success.blade.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 3/7/16
- * Time: 6:47 PM
- */
+<div class="login-container form-style" ng-if="login.active_registration_success">
+    <div class="title" ng-if="!login.linked">Thank you for registering to Future Lesson!</div>
+    <div class="title" ng-if="login.linked">Confirm Your Email Address</div>
+
+    <div class="alert alert-danger" ng-if="login.errors">
+        <p ng-repeat="error in login.errors" >
+            {! error !}
+        </p>
+    </div>
+
+    <div ng-if="login.confirmed">
+        <div class="tittle">
+            <h3>Success!</h3>
+        </div>
+
+        <div class="form_content">
+            <div class="roundcon">
+                <i class="fa fa-check fa-5x img-rounded text-center"></i>
+            </div>
+
+            <p>Your email account has been successfully confirmed.</p>
+
+        </div>
+    </div>
+</div>
