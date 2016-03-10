8e675cdac7a1a8315f8d8920a5012bc0e12e2492
Roy <rreynes@nerubia.com>
2016-03-10 15:52:02 +0800

    indented

--- app/Http/Requests/Api/StudentRegistrationRequest.php
+++ app/Http/Requests/Api/StudentRegistrationRequest.php
@@ -22,17 +22,17 @@ class StudentRegistrationRequest extends ApiRequest {
 	public function rules()
 	{
 		return [
-				'first_name'    => 'required|min:2|regex:'.config('regex.name').'|max:64',
-				'last_name'     => 'required|min:2|regex:'.config('regex.name').'|max:64',
-				'gender'        => 'required|alpha|in:male,female',
-				'birth_date'    => 'required|date_format:Ymd|before:-14 year',
-				'grade_code'    => 'numeric',
-				'country_id'    => 'numeric',
-				'state'         => 'max:128|regex:'.config('regex.state_city'),
-				'city'          => 'max:128|regex:'.config('regex.state_city'),
-				'email'         => 'required|email',
-				'username'      => 'min:8|max:32|alpha_num',
-				'callback_uri'  => 'required|string|max:128'
+			'first_name'    => 'required|min:2|regex:'.config('regex.name').'|max:64',
+			'last_name'     => 'required|min:2|regex:'.config('regex.name').'|max:64',
+			'gender'        => 'required|alpha|in:male,female',
+			'birth_date'    => 'required|date_format:Ymd|before:-14 year',
+			'grade_code'    => 'numeric',
+			'country_id'    => 'numeric',
+			'state'         => 'max:128|regex:'.config('regex.state_city'),
+			'city'          => 'max:128|regex:'.config('regex.state_city'),
+			'email'         => 'required|email',
+			'username'      => 'min:8|max:32|alpha_num',
+			'callback_uri'  => 'required|string|max:128'
 		];
 	}
 
