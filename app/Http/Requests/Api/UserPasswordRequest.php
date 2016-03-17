647416225630893931abc3d601644508aaa3993f
jmaglangit <jmaglangit@nerubia.com>
2016-03-17 14:56:10 +0800

    added filter for user/password/forgot uri

--- app/Http/Requests/Api/UserPasswordRequest.php
+++ app/Http/Requests/Api/UserPasswordRequest.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 3/17/16
- * Time: 2:22 PM
- */
+<?php namespace FutureEd\Http\Requests\Api;
+
+
+class UserPasswordRequest extends ApiRequest {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules(){
+
+		switch($this->method()){
+
+			case 'POST':
+
+				switch($this->route()->getName()){
+
+					case 'api.v1.user.password.forgot':
+						return [
+							'username' => 'required|string',
+							'user_type' => 'required|in:'.config('futureed.admin')
+								.','.config('futureed.client').','.config('futureed.student'),
+							'callback_uri' => 'required|string'
+						];
+						break;
+				}
+				break;
+		}
+	}
+
+}
+
+
