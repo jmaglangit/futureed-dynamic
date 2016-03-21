72543e615c36f6ac22313e075c35d77699b53382
Farrah Faye Dy <fdy@nerubia.com>
2016-03-21 19:48:20 +0800

    changes for localisation for student login confirm

--- resources/views/student/login/partials/confirm-media.blade.php
+++ resources/views/student/login/partials/confirm-media.blade.php
@@ -10,10 +10,10 @@
 								{!! Html::image('/images/user_student.png') !!}
 							</div>
 							<div class="media-body">
-								<h3 class="box-title">Register {! login.record.media_type !} Detail</h3>
+								<h3 class="box-title">{!! trans('messages.register') !!} {! login.record.media_type !} {!! trans('messages.detail') !!}</h3>
 								<div class="info-box rd">
-									<h4>For Students 13 years old and below</h4>
-									<p>Parents should be the one to register, please click {!! Html::link(route('client.registration'), 'here') !!} to register.</p>
+									<h4>{!! trans('messages.for_students_13_years') !!}</h4>
+									<p>{!! trans('messages.parent_should_register') !!} {!! Html::link(route('client.registration'), 'trans('messages.here')') !!} {!! trans('messages.to_register') !!}</p>
 								</div>
 							</div>
 						</div>
@@ -37,102 +37,102 @@
 					</div>
 				</div>
 
-				<div>(<span class="required">*</span> ) Indicates a required field.</div>
+				<div>(<span class="required">*</span> ) {!! trans('messages.indicate_required_field') !!}</div>
 				<fieldset>
-					<legend>Personal Information</legend>
+					<legend>{!! trans('messages.personal_info') !!}</legend>
 					<div class="form-group">
-						<label class="col-xs-2 control-label">Birthday<span class="required">*</span></label>
+						<label class="col-xs-2 control-label">{!! trans('messages.birthday') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							<input type="hidden" id="birth_date" 
 								onchange="checkAge()"
 								ng-init="login.setDropdown(login.record.birth_date)">
 						</div>
-						<label class="col-xs-2 control-label">Gender<span class="required">*</span></label>
+						<label class="col-xs-2 control-label">{!! trans('messages.gender') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							{!! Form::select('gender'
 								, array(
-									'' => '-- Select Gender --'
-									, 'Male' => 'Male'
-									, 'Female' => 'Female'
+									'' => 'trans('messages.select_gender')'
+									, 'Male' => 'trans('messages.male')'
+									, 'Female' => 'trans('messages.female')'
 								)
 								, ''
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['gender'] }"
 									, 'ng-model' => 'login.record.gender')
-							) !!}
+							); !!}
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-xs-2 control-label">First Name<span class="required">*</span></label>
+						<label class="col-xs-2 control-label">{!! trans('messages.first_name') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							{!! Form::text('first_name', ''
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['first_name'] }"
-									, 'placeholder' => 'First Name' 
+									, 'placeholder' => 'trans('messages.first_name')' 
 									, 'ng-model' => 'login.record.first_name'
 								)
 							) !!}
 						</div>
-						<label class="col-xs-2 control-label">Last Name<span class="required">*</span></label>
+						<label class="col-xs-2 control-label">{!! trans('messages.last_name') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							{!! Form::text('last_name', ''
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['last_name'] }"
-									, 'placeholder' => 'Last Name' 
+									, 'placeholder' => 'trans('messages.last_name')' 
 									, 'ng-model' => 'login.record.last_name')
 							) !!}
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-xs-2 control-label">City</label>
+						<label class="col-xs-2 control-label">{!! trans('messages.city') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							{!! Form::text('city', ''
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['city'] }"
-									, 'placeholder' => 'City' 
+									, 'placeholder' => 'trans('messages.city')' 
 									, 'ng-model' => 'login.record.city')
 							) !!}
 						</div>
-						<label class="col-xs-2 control-label">State</label>
+						<label class="col-xs-2 control-label">{!! trans('messages.state') !!}</label>
 						<div class="col-xs-4">
 							{!! Form::text('state', ''
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['state'] }"
-									, 'placeholder' => 'State' 
+									, 'placeholder' => 'trans('messages.state')' 
 									, 'ng-model' => 'login.record.state'
 								)
 							) !!}
 						</div>
 					</div>
 					<div class="form-group" ng-init="getCountries()">
-						<label class="col-xs-2 control-label">Country</label>
+						<label class="col-xs-2 control-label">{!! trans('messages.country') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							<select name="country_id" id="country" class="form-control" 
 								ng-class="{ 'required-field' : login.fields['country_id'] }"
 								ng-model="login.record.country_id" 
 								ng-change="getGradeLevel(login.record.country_id)">
-								<option value="">-- Select Country --</option>
+								<option value="">{!! trans('messages.select_country') !!}</option>
 								<option ng-repeat="country in countries" ng-value="country.id">{! country.name !}</option>
 							</select>
 						</div>
 					</div>
 				</fieldset>
 				<fieldset>
-					<legend>User Credentials</legend>
+					<legend>{!! trans('messages.user_credentials') !!}</legend>
 					<div class="form-group">
-						<label class="col-xs-2 control-label">Username <span class="required">*</span></label>
+						<label class="col-xs-2 control-label">{!! trans('messages.username') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							{!! Form::text('username', ''
 								, array(
 									'class' => 'form-control'
 									, 'autocomplete' => 'off'
 									, 'ng-class' => "{ 'required-field' : login.fields['username'] }"
-									, 'placeholder' => 'Username'
+									, 'placeholder' => 'trans('messages.username')'
 									, 'ng-model' => 'login.record.username'
 									, 'ng-model-options' => "{ debounce : {'default' : 1000} }"
 									, 'ng-change' => "login.checkUsername(login.record.username, futureed.STUDENT, futureed.FALSE)"
@@ -152,7 +152,7 @@
 								, array(
 									'class' => 'form-control'
 									, 'ng-class' => "{ 'required-field' : login.fields['email'] }"
-									, 'placeholder' => 'Email Address' 
+									, 'placeholder' => 'trans('messages.email_address')' 
 									, 'ng-model' => 'login.record.email'
 									, 'ng-disabled' => 'true'
 								)
@@ -161,15 +161,15 @@
 					</div>
 				</fieldset>
 				<fieldset>
-					<legend>School Information</legend>
+					<legend>{!! trans('messages.school_info') !!}</legend>
 					<div class="form-group">
-						<label class="col-xs-2 control-label">School level</label>
+						<label class="col-xs-2 control-label">{!! trans('messages.school_level') !!}<span class="required">*</span></label>
 						<div class="col-xs-4">
 							<select name="grade_code" ng-disabled="!grades.length" 
 								class="form-control"
 								ng-class="{ 'required-field' : login.fields['grade_code'] }"
 								ng-model="login.record.grade_code">
-								<option value="">-- Select Level --</option>
+								<option value="">{!! trans('messages.select_level') !!}</option>
 								<option ng-selected="login.record.grade_code == grade.code" ng-repeat="grade in grades"  ng-value="grade.code">{! grade.name !}</option>
 							</select>
 						</div><br><br>
@@ -183,9 +183,9 @@
 							<label>
 								{!! Form::checkbox('terms', 1, null, array('ng-model' => 'login.record.terms')) !!}
 								
-								I agree on the 
+								{!! trans('messages.i_agree') !!}
 
-								{!! Html::link('#', 'Terms and Conditions'
+								{!! Html::link('#', 'trans('messages.terms_and_conditions')'
 									, array(
 										'ng-click' => "showModal('terms_modal')"
 										, 'data-toggle' => 'modal'
@@ -194,7 +194,7 @@
 
 								 and 
 
-								{!! Html::link('#', 'Data Privacy Policy'
+								{!! Html::link('#', 'trans('messages.data_privacy_policy')'
 									, array(
 										'ng-click' => "showModal('policy_modal')"
 										, 'data-toggle' => 'modal'
@@ -204,14 +204,14 @@
 						</div>
 					</div>
 					<div class="btn-container col-sm-6 col-sm-offset-3">
-						{!! Form::button('Confirm'
+						{!! Form::button('trans('messages.confirm')'
 							, array(
 								'class' => 'btn btn-maroon btn-medium'
 								, 'ng-click' => "login.confirmMediaDetails()"
 							)
 						) !!}
 
-						{!! Html::link(route('student.login'), 'Cancel'
+						{!! Html::link(route('student.login'), 'trans('messages.cancel')'
 							, array(
 								'class' => 'btn btn-gold btn-medium'
 							)
@@ -229,11 +229,11 @@
 					
 				</div>
 				<div class="modal-body">
-					<h5>Students 13 years old or younger cannot register. Please ask a Parent to sign up in the <a href="{!! route('client.registration') !!}">client site</a></h5>
+					<h5>{!! trans('messages.student_13_years_old_younger') !!} <a href="{!! route('client.registration') !!}">{!! trans('messages.client_site') !!}</a></h5>
 				</div>
 				<div class="modal-footer">
 					<div class="btncon col-xs-8 col-xs-offset-4 pull-left">
-						{!! Form::button('Cancel'
+						{!! Form::button('trans('messages.cancel')'
 							, array(
 								'class' => 'btn btn-gold btn-medium'
 								, 'data-dismiss' => 'modal'
@@ -255,14 +255,14 @@
 			{!! Html::image('/images/logo-md.png') !!}
 		</div>
 
-		<div class="title title-student">Registration Success</div>
+		<div class="title title-student">{!! trans('messages.registration_success') !!}</div>
 
 		<div class="alert alert-success">
-			<p> You have successfully registered.</p>
+			<p> {!! trans('messages.you_have_successfully_registered') !!}</p>
 		</div>
 
 		<div class="form-group">
-			{!! Form::button('Proceed to Dashboard' 
+			{!! Form::button('trans('messages.proceed_to_dashboard')' 
 			, array(
 				  'class' => 'btn btn-maroon btn-medium'
 				, 'ng-click' => 'login.proceedToDashboard()'
