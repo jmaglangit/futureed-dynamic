2f5af87b2ecf0670dad32c75eb77f6f5ac45c3d9
Farrah Faye Dy <fdy@nerubia.com>
2016-03-21 20:15:52 +0800

    changes for localisation for student class module help request view

--- resources/views/student/class/module/partials/help_request/view.blade.php
+++ resources/views/student/class/module/partials/help_request/view.blade.php
@@ -2,13 +2,13 @@
 	<div class="view-help-btn">
 		<div ng-if="help.record.student_id == user.id && help.record.question_status == futureed.OPEN">
 			<button type="button" class="btn btn-maroon btn-small col-xs-6" ng-if="help.answers.length"
-				ng-click="help.closeRequest(futureed.TRUE)"> Close Request </button>
+				ng-click="help.closeRequest(futureed.TRUE)"> {!! trans('messages.close_req') !!} </button>
 			<button type="button" class="btn btn-maroon btn-small col-xs-6" ng-if="!help.answers.length"
-				ng-click="help.deleteRequest(futureed.TRUE)"> Delete Request </button>
+				ng-click="help.deleteRequest(futureed.TRUE)"> {!! trans('messages.del_req') !!} </button>
 		</div>
 
 		<button type="button" class="btn btn-gold btn-small"
-			ng-click="help.setModuleActive()"> Back </button>
+			ng-click="help.setModuleActive()"> {!! trans('messages.back') !!} </button>
 	</div>
 
     <div class="alert alert-error" ng-if="help.errors">
@@ -17,7 +17,7 @@
 	        </p>
 	    </div>
 	    <div class="alert alert-success" ng-if="help.success">
-	    	<p>Successfully submitted your answer.</p>
+	    	<p>{!! trans('messages.successful_submit_answer') !!}</p>
 	    </div>
 
 	<div id="help_request_details">
@@ -44,7 +44,7 @@
 							<span>{! help.record.subject_area_name !} > {! help.record.subject_name !}</span>
 						</div>
 						<div class="col-xs-6">
-							<span><i class="fa fa-user"></i> By : {! help.record.name !}</span>
+							<span><i class="fa fa-user"></i> {!! trans('messages.by') !!} : {! help.record.name !}</span>
 						</div>
 					</div>
 
@@ -56,7 +56,7 @@
 			</div>
 		</div>
 		<fieldset>
-			<legend class="tip-title">Answers</legend>
+			<legend class="tip-title">{!! trans('messages.answer') !!}</legend>
 		</fieldset>
 		<div class="help-answers-container" ng-repeat="answer in help.answers">
 			<div class="content-box row margin-top-bot-5">
@@ -80,7 +80,7 @@
 						<p>{! help.record.subject_area_name !} > {! help.record.subject_name !}</p>
 					</div>
 					<div class="col-xs-6">
-						<span><i class="fa fa-user"></i> By : {! answer.student.first_name !} {! answer.student.last_name !}</span>
+						<span><i class="fa fa-user"></i> {!! trans('messages.by') !!} : {! answer.student.first_name !} {! answer.student.last_name !}</span>
 					</div>
 				</div>
 			</div>
@@ -90,7 +90,7 @@
 			{!! Form::textarea('answer', ''
 				, array(
 					'class' => 'form-control disabled-textarea'
-					, 'placeholder' => 'Answer'
+					, 'placeholder' => 'trans('messages.answer')'
 					, 'ng-model' => 'help.record.answer'
 					, 'rows' => '5'
 				)
@@ -98,14 +98,14 @@
 		</div>
 
 		<div class="btn-container" ng-if="help.record.question_status == futureed.OPEN">
-			{!! Form::button('Clear'
+			{!! Form::button('trans('messages.clear')'
 				, array(
 					'class' => 'btn btn-gold btn-small pull-right'
 					, 'ng-click' => "help.clearAnswer()"
 				)
 			) !!}
 
-			{!! Form::button('Submit'
+			{!! Form::button('trans('messages.submit')'
 				, array(
 					'class' => 'btn btn-maroon btn-small pull-right'
 					, 'ng-click' => "help.answerRequest()"
