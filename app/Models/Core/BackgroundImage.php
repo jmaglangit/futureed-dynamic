98b17970b5f30c19e28e02482bf8d436c9cd86b7
jmaglangit <jmaglangit@nerubia.com>
2016-03-22 16:36:37 +0800

    created new files

---
+++ app/Models/Core/BackgroundImage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/22/16
+ * Time: 11:18 AM
+ */
+
+namespace FutureEd\Models\Core;
+
+
+class BackgroundImage {
+
+}
---
+++ app/Models/Repository/BackgroundImage/BackgroundImageRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/22/16
+ * Time: 11:21 AM
+ */
+class BackgroundImageRepository {
+
+}
