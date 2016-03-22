665cb606c2ceca96d2d80f322b2c9bc7d1d6500d
jmaglangit <jmaglangit@nerubia.com>
2016-03-22 16:43:48 +0800

    created controller for retrieving list of background images

---
+++ app/Http/Controllers/Api/v1/BackgroundImageController.php
@@ -0,0 +1,30 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class BackgroundImageController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+}
