efe72403b542d73138c79218898f112ee9e79a80
jmaglangit <jmaglangit@nerubia.com>
2016-03-22 16:45:15 +0800

    added request for the student background controller

---
+++ app/Http/Requests/Api/StudentBackgroundImageRequest.php
@@ -0,0 +1,34 @@
+<?php namespace FutureEd\Http\Requests\Api;
+
+use FutureEd\Http\Requests\Request;
+
+class StudentBackgroundImageRequest extends ApiRequest {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		switch ($this->method()){
+
+			case 'PUT':
+				return [
+					'id' => 'required|exists:background_images,id,deleted_at,NULL'
+				];
+				break;
+		}
+	}
+
+}
