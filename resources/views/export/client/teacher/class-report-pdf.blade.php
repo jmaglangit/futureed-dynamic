eb35b244029bbdd2325cb7103a41d5a1f1547195
Farrah Faye Dy <fdy@nerubia.com>
2016-03-22 17:55:01 +0800

    changes for localisation for export client teacher class report

--- resources/views/export/client/teacher/class-report-pdf.blade.php
+++ resources/views/export/client/teacher/class-report-pdf.blade.php
@@ -7,26 +7,26 @@
 @section('content')
 	<div>
 		<img src="{{ base_path().'/public/images/logo-md.png' }}">
-		<p>Teacher: {{ $additional_information['teacher_first_name'] . ' ' . $additional_information['teacher_last_name'] }}</p>
+		<p>{!! trans('messages.teacher') !!}: {{ $additional_information['teacher_first_name'] . ' ' . $additional_information['teacher_last_name'] }}</p>
 	</div>
 
-	<h3 class="export-sub-header">Teacher Details</h3>
+	<h3 class="export-sub-header">{!! trans('messages.client_teacher_details') !!}</h3>
 	<table class="export-table export-table-bordered"> 
 		<tr class="magenta-row class-report">
-			<th>Class Name</th>
+			<th>{!! trans('messages.class_name') !!}</th>
 			<td>{{ $additional_information['class_name'] }}</td>
 		</tr>
 		<tr class="magenta-row class-report">
-			<th>Class Level</th>
+			<th>{!! trans('messages.class_level') !!}</th>
 			<td>{{ $additional_information['grade_name'] }}</td>
 		</tr>
 	</table>
 
-	<h3 class="export-sub-header">Student Status</h3>
+	<h3 class="export-sub-header">{!! trans('messages.student_status') !!}</h3>
 	<table class="export-table export-table-bordered">
 		<tr class="magenta">
-			<th>Student Name</th>
-			<th>Status</th>
+			<th>{!! trans('messages.student_name') !!}</th>
+			<th>{!! trans('messages.status') !!}</th>
 		</tr>
 		@foreach($rows[0]['student_progress'] as $row)
 		<tr>
@@ -36,14 +36,14 @@
 		@endforeach
 	</table>
 
-	<h3 class="export-sub-header">Students to Watch</h3>
+	<h3 class="export-sub-header">{!! trans('messages.student_to_watch') !!}</h3>
 	<table class="export-table export-table-bordered">
 		<tr class="magenta-row class-report">
-			<th>Struggling</th>
+			<th>{!! trans('messages.struggling') !!}</th>
 			<td>{{ $rows[0]['student_watch']['struggling'] }}</td>
 		</tr>
 		<tr class="magenta-row class-report">
-			<th>Excelling</th>
+			<th>{!! trans('messages.excelling') !!}</th>
 			<td>{{ $rows[0]['student_watch']['excelling'] }}</td>
 		</tr>
 	</table>
