a029f073cda74c5796cdb1bb53e2f1699a43d640
Farrah Faye Dy <fdy@nerubia.com>
2016-03-22 19:06:58 +0800

    changes for localisation for verify client email

--- resources/views/emails/client/verify-client.blade.php
+++ resources/views/emails/client/verify-client.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
 </head>
 <body>
-<div>Hi there {{ $name  }},</div><br/>
+<div>{!! trans('messages.hi_there') !!} {{ $name  }},</div><br/>
 
 <div>
-	<div>Your Future Lesson account is now active.  <br/><br/>Please go to <a href = "{{$link}}">Login Page</a> and login to begin your adventure on the Future Lesson platform.
+	<div>{!! trans('messages.email_verify_client_msg') !!} <br/><br/>{!! trans('messages.email_verify_client_msg2') !!} <a href = "{{$link}}">{!! trans('messages.email_verify_client_msg3') !!}</a> {!! trans('messages.email_verify_client_msg4') !!}
 </div><br/>
 
-    <div>Regards,</div><br/>
-    <div>Admin</div>
+    <div>{!! trans('messages.regards') !!},</div><br/>
+    <div>{!! trans('messages.admin') !!}</div>
     
 </div>
 </body>
