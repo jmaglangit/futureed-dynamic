31e8267bf2eee677269774b36da603cad4c41dd9
jmaglangit <jmaglangit@nerubia.com>
2016-03-22 16:38:05 +0800

    added background_images table

--- app/Models/Core/BackgroundImage.php
+++ app/Models/Core/BackgroundImage.php
@@ -1,14 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 3/22/16
- * Time: 11:18 AM
- */
+<?php namespace FutureEd\Models\Core;
 
-namespace FutureEd\Models\Core;
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+class BackgroundImage extends Model{
 
-class BackgroundImage {
+	use SoftDeletes;
 
+	use TransactionTrait;
+
+	protected $table = 'background_images';
+
+	protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at'];
+
+	protected $fillable = ['name','filename','status'];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1
+	];
 }
