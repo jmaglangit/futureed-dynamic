cd282e9edc7fd0bac5c5c260d2dd1df18f43ac0a
Farrah Faye Dy <fdy@nerubia.com>
2016-03-22 19:36:36 +0800

    changes for localisation for client teacher question

--- resources/views/client/teacher/question/partials/list.blade.php
+++ resources/views/client/teacher/question/partials/list.blade.php
@@ -1,14 +1,18 @@
 <div class="col-xs-12 teacher-module-container" ng-if="question.active_list">
 	<div class="content-title">
 		<div class="title-main-content">
-			<span><i class="fa fa-lightbulb-o"></i> Questions </span>
+			<span><i class="fa fa-lightbulb-o"></i> {!! trans('messages.question') !!} </span>
 
 			<div class="col-xs-2 pull-right">
 				<span>
-					<a href="{!! route('client.teacher.module.index') !!}" class="btn btn-gold teacher-module-back">Back</a>
+					<a href="{!! route('client.teacher.module.index') !!}" class="btn btn-gold teacher-module-back">{!! trans('messages.back') !!}</a>
 				</span>
 			</div>
 		</div>
+	</div>
+
+	<div class="view-contents-btn">
+		<button class="btn btn-maroon" ng-click="question.viewContents('{!! route('client.teacher.teaching_content.index') !!}')">{!! trans('messages.view_contents') !!}</button> 
 	</div>
 
 	<div class="col-xs-12 question-search-container">
@@ -19,7 +23,7 @@
 				<div class="col-xs-4">
 					{!! Form::select('search_status'
 						, array(
-							'' => '-- Select Difficulty --'
+							'' => 'trans('messages.select_difficulty')'
 							, '1' => '1'
 							, '2' => '2'
 							, '3' => '3'
@@ -33,7 +37,7 @@
 					) !!}
 				</div>
 				<div class="col-xs-2">
-					{!! Form::button('Clear'
+					{!! Form::button('trans('messages.clear')'
 						,array(
 							'class' => 'btn btn-gold'
 							, 'ng-click' => 'question.clearFnc($event)'
@@ -46,7 +50,7 @@
 
 	<div class="no-content-container col-xs-12" ng-if="!question.record">
 		<div class="alert alert-info">
-			<center><span><i class="fa fa-info-circle"></i> No Question Available</span></center>
+			<center><span><i class="fa fa-info-circle"></i> {!! trans('messages.no_questions_available') !!}</span></center>
 		</div>
 	</div>
 
