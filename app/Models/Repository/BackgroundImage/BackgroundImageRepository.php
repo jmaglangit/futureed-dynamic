83d4f660c8a14e117fdc87c91d1524dfba78c086
jmaglangit <jmaglangit@nerubia.com>
2016-03-22 16:39:30 +0800

    created functions for retrieving datda

--- app/Models/Repository/BackgroundImage/BackgroundImageRepository.php
+++ app/Models/Repository/BackgroundImage/BackgroundImageRepository.php
@@ -1,11 +1,41 @@
-<?php
+<?php namespace FutureEd\Models\Repository\BackgroundImage;
 
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 3/22/16
- * Time: 11:21 AM
- */
-class BackgroundImageRepository {
+use FutureEd\Models\Core\BackgroundImage;
 
+class BackgroundImageRepository implements BackgroundImageRepositoryInterface {
+
+	/**
+	 * get list of background images.
+	 * @param array $criteria
+	 * @param int $limit
+	 * @param int $offset
+	 * @return array
+	 */
+	public function getBackgroundImages($criteria = [], $limit = 0, $offset = 0) {
+
+		$background_image = new BackgroundImage();
+
+		if ($limit > 0 && $offset >= 0) {
+
+			$background_image = $background_image->offset($offset)->limit($limit);
+		}
+
+		$records = $background_image->get();
+		$count = $background_image->get()->count();
+
+		return [
+			'total' => $count,
+			'records' => $records
+		];
+	}
+
+	/**
+	 * get a background image.
+	 * @param $id
+	 * @return mixed
+	 */
+	public function getBackgroundImage($id) {
+
+		return BackgroundImage::whereId($id)->get();
+	}
 }
