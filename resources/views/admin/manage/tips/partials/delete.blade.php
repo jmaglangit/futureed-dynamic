cf6c88eefa38540cd274009e697cd55ab52cc3d5
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 15:13:23 +0800

    changes for tips delete

--- resources/views/admin/manage/tips/partials/delete.blade.php
+++ resources/views/admin/manage/tips/partials/delete.blade.php
@@ -2,14 +2,14 @@
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				Delete Tip
+				{!! trans('messages.admin_delete_tip') !!}
 			</div>
 			<div class="modal-body">
-				Are you sure you want to delete this tip?
+				{!! trans('messages.admin_delete_tip_msg') !!}
 			</div>
 			<div class="modal-footer">
 				<div class="btncon col-xs-8 col-xs-offset-4 pull-left">
-					{!! Form::button('Yes'
+					{!! Form::button('trans('messages.yes')'
 						, array(
 							'class' => 'btn btn-blue btn-medium'
 							, 'data-dismiss' => 'modal'
@@ -18,7 +18,7 @@
 						)
 					) !!}
 
-					{!! Form::button('No'
+					{!! Form::button('trans('messages.no')'
 						, array(
 							'class' => 'btn btn-gold btn-medium'
 							, 'data-dismiss' => 'modal'
