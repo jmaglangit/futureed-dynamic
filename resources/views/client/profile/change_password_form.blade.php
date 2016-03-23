55c2ce27ca802e979c02bc5fe26ea23e548b68b8
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 09:36:29 +0800

    changes for localisation client profile change password

--- resources/views/client/profile/change_password_form.blade.php
+++ resources/views/client/profile/change_password_form.blade.php
@@ -1,50 +1,50 @@
 {!! Form::open(array('id' => 'client_change_pass_form'
 	, 'class' => 'form-horizontal', 'ng-if' => 'profile.active_password && !profile.password_changed'))!!}
 	<div class="form-group">
-		<label class="col-xs-3 control-label">Current Password <span class="required">*</span></label>
+		<label class="col-xs-3 control-label">{!! trans('messages.current_password') !!} <span class="required">*</span></label>
 		<div class="col-xs-6">
     		{!! Form::password('password'
     			, array(
         			'class' => 'form-control'
-        			, 'placeholder' => 'Current Password'
+        			, 'placeholder' => 'trans('messages.current_password')'
         			, 'ng-model' => 'profile.change.password')
         		)!!}
         </div>		
 	</div>
 	<div class="form-group">
-		<label class="col-xs-3 control-label">New Password <span class="required">*</span></label>
+		<label class="col-xs-3 control-label">{!! trans('messages.new_password') !!} <span class="required">*</span></label>
 		<div class="col-xs-6">
     		{!! Form::password('new_password'
     			, array(
         			'class' => 'form-control'
-        			, 'placeholder' => 'New Password'
+        			, 'placeholder' => 'trans('messages.new_password')'
         			, 'ng-model' => 'profile.change.new_password'
         			, 'ng-model-option' => "{debounce: {'default' : 10000} }") 
         		)!!}
-        	<p class="help-block">Password must be at least 8 characters and with at least 1 number.</p>
+        	<p class="help-block">{!! trans('messages.password_limit') !!}</p>
         </div>
 	</div>
 	<div class="form-group">
-		<label class="col-xs-3 control-label">Confirm Password <span class="required">*</span></label>
+		<label class="col-xs-3 control-label">{!! trans('messages.confirm_password') !!} <span class="required">*</span></label>
 		<div class="col-xs-6">
     		{!! Form::password('confirm_password'
     			, array(
         			'class' => 'form-control'
-        			, 'placeholder' => 'Confirm Password'
+        			, 'placeholder' => 'trans('messages.confirm_password')'
         			, 'ng-model' => 'profile.change.confirm_password'
         			, 'ng-model-option' => "{debounce: {'default' : 10000} }") 
         		)!!}
         </div>		
 	</div>
 	<div class="btn-container">
-		{!! Form::button('Save'
+		{!! Form::button('trans('messages.save')'
             , array(
                 'class' => 'btn btn-blue btn-medium'
                 , 'ng-click' => "profile.changeClientPassword()"
             )
         ) !!}
 
-        {!! Form::button('Cancel'
+        {!! Form::button('trans('messages.cancel')'
             , array(
                 'class' => 'btn btn-gold btn-medium'
                 , 'ng-click' => "profile.setClientProfileActive('index')"
@@ -55,11 +55,11 @@
 
 <div ng-if="profile.active_password && profile.password_changed">
 	<div class="alert alert-success">
-		You have successfully changed your password.
+		{!! trans('messages.client_successfully_change_password') !!}
 	</div>
 
 	<div class="btn-container">
-        {!! Form::button('View Profile'
+        {!! Form::button('trans('messages.view_profile') !!}'
             , array(
                 'class' => 'btn btn-blue btn-medium'
                 , 'ng-click' => "profile.setClientProfileActive('index')"
