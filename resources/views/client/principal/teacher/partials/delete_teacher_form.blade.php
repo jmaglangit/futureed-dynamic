ddbbe057de20eb7401bce88faeed3f4d7cdd34ea
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 09:40:45 +0800

    changes for localisation for principal teacher delete

--- resources/views/client/principal/teacher/partials/delete_teacher_form.blade.php
+++ resources/views/client/principal/teacher/partials/delete_teacher_form.blade.php
@@ -2,14 +2,14 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
         <div class="modal-header">
-            Delete Teacher
+            {!! trans('messages.client_delete_teacher') !!}
         </div>
         <div class="modal-body">
-            Are you sure you want to delete this teacher account?
+            {!! trans('messages.client_delete_teacher_msg') !!}
         </div>
         <div class="modal-footer">
         	<div class="btncon col-xs-8 col-xs-offset-4 pull-left">
-                {!! Form::button('Yes'
+                {!! Form::button('trans('messages.yes')'
                     , array(
                         'class' => 'btn btn-blue btn-medium'
                         , 'ng-click' => 'teacher.delete()'
@@ -17,7 +17,7 @@
                     )
                 ) !!}
 
-                {!! Form::button('No'
+                {!! Form::button('trans('messages.no')'
                     , array(
                         'class' => 'btn btn-gold btn-medium'
                         , 'data-dismiss' => 'modal'
