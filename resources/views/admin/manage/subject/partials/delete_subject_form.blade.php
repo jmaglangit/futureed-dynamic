814467b144406ec50ff7124157d28c63aebfac80
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 15:50:00 +0800

    changes for localisation for admin delete subject

--- resources/views/admin/manage/subject/partials/delete_subject_form.blade.php
+++ resources/views/admin/manage/subject/partials/delete_subject_form.blade.php
@@ -2,14 +2,14 @@
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
         <div class="modal-header">
-            Delete Subject
+            {!! trans('messages.admin_delete_subject') !!}
         </div>
         <div class="modal-body">
-            Are you sure you want to delete this subject?
+            {!! trans('messages.admin_delete_subject_msg') !!}
         </div>
         <div class="modal-footer">
         	<div class="btncon col-md-8 col-md-offset-4 pull-left">
-                {!! Form::button('Yes'
+                {!! Form::button('trans('messages.yes')'
                     , array(
                         'class' => 'btn btn-blue btn-medium'
                         , 'ng-click' => 'subject.deleteSubject()'
@@ -17,7 +17,7 @@
                     )
                 ) !!}
 
-                {!! Form::button('No'
+                {!! Form::button('trans('messages.no')'
                     , array(
                         'class' => 'btn btn-gold btn-medium'
                         , 'data-dismiss' => 'modal'
