8c850c52aa3a9a5fdba5028541c0443ce4690472
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 11:13:10 +0800

    changes for client parent dashboard

--- resources/views/client/parent/dashboard/index.blade.php
+++ resources/views/client/parent/dashboard/index.blade.php
@@ -1,21 +1,21 @@
 <div ng-if="user.role == futureed.PARENT" ng-controller="ManageParentReportsController as reports" ng-init="reports.setActive()">
 	<div ng-if="!reports.active_report" class="dashboard-content" ng-cloak>
 		<p>
-			To get started on using Future Lesson, you need to add a student, click
-			<a href="{!! route('client.parent.student.index') !!}"> student</a>.
+			{!! trans('messages.client_parent_dashboard_msg_1') !!}
+			<a href="{!! route('client.parent.student.index') !!}"> {!! trans('messages.student') !!}</a>.
 		</p>
 
-		<p>If you already added a Student, you can
-			<a href="{!! route('client.parent.payment.index') !!}"> buy a subject</a> for the your student
+		<p>{!! trans('messages.client_parent_dashboard_msg_2') !!}
+			<a href="{!! route('client.parent.payment.index') !!}"> {!! trans('messages.client_parent_dashboard_msg_3') !!}</a> {!! trans('messages.client_parent_dashboard_msg_4') !!}
 		</p>
 
-		<p>You can also
-			<a href="{!! route('client.parent.module.index') !!}"> review</a> the lessons and practice questions.
+		<p>{!! trans('messages.client_parent_dashboard_msg_5') !!}
+			<a href="{!! route('client.parent.module.index') !!}"> {!! trans('messages.client_parent_dashboard_msg_6') !!}</a> {!! trans('messages.client_parent_dashboard_msg_7') !!}
 		</p>
 	</div>
 
 	<div ng-if="reports.active_report" class="reports">
-		<h3>Student Report</h3>
+		<h3>{!! trans('messages.student_report') !!}</h3>
 			<div class="student-search">
 				<select ng-model="reports.student_id"
 					ng-change="reports.changeStudentId()"
@@ -33,25 +33,25 @@
 				<li role="presentation" ng-class="{ 'active' : reports.active_report_card }">
 					<a href="javascript:void(0)" ng-click="reports.setActive(futureed.REPORT_CARD)">
 						<i class="fa fa-file-text-o"></i>
-						 Report Card
+						 {!! trans('messages.report_card') !!}
 					 </a>
 				</li>
 				<li role="presentation" ng-class="{ 'active' : reports.active_summary_progress }">
 					<a href="javascript:void(0)" ng-click="reports.setActive(futureed.SUMMARY_PROGRESS)">
 						<i class="fa fa-list-ul"></i>
-						 Summary Progress
+						 {!! trans('messages.summary_progress') !!}
 					</a>
 				</li>
 				<li role="presentation" ng-class="{ 'active' : reports.active_subject_area }">
 					<a href="javascript:void(0)" ng-click="reports.setActive(futureed.SUBJECT_AREA)">
 						<i class="fa fa-book"></i>
-						 Subject Area
+						 {!! trans('messages.subject_area') !!}
 					</a>
 				</li>
 				<li role="presentation" ng-class="{ 'active' : reports.active_current_learning }">
 					<a href="javascript:void(0)" ng-click="reports.setActive(futureed.CURRENT_LEARNING)">
 						<i class="fa fa-bar-chart"></i>
-						 Current Learning
+						 {!! trans('messages.current_learning') !!}
 					</a>
 				</li>
 			</ul>
