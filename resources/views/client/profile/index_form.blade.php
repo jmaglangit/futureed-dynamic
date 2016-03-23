67038e5312ce5cb4a520794f062cd70e7639144e
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 09:14:07 +0800

    changes for localisation for client profile

--- resources/views/client/profile/index_form.blade.php
+++ resources/views/client/profile/index_form.blade.php
@@ -1,15 +1,15 @@
 {!! Form::open(array('id' => 'client_profile_form', 'class' => 'form-horizontal', 'ng-if' => 'profile.active_index || profile.active_edit')) !!}
 	<fieldset>
 		<legend class="legend-name-mid">
-			User Credentials
+			{!! trans('messages.user_credentials') !!}
 		</legend>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">Username <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.username') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('username', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Username' 
+						, 'placeholder' => 'trans('messages.username')' 
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.username'
 						, 'ng-model-options' => "{debounce : {'default' : 1000} }"
@@ -25,13 +25,13 @@
 			</div>		
 		</div>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">Email <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.email') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				<div class="input-group"> 
 					{!! Form::text('email', ''
 						, array(
 							'class' => 'form-control'
-							, 'placeholder' => 'Email Address' 
+							, 'placeholder' => 'trans('messages.email_address')' 
 							, 'readonly' => 'readonly' 
 							, 'ng-model' => 'profile.prof.email'
 						)
@@ -43,46 +43,46 @@
 		</div>
 		
 		<div class="form-group" ng-if='profile.prof.new_email'>
-			<label for="" class="col-xs-3 control-label" style="color:#7F7504">Pending Email <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label" style="color:#7F7504">{!! trans('messages.pending_email') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('email', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Email Address' 
+						, 'placeholder' => 'trans('messages.email_address')' 
 						, 'readonly' => 'readonly' 
 						, 'ng-model' => 'profile.prof.new_email'
 					)
 				) !!}
 			</div>
 			<div class="col-xs-2">
-				<a href="" ng-click="profile.setClientProfileActive('confirm_email')" class="edit-email">Confirm</a>
+				<a href="" ng-click="profile.setClientProfileActive('confirm_email')" class="edit-email">{!! trans('messages.confirm') !!}</a>
 			</div>	
 		</div>
 	</fieldset>
 
 	<fieldset>
 		<legend class="legend-name-mid">
-			Personal Information
+			{!! trans('messages.personal_info') !!}
 		</legend>
 		<div class="form-group">
-				<label for="" class="col-xs-3 control-label">First Name <span class="required">*</span></label>
+				<label for="" class="col-xs-3 control-label">{!! trans('messages.first_name') !!} <span class="required">*</span></label>
 				<div class="col-xs-5">
 					{!! Form::text('first_name', ''
 						, array(
 							'class' => 'form-control'
-							, 'placeholder' => 'First Name'
+							, 'placeholder' => 'trans('messages.first_name')'
 							, 'ng-disabled' => '!profile.active_edit' 
 							, 'ng-model' => 'profile.prof.first_name')
 					) !!}
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="" class="col-xs-3 control-label">Last Name <span class="required">*</span></label>
+				<label for="" class="col-xs-3 control-label">{!! trans('messages.last_name') !!} <span class="required">*</span></label>
 				<div class="col-xs-5">
 					{!! Form::text('last_name', ''
 						, array(
 							'class' => 'form-control'
-							, 'placeholder' => 'Last Name'
+							, 'placeholder' => 'trans('messages.last_name')'
 							, 'ng-disabled' => '!profile.active_edit' 
 							, 'ng-model' => 'profile.prof.last_name')
 					) !!}
@@ -92,15 +92,15 @@
 
 	<fieldset ng-if="profile.is_teacher">
 		<legend class="legend-name-mid">
-			School Information
+			{!! trans('messages.school_info') !!}
 		</legend>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">School Name <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.school_name') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('school_name', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'School Name'
+						, 'placeholder' => 'trans('messages.school_name')'
 						, 'ng-disabled' => 'true' 
 						, 'ng-model' => 'profile.prof.school_name')
 				) !!}
@@ -110,83 +110,83 @@
 
 	<fieldset ng-if="profile.is_principal">
 		<legend class="legend-name-mid">
-			School Information
+			{!! trans('messages.school_info') !!}
 		</legend>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">School Name <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.school_name') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('school_name', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'School Name'
+						, 'placeholder' => 'trans('messages.school_name')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.school_name')
 				) !!}
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">Street Address <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.street_address') !!} <span class="required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('school_street_address', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Address'
+						, 'placeholder' => 'trans('messages.street_address')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.school_street_address')
 				) !!}
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">City </label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.city') !!} </label>
 			<div class="col-xs-3">
 				{!! Form::text('school_city', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'City'
+						, 'placeholder' => 'trans('messages.city')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.school_city')
 				) !!}
 			</div>
-			<label for="" class="col-xs-3 control-label">State <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.state') !!} <span class="required">*</span></label>
 			<div class="col-xs-3">
 				{!! Form::text('school_state', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'State'
+						, 'placeholder' => 'trans('messages.state')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.school_state')
 				) !!}
 			</div>
 		</div>
 		<div class="form-group" ng-init="getCountries()">
-			<label for="" class="col-xs-3 control-label">Postal Code</label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.postal_code') !!}</label>
 			<div class="col-xs-3">
 				{!! Form::text('school_zip', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Postal Code'
+						, 'placeholder' => 'trans('messages.postal_code')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.school_zip')
 				) !!}
 			</div>
-			<label for="" class="col-xs-3 control-label">Country <span class="required">*</span></label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.country') !!} <span class="required">*</span></label>
 			<div class="col-xs-3">
 				<select name="school_country_id" class="form-control" ng-model="profile.prof.school_country_id" ng-disabled="!profile.active_edit">
-					<option ng-selected="profile.prof.school_country_id == futureed.FALSE" value="">-- Select Country --</option>
+					<option ng-selected="profile.prof.school_country_id == futureed.FALSE" value="">{!! trans('messages.select_country') !!}</option>
 					<option ng-selected="profile.prof.school_country_id == country.id" ng-repeat="country in countries" ng-value="country.id">{! country.name!}</option>
 				</select>
 			</div>
 		</div>
 		<legend class="legend-name-mid">
-			School Contact Information
+			{!! trans('messages.school_contact_info') !!}
 		</legend>
 		<div class="form-group">
-			<label class="col-xs-3 control-label">Contact Person <span class="required">*</span></label>
+			<label class="col-xs-3 control-label">{!! trans('messages.contact_person') !!} <span class="required">*</span></label>
 			<div class="col-xs-6">
 			  {!! Form::text('school_contact_name', ''
 				  , array(
 					'class' => 'form-control'
-					, 'placeholder' => 'Contact Person'
+					, 'placeholder' => 'trans('messages.contact_person')'
 					, 'ng-disabled' => '!profile.active_edit' 
 					, 'ng-model' => 'profile.prof.school_contact_name'
 				  )
@@ -195,12 +195,12 @@
 		</div>
 		
 		<div class="form-group">
-			<label class="col-xs-3 control-label">Contact Number <span class="required">*</span></label>
+			<label class="col-xs-3 control-label">{!! trans('messages.contact_number') !!} <span class="required">*</span></label>
 			<div class="col-xs-6">
 			  {!! Form::text('school_contact_number', ''
 				  , array(
 					'class' => 'form-control'
-					, 'placeholder' => 'Contact Number'
+					, 'placeholder' => 'trans('messages.contact_number')'
 					, 'ng-disabled' => '!profile.active_edit' 
 					, 'ng-model' => 'profile.prof.school_contact_number'
 				  )
@@ -210,60 +210,60 @@
 	</fieldset>
 	<fieldset>
 		<legend class="legend-name-mid" ng-if="!profile.is_parent">
-			Other Address Information(Optional)
+			{!! trans('messages.other_address_info') !!}
 		</legend>
 		<legend class="legend-name-mid" ng-if="profile.is_parent">
-			Address Information
+			{!! trans('messages.address_info') !!}
 		</legend>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">Street Address</label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.street_address') !!} <span class="required" ng-if="profile.is_required">*</span></label>
 			<div class="col-xs-5">
 				{!! Form::text('street_address', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Address'
+						, 'placeholder' => 'trans('messages.street_address')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.street_address')
 				) !!}
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="" class="col-xs-3 control-label">City</label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.city') !!} <span class="required" ng-if="profile.is_required">*</span></label>
 			<div class="col-xs-3">
 				{!! Form::text('city', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'City'
+						, 'placeholder' => 'trans('messages.city')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.city')
 				) !!}
 			</div>
-			<label for="" class="col-xs-3 control-label">State </label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.state') !!} </label>
 			<div class="col-xs-3">
 				{!! Form::text('state', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'State'
+						, 'placeholder' => 'trans('messages.state')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.state')
 				) !!}
 			</div>
 		</div>
 		<div class="form-group" ng-init="getCountries()">
-			<label for="" class="col-xs-3 control-label">Postal Code </label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.postal_code') !!} </label>
 			<div class="col-xs-3">
 				{!! Form::text('zip', ''
 					, array(
 						'class' => 'form-control'
-						, 'placeholder' => 'Postal Code'
+						, 'placeholder' => 'trans('messages.postal_code')'
 						, 'ng-disabled' => '!profile.active_edit' 
 						, 'ng-model' => 'profile.prof.zip')
 				) !!}
 			</div>
-			<label for="" class="col-xs-3 control-label">Country</label>
+			<label for="" class="col-xs-3 control-label">{!! trans('messages.country') !!} <span class="required" ng-if="profile.is_required">*</span></label>
 			<div class="col-xs-3">
 				<select name="country_id" class="form-control" ng-model="profile.prof.country_id" ng-disabled="!profile.active_edit">
-					<option ng-selected="profile.prof.country_id == futureed.FALSE" value="">-- Select Country --</option>
+					<option ng-selected="profile.prof.country_id == futureed.FALSE" value="">{!! trans('messages.select_country') !!}</option>
 					<option ng-selected="profile.prof.country_id == country.id" ng-repeat="country in countries" ng-value="country.id">{! country.name!}</option>
 				</select>
 			</div>
@@ -273,7 +273,7 @@
 	<fieldset>
 		<div class="form-group">
 			<div class="btn-container">
-				{!! Form::button('Edit'
+				{!! Form::button('trans('messages.edit')'
 					, array(
 						'class' => 'btn btn-blue btn-medium'
 						, 'ng-click' => "profile.setClientProfileActive('edit')"
@@ -281,7 +281,7 @@
 					)
 				) !!}
 
-				{!! Form::button('Save'
+				{!! Form::button('trans('messages.save')'
 					, array(
 						'class' => 'btn btn-blue btn-medium'
 						, 'ng-click' => 'profile.saveClientProfile()'
@@ -289,7 +289,7 @@
 					)
 				) !!}
 
-				{!! Form::button('Cancel'
+				{!! Form::button('trans('messages.cancel')'
 					, array(
 						'class' => 'btn btn-gold btn-medium'
 						, 'ng-click' => "profile.setClientProfileActive('index')"
