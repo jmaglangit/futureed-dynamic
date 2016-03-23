b7ab2f380092c3d2f5c69101cf0641afa7ae396a
Farrah Faye Dy <fdy@nerubia.com>
2016-03-23 09:50:13 +0800

    changes for localisation for client main

--- resources/views/client/partials/main-nav.blade.php
+++ resources/views/client/partials/main-nav.blade.php
@@ -8,16 +8,16 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Welcome, {! user.first_name !} <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{!! trans('messages.welcome') !!}, {! user.first_name !} <span class="caret"></span></a>
                         
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{!! route('client.profile.index') !!}">Profile</a></li>
+                            <li><a href="{!! route('client.profile.index') !!}">{!! trans('messages.profile') !!}</a></li>
                             <li>
                                 <a  ng-if="!user.impersonate" href="javascript:void(0)" ng-click="logout(user.id, futureed.CLIENT, '{!! route('client.logout') !!}')">
-                                <span>Logout</span>
+                                <span>{!! trans('messages.logout') !!}</span>
                             </a>
                                 <a ng-if="user.impersonate" href="javascript:void(0)" ng-click="stopImpersonate(user.user_id, '{!! route('client.logout') !!}')">
-                                    <span >Stop Impersonating</span>
+                                    <span>{!! trans('messages.stop_impersonating') !!}</span>
                                 </a>
                             </li>
                         </ul>
