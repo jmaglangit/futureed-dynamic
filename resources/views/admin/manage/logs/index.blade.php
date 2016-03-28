aa1ccfb72945d41dc5e30f690e1d859c87a8f845
Farrah Faye Dy <fdy@nerubia.com>
2016-03-28 14:14:38 +0800

    changes for localisation for admin logs

--- resources/views/admin/manage/logs/index.blade.php
+++ resources/views/admin/manage/logs/index.blade.php
@@ -19,25 +19,25 @@
 			<div class="client-content">
 				<div class="content-title">
 					<div class="title-main-content">
-						<span ng-if="logs.active_security_log">Security Logs</span>
-						<span ng-if="logs.active_administrator_log">Administrator Logs</span>
-						<span ng-if="logs.active_users_log">Users Logs</span>
-						<span ng-if="logs.active_system_log">System Logs</span>
-						<span ng-if="logs.active_errors_log">Error Logs</span>
+						<span ng-if="logs.active_security_log">{!! trans('messages.admin_security_logs') !!}</span>
+						<span ng-if="logs.active_administrator_log">{!! trans('messages.admin_administrator_logs') !!}</span>
+						<span ng-if="logs.active_users_log">{!! trans('messages.admin_user_logs') !!}</span>
+						<span ng-if="logs.active_system_log">{!! trans('messages.admin_system_logs') !!}</span>
+						<span ng-if="logs.active_errors_log">{!! trans('messages.admin_error_logs') !!}</span>
 					</div>
 				</div>
 
 			    <ul class="nav nav-pills nav-admin">
 					<li role="presentation" ng-class="{ 'active' : logs.active_security_log }">
-						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.SECURITY)">Security</a></li>
+						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.SECURITY)">{!! trans('messages.admin_security') !!}</a></li>
 					<li role="presentation" ng-class="{ 'active' : logs.active_administrator_log }">
-						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.ADMINISTRATOR)">Administrator</a></li>
+						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.ADMINISTRATOR)">{!! trans('messages.admin_administrator') !!}</a></li>
 					<li role="presentation" ng-class="{ 'active' : logs.active_users_log }">
-						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.USERS)">Users</a></li>
+						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.USERS)">{!! trans('messages.admin_user') !!}</a></li>
 					<li role="presentation" ng-class="{ 'active' : logs.active_system_log }">
-						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.SYSTEM)">System</a></li>
+						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.SYSTEM)">{!! trans('messages.admin_system') !!}</a></li>
 					<li role="presentation" ng-class="{ 'active' : logs.active_errors_log }">
-						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.ERRORS)">Errors</a></li>
+						<a href="javascript:void(0)" ng-click="logs.setActive(futureed.ERRORS)">{!! trans('messages.admin_error') !!}</a></li>
 				</ul>
 
 				<div template-directive template-url="{!! route('admin.manage.logs.partials.security_list_form') !!}"></div>
