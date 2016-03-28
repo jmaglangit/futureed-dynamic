877147a89fa4e078a0abb1e1921c5c73566807dc
jmaglangit <jmaglangit@nerubia.com>
2016-03-28 17:26:03 +0800

    added controller for background image

--- app/Http/Controllers/Api/v1/BackgroundImageController.php
+++ app/Http/Controllers/Api/v1/BackgroundImageController.php
@@ -1,11 +1,22 @@
 <?php namespace FutureEd\Http\Controllers\Api\v1;
 
 use FutureEd\Http\Requests;
-use FutureEd\Http\Controllers\Controller;
+use FutureEd\Models\Repository\BackgroundImage\BackgroundImageRepositoryInterface;
+use FutureEd\Services\ImageServices;
+use Illuminate\Support\Facades\Input;
 
-use Illuminate\Http\Request;
+class BackgroundImageController extends ApiController {
 
-class BackgroundImageController extends Controller {
+	protected $background_image;
+	protected $image_service;
+
+	public function __construct(
+		BackgroundImageRepositoryInterface $backgroundImageRepositoryInterface,
+		ImageServices $imageServices
+	){
+		$this->background_image = $backgroundImageRepositoryInterface;
+		$this->image_service = $imageServices;
+	}
 
 	/**
 	 * Display a listing of the resource.
@@ -14,7 +25,27 @@ class BackgroundImageController extends Controller {
 	 */
 	public function index()
 	{
-		//
+		$criteria = [];
+
+		if (Input::get('status')) {
+			$criteria['status'] = Input::get('status');
+		}
+
+		$limit = (Input::get('limit')) ? Input::get('limit') : 0;
+
+		$offset = (Input::get('offset')) ? Input::get('offset') : 0;
+
+		$record = $this->background_image->getBackgroundImages($criteria, $limit, $offset);
+
+		//update with url images
+		foreach ($record['records'] as $background_image) {
+
+			if ($background_image->filename) {
+				$background_image->url = $this->image_service->getBackgroundImage($background_image->filename);
+			}
+		}
+
+		return $this->respondWithData($record);
 	}
 
 	/**
@@ -25,6 +56,13 @@ class BackgroundImageController extends Controller {
 	 */
 	public function show($id)
 	{
-		//
+		$record = $this->background_image->getBackgroundImage($id);
+
+		//update with url images
+		if($record->filename){
+			$record->url = $this->image_service->getBackgroundImage($record->filename);
+		}
+
+		return $this->respondWithData($record);
 	}
 }
