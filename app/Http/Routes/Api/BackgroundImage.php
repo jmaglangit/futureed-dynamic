0d05597a218f20c5c43532cb4e5f270ea3a6b144
jmaglangit <jmaglangit@nerubia.com>
2016-03-28 17:22:34 +0800

    added view files and route for background image

---
+++ app/Http/Routes/Api/BackgroundImage.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/28/16
+ * Time: 10:06 AM
+ */
---
+++ resources/views/student/profile/settings.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/23/16
+ * Time: 4:19 PM
+ */
