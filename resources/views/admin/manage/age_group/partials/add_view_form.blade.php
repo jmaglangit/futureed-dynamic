ddff7f992ad9efa11feb2a218c3d9d76cd7b101f
Farrah Faye Dy <fdy@nerubia.com>
2016-03-28 14:49:20 +0800

    changes for age group add view

--- resources/views/admin/manage/age_group/partials/add_view_form.blade.php
+++ resources/views/admin/manage/age_group/partials/add_view_form.blade.php
@@ -15,24 +15,24 @@
 		<div class="col-xs-12 search-container">
 			<fieldset>
 				<legend class="legend-name-mid">
-					Add Age Group
+					{!! trans('messages.admin_add_age_group') !!}
 				</legend>
 				<div class="form-group">
-					<label class="control-label col-xs-3">Age <span class="required">*</span></label>
+					<label class="control-label col-xs-3">{!! trans('messages.age') !!} <span class="required">*</span></label>
 					<div class="col-xs-5" ng-init="age.getAges()">
 						<select class="form-control" ng-model="age.record.age_group_id" 
 							ng-class="{'required-field' : age.fields['age_group_id']}">
-							<option value="">-- Select Age --</option>
+							<option value="">{!! trans('messages.admin_select_age') !!}</option>
 							<option ng-repeat="ageinfo in age.ages" ng-value="ageinfo.id">{! ageinfo.age!}</option>
 						</select>
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="control-label col-xs-3">Total Points Earned <span class="required">*</span></label>
+					<label class="control-label col-xs-3">{!! trans('messages.admin_total_points_earned') !!} <span class="required">*</span></label>
 					<div class="col-xs-5">
 						{!! Form::text('points_earned',''
 							, array(
-								'placeHolder' => 'Total Points Earned'
+								'placeHolder' => 'trans('messages.admin_total_points_earned')'
 								, 'ng-model' => 'age.record.points_earned'
 								, 'class' => 'form-control'
 								, 'ng-class' => "{ 'required-field' : age.fields['points_earned'] }"
@@ -45,14 +45,14 @@
 			<fieldset>
 				<div class="form-group">
 					<div class="btn-container col-xs-9 col-xs-offset-1">
-						{!! Form::button('Add'
+						{!! Form::button('trans('messages.add')'
 							, array(
 								'class' => 'btn btn-blue btn-medium'
 								, 'ng-click' => 'age.add()'
 							)
 						) !!}
 
-						{!! Form::button('Cancel'
+						{!! Form::button('trans('messages.cancel')'
 							, array(
 								'class' => 'btn btn-gold btn-medium'
 								, 'ng-click' => "age.setActive()"
