359feec08773e3b67936c07060d5151a8aac2db8
jmaglangit <jmaglangit@nerubia.com>
2016-03-28 17:28:16 +0800

    added controller functions for student background images

--- app/Http/Controllers/Api/v1/StudentBackgroundImageController.php
+++ app/Http/Controllers/Api/v1/StudentBackgroundImageController.php
@@ -1,40 +1,64 @@
 <?php namespace FutureEd\Http\Controllers\Api\v1;
 
 use FutureEd\Http\Requests;
-use FutureEd\Http\Controllers\Controller;
-
 use FutureEd\Models\Repository\Student\StudentRepositoryInterface;
 use FutureEd\Models\Repository\User\UserRepositoryInterface;
 use FutureEd\Http\Requests\Api\StudentBackgroundImageRequest;
+use FutureEd\Services\ImageServices;
 
-class StudentBackgroundImageController extends Controller {
+class StudentBackgroundImageController extends ApiController {
 
 	protected $student;
 	protected $user;
+	protected $image_service;
 
 	public function __construct(
 		StudentRepositoryInterface $studentRepositoryInterface,
-		UserRepositoryInterface $userRepositoryInterface
+		UserRepositoryInterface $userRepositoryInterface,
+		ImageServices $imageServices
 	){
 		$this->student = $studentRepositoryInterface;
 		$this->user = $userRepositoryInterface;
+		$this->image_service = $imageServices;
 	}
 
-	//get student background image settings.
+	/**
+	 * Get student background image settings.
+	 * @param $id
+	 * @return mixed
+	 */
 	public function show($id){
 
 		//get student background image
 		$student_background = $this->user->getBackgroundImage(config('futureed.student'),$id);
 
-		dd($student_background);
-		//translate the image
+		$response = 0;
 
+		//if student has background image.
+		if($student_background[0]->background_image){
+
+			$response = $student_background[0]->background_image;
+
+			//get image service to get background image
+			$response->url = $this->image_service->getBackgroundImage($response->filename);
+		}
+
+		return $this->respondWithData($response);
 	}
 
-	//update student background image settings.
-	public function edit(StudentBackgroundImageRequest $request){
+	/**
+	 * update student background image settings.
+	 * @param $id
+	 * @param StudentBackgroundImageRequest $request
+	 * @return mixed
+	 */
+	public function update($id,StudentBackgroundImageRequest $request){
 
+		$background_image_id = $request->get('background_image_id');
 
+		$response = $this->user->updateBackgroundImage($id,$background_image_id);
+
+		return $this->respondWithData($response);
 	}
 
 }
