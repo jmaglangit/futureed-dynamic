876e3b16519760ef24ad3818cbd7b2335bc63393
jmaglangit <jmaglangit@nerubia.com>
2016-03-28 17:46:32 +0800

    added settings content view

--- resources/views/student/profile/settings.blade.php
+++ resources/views/student/profile/settings.blade.php
@@ -1,7 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 3/23/16
- * Time: 4:19 PM
- */
+<div ng-if="profile.settings">
+    <div class="alert alert-error" ng-if="profile.errors">
+        <p>
+            {! error !}
+        </p>
+    </div>
+
+<fieldset>
+    <legend>Current Background Image</legend>
+    <div class="form-group">
+        <label for="" class="col-xs-3 control-label">Background Image</label>
+        <img ng-src="{! profile.background_image.url !}" class="bg-image-item" alt="{! profile.background_image.name !}">
+    </div>
+
+</fieldset>
+<fieldset>
+    <legend>Select New Background Image</legend>
+    <ul class="bg-image-list list-unstyled list-inline ng-scope" ng-init="profile.getBackgroundImage()">
+        <li class="item bg-image-item"
+            ng-repeat="image in profile.background_image_list"
+            ng-click="profile.updateStudentBackgroundImage(image)">
+            <img ng-src="{! image.url !}" alt="">
+        </li>
+    </ul>
+
+</fieldset>
+</div>
