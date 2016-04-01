fb5124b7229c4ed2d6ad05f56b18510865fe722a
Roy <rreynes@nerubia.com>
2016-04-01 20:19:54 +0800

    added trial module list message

--- resources/views/student/class/partials/trial_question_list.blade.php
+++ resources/views/student/class/partials/trial_question_list.blade.php
@@ -11,8 +11,8 @@
                 <div class="list-container" ng-cloak>
                     <div class="clearfix"></div>
 
-                    <div class="module-list">
-                        <div class="module-item">
+                    <div class="module-list row">
+                        <div class="module-item col-xs-2">
                             <div class="module-image-holder">
                                 <img class="module-icon"
                                      ng-src="{!! url('images/trial-module/images/module/icon-spatial.png') !!}"
@@ -28,6 +28,14 @@
                                     ng-click="class.redirect('{!! route('student.class.module.trial-index') !!}')"
                                     class="btn btn-blue module-btn margin-bottom-15"><i class="fa fa-pencil"></i> Begin </button>
 
+                        </div>
+                        <div class="col-xs-8">
+                            <h4 class="margin-top-30 trial-module-message">
+                                This is a Trial Module. If you would like to subscribe, please proceed to the Payment Page
+                            </h4>
+                            <h4 class="trial-module-message">
+                                OR have a parent or teacher invite you to a class.
+                            </h4>
                         </div>
                     </div>
                 </div>
