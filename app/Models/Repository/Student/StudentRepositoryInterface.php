5ea7d82ac098bbbb7fa4728251793efef436cb95
jmaglangit <jmaglangit@nerubia.com>
2016-04-01 15:59:09 +0800

    created interface function for lsp take

--- app/Models/Repository/Student/StudentRepositoryInterface.php
+++ app/Models/Repository/Student/StudentRepositoryInterface.php
@@ -61,4 +61,6 @@ interface StudentRepositoryInterface {
     public function getStudentPointsUsed($user_id);
 
     public function getStudentLSP($student_id);
+
+    public function getStudentLSPDate($student_id);
 }
