7403eb44f0255c3124cec3abb8d262c42a32e343
Roy <rreynes@nerubia.com>
2016-04-05 17:02:13 +0800

    fixes on redirect error when student exits the module

--- resources/views/student/class/module/partials/questions/list.blade.php
+++ resources/views/student/class/module/partials/questions/list.blade.php
@@ -4,12 +4,9 @@
 			{{--Header--}}
 			<div class="row questions-header col-xs-12">
 				<div class="row col-xs-3">
-					<button type="button" class="btn btn-gold next-btn left-0"
-							ng-if="!mod.errors && !mod.record.module_done"
-							ng-click="mod.exitModule('{!! route('student.dashboard.index') !!}')">Exit Module</button>
-					<button type="button" class="btn btn-gold next-btn left-0"
-							ng-if="mod.errors && !mod.record.module_done"
-							ng-click="mod.exitModule('{!! route('student.dashboard.index') !!}')">Exit Module</button>
+					<button type="button" class="btn btn-gold next-btn left-0" ng-click="mod.exitModule('{!! route('student.class.index') !!}')">
+						Exit Module
+					</button>
 				</div>
 
 				<div class="row col-xs-6">
