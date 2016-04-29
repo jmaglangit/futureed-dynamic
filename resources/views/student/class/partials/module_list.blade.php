c30d272cf4c112f77ce4d23d956b8b350c0b7eae
Roy <rreynes@nerubia.com>
2016-04-29 11:55:01 +0800

    changed the pagination button in module list

--- resources/views/student/class/partials/module_list.blade.php
+++ resources/views/student/class/partials/module_list.blade.php
@@ -60,7 +60,7 @@
 	</div>
 
 	<div class="col-xs-12 class-container">
-		<ul class="nav nav-pills module-pills" role="tablist" ng-init="class.listClass()">
+		<ul class="nav nav-pills module-pills" role="tablist">
 			<li role="presentation" class="module-tabs" ng-repeat="aClass in class.classes track by $index" ng-class="{ 'active' : aClass.class_id == class.current_class }"
 				ng-click="class.redirectClass('{!! route('student.class.index') !!}', aClass.class_id)">
 				<a href="#home" aria-controls="home" role="tab" data-toggle="tab" >{! aClass.classroom.subject.name !}</a>
@@ -71,7 +71,7 @@
 		</ul>
 
 		<div class="tab-content">
-		    <div class="tab-pane active">
+			<div class="tab-pane active">
 				<div class="list-container" ng-cloak>
 					<div class="clearfix"></div>
 
@@ -91,59 +91,58 @@
 						<div class="module-item" ng-repeat="record in class.records">
 							<div class="module-image-holder">
 								<img ng-if="record.module_status != 'Completed' && user.points >= record.points_to_unlock" class="module-icon"
-									ng-src="{! record.icon_image == futureed.NONE && '/images/icons/default-module-icon.png' || record.icon_image !}"
-									ng-click="class.redirect('{!! route('student.class.module.index') !!}', record)" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
+									 ng-src="{! record.icon_image == futureed.NONE && '/images/icons/default-module-icon.png' || record.icon_image !}"
+									 ng-click="class.redirect('{!! route('student.class.module.index') !!}', record)" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
 
 								<img ng-if="record.module_status !== 'Completed' && user.points < record.points_to_unlock" class="locked-module-icon"
-									ng-src="/images/icons/icon-lock.png" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
+									 ng-src="/images/icons/icon-lock.png" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
 
 								<img ng-if="record.module_status == 'Completed'" class="locked-module-icon"
-									ng-src="{! record.icon_image == futureed.NONE && '/images/icons/default-module-icon.png' || record.icon_image !}" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
+									 ng-src="{! record.icon_image == futureed.NONE && '/images/icons/default-module-icon.png' || record.icon_image !}" tooltip-class="module-tooltip" tooltip-placement="bottom" tooltip="{! record.name !}">
 							</div>
 
 							<p class="module-name">{! record.name !}</p>
 
 							<button ng-if="record.module_status == 'On Going' && user.points >= record.points_to_unlock"
-								ng-click="class.redirect('{!! route('student.class.module.index') !!}', record)" 
-								type="button" class="btn btn-blue module-btn"><i class="fa fa-play-circle"></i> {!! trans('messages.resume') !!} </button>
+									ng-click="class.redirect('{!! route('student.class.module.index') !!}', record)"
+									type="button" class="btn btn-blue module-btn"><i class="fa fa-play-circle"></i> {!! trans('messages.resume') !!} </button>
 
 							<button ng-if="!record.module_status && user.points >= record.points_to_unlock" ng-click="class.redirect('{!! route('student.class.module.index') !!}', record)"
-								type="button" class="btn btn-blue module-btn"><i class="fa fa-pencil"></i> {!! trans('messages.begin') !!} </button>
+									type="button" class="btn btn-blue module-btn"><i class="fa fa-pencil"></i> {!! trans('messages.begin') !!} </button>
 
 							<button ng-if="user.points < record.points_to_unlock"
-								type="button" class="btn btn-blue module-btn" ng-disabled="true"><i class="fa fa-lock"></i> {!! trans('messages.locked') !!}</button>
+									type="button" class="btn btn-blue module-btn" ng-disabled="true"><i class="fa fa-lock"></i> {!! trans('messages.locked') !!}</button>
 
 							<button ng-if="record.module_status == 'Completed'"
-								type="button" class="btn btn-blue module-btn" ng-disabled="true"><i class="fa fa-lock"></i> {!! trans('messages.completed') !!}</button>
+									type="button" class="btn btn-blue module-btn" ng-disabled="true"><i class="fa fa-lock"></i> {!! trans('messages.completed') !!}</button>
 
 							<div class="progress">
 								<div class="progress-bar progress-bar-striped" role="progressbar" aria-valuemin="0" aria-valuemax="100"
-									ng-class="{ 
+									 ng-class="{
 										'progress-bar-success' : record.progress > 75,
 										'progress-bar-info' : record.progress > 50 && record.progress <= 75 ,
 										'progress-bar-warning' : record.progress > 25 && record.progress <= 50 ,
 										'progress-bar-danger' : record.progress <= 25,
 
 									}"
-									ng-style="{ 'width' : record.progress+'%' }">
+									 ng-style="{ 'width' : record.progress+'%' }">
 								</div>
 							</div>
 							<span class="module-progress">{! record.progress !}%</span>
 						</div>
 					</div>
-					
-					<div class="pull-right" ng-if="class.records.length">
-						<pagination 
-							total-items="class.table.total_items" 
-							ng-model="class.table.page"
-							max-size="3"
-							items-per-page="class.table.size" 
-							previous-text = "&lt;"
-							next-text="&gt;"
-							class="pagination" 
-							boundary-links="true"
-							ng-change="class.paginateByPage()">
-						</pagination>
+
+					<div ng-if="class.records.length">
+						<div class="previous-btn-position"
+							 ng-if="class.table.page > 1">
+							<span  ng-click="class.previousPage()"
+								   ng-model="class.table.page">&lt;</span>
+						</div>
+						<div class="next-btn-position"
+							 ng-if="class.total_module_items_loaded < class.table.total_items">
+							<span  ng-click="class.nextPage()"
+								   ng-model="class.table.page">&gt;</span>
+						</div>
 					</div>
 				</div>
 			</div>
