93ca17b4527d6d0e87ed3995751c5c9162c3c767
jmaglangit <jmaglangit@nerubia.com>
2016-05-13 19:41:46 +0800

    added function for change of elements

--- public/js/common/form_service.js
+++ public/js/common/form_service.js
@@ -1,3 +1,19 @@
-/**
- * Created by jason on 5/13/16.
- */
+
+
+function labelToForm(){
+
+    //change child
+    $(".form-label").replaceWith(function(){
+            return  '<input class="' + this.classList.value + '" type="text" value="'
+                + this.textContent +'" name="'+ this.getAttribute('name') +'">';
+    });
+}
+
+function formToLabel(){
+
+    //change child expects form
+    $(".form-label").replaceWith(function(){
+            return '<label class="' + this.classList.value
+                + '" name='+ this.getAttribute('name') + '>'+ this.value +'</label>';
+    });
+}
