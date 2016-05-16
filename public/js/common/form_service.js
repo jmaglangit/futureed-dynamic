e0d016f249cd1e9cee3aa8a62d6dd58dd6d4dc53
jmaglangit <jmaglangit@nerubia.com>
2016-05-16 15:13:31 +0800

    updated initialy for country drop-down

--- public/js/common/form_service.js
+++ public/js/common/form_service.js
@@ -4,6 +4,9 @@ function labelToForm(){
 
     //change child
     $(".form-label").replaceWith(function(){
+
+        //TODO: check for countries into drop down.
+
             return  '<input class="' + this.classList.value + '" type="text" value="'
                 + this.textContent +'" name="'+ this.getAttribute('name') +'">';
     });
@@ -13,7 +16,20 @@ function formToLabel(){
 
     //change child expects form
     $(".form-label").replaceWith(function(){
+
+        //check if label is country.
+        if(this.getAttribute('name') == 'user_country'){
+
+            // TODO: get countries from api -- self.country_list
+        //<option value="volvo">Volvo</option> TODO: generate for each country.
+
+
+            return '<select class="' + this.classList.value
+                + '" name='+ this.getAttribute('name') + '>'+ this.value +'</select>';
+        }else {
             return '<label class="' + this.classList.value
                 + '" name='+ this.getAttribute('name') + '>'+ this.value +'</label>';
+        }
+
     });
 }
