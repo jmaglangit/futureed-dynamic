f731e0f91c1128a5d0ba54994482e1c39961b4d8
jmaglangit <jmaglangit@nerubia.com>
2016-05-25 18:41:43 +0800

    created function to get an explanation

--- app/Models/Repository/AnswerExplanation/AnswerExplanationRepository.php
+++ app/Models/Repository/AnswerExplanation/AnswerExplanationRepository.php
@@ -1,14 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 5/25/16
- * Time: 5:22 PM
- */
+<?php namespace FutureEd\Models\Repository\AnswerExplanation;
 
-namespace FutureEd\Models\Repository\AnswerExplanation;
+use FutureEd\Models\Core\AnswerExplanation;
 
+class AnswerExplanationRepository implements  AnswerExplanationRepositoryInterface{
 
-class AnswerExplanationRepository {
+	/**
+	 * Get specefic answer explanation.
+	 * @param $module_id
+	 * @param $question_id
+	 * @param $seq_no
+	 * @return mixed
+	 */
+	public function getAnswerExplanation($module_id, $question_id,$seq_no){
+
+		return AnswerExplanation::moduleId($module_id)->questionId($question_id)->seqNo($seq_no)->get();
+	}
 
 }
