d261bf53d6432cdabda9d3dff8d266bf84645663
jmaglangit <jmaglangit@nerubia.com>
2016-05-25 18:36:06 +0800

    initialize classes for answer explanation

---
+++ app/Http/Routes/Api/AnswerExplanation.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 5/25/16
+ * Time: 5:42 PM
+ */
---
+++ app/Models/Repository/AnswerExplanation/AnswerExplanationRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 5/25/16
+ * Time: 5:22 PM
+ */
+
+namespace FutureEd\Models\Repository\AnswerExplanation;
+
+
+class AnswerExplanationRepository {
+
+}
