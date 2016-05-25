57b25788bbe57d61d5626024e77edc85910f4c5e
jmaglangit <jmaglangit@nerubia.com>
2016-05-25 18:39:50 +0800

    added route for answer explanation

--- app/Http/Routes/Api/AnswerExplanation.php
+++ app/Http/Routes/Api/AnswerExplanation.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 5/25/16
- * Time: 5:42 PM
- */
+
+Routes::group([
+//	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+],function(){
+
+	Routes::get('/answer-explanation',[
+		'uses' => 'Api\v1\AnswerExplanationController@getAnswerExplanation',
+		'as' => 'api.v1.answer-explanation.specific'
+	]);
+});
