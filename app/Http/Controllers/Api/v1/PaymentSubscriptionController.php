1215e41f6858414dc0898c39db4ceb4884a69f9e
jmaglangit <jmaglangit@nerubia.com>
2016-05-27 21:02:05 +0800

    updated payment subscriptions

--- app/Http/Controllers/Api/v1/PaymentSubscriptionController.php
+++ app/Http/Controllers/Api/v1/PaymentSubscriptionController.php
@@ -6,12 +6,13 @@ use FutureEd\Models\Repository\Invoice\InvoiceRepositoryInterface;
 use FutureEd\Models\Repository\ClientDiscount\ClientDiscountRepositoryInterface;
 use FutureEd\Models\Repository\Order\OrderRepositoryInterface;
 use FutureEd\Models\Repository\InvoiceDetail\InvoiceDetailRepositoryInterface;
+use FutureEd\Models\Repository\Student\StudentRepositoryInterface;
 use FutureEd\Services\InvoiceServices;
 use FutureEd\Models\Repository\OrderDetail\OrderDetailRepositoryInterface;
 use FutureEd\Models\Repository\Classroom\ClassroomRepositoryInterface;
 use FutureEd\Models\Repository\ClassStudent\ClassStudentRepositoryInterface;
 
-class RenewSubscriptionController extends ApiController {
+class PaymentSubscriptionController extends ApiController {
 
 	protected $invoice;
 	protected $discount;
@@ -21,6 +22,7 @@ class RenewSubscriptionController extends ApiController {
 	protected $order_detail;
 	protected $classroom;
 	protected $class_student;
+	protected $student;
 
 
 	public function __construct(
@@ -32,9 +34,11 @@ class RenewSubscriptionController extends ApiController {
 		InvoiceServices $invoice_service,
 		OrderDetailRepositoryInterface $order_detail,
 		ClassroomRepositoryInterface $classroom,
-		ClassStudentRepositoryInterface $class_student
+		ClassStudentRepositoryInterface $class_student,
+		StudentRepositoryInterface $studentRepositoryInterface
 
 	){
+		$this->student = $studentRepositoryInterface;
 		$this->invoice = $invoice;
 		$this->discount = $discount;
 		$this->order = $order;
@@ -61,27 +65,6 @@ class RenewSubscriptionController extends ApiController {
 		//get invoice details
 		$invoice = $this->invoice->getInvoice($id);
 
-		//TODO: check new fields.
-//		"id" => 1
-//		"order_no" => "0003-000001-2016"
-//		"invoice_date" => "2016-05-24 00:00:00"
-//		"client_id" => 0
-//		"client_name" => "0"
-//		"student_id" => 3
-//		"student_name" => "Jason Maglangit"
-//		"date_start" => "2016-05-20 00:00:00"
-//		"date_end" => "2016-05-23 00:00:00"
-//		"seats_total" => 1
-//		"discount_type" => "Volume"
-//		"discount_id" => 1
-//		"discount" => "25.00"
-//		"total_amount" => "3.75"
-//		"subscription_id" => 1
-//		"subscription_package_id" => 1
-//		"renew" => 0
-//		"payment_status" => "Paid"
-//		"status" => "Disabled"
-
 		$data['order_no'] = $invoice['order_no'];
 		$data['invoice_date'] = Carbon::now()->toDateString();
 		$data['client_id'] = $invoice['client_id'];
@@ -94,14 +77,12 @@ class RenewSubscriptionController extends ApiController {
 		$data['discount_type'] = $invoice['discount_type'];
 		$data['discount_id'] = $invoice['discount_id'];
 		$data['discount'] = $invoice['discount'];
-		$data['total_amount'] = 0;
+		$data['total_amount'] = $invoice['total_amount'];
 		$data['subscription_id'] = $invoice['subscription_id'];
 		$data['subscription_package_id'] = $invoice['subscription_package_id'];
 		$data['renew'] = 1;
 		$data['payment_status'] = config('futureed.pending');
 		$data['seats_taken'] = $invoice['order']['seats_taken'];
-
-		//date start and ende
 
 		//add invoice
 		$added_invoice = $this->invoice->addInvoice($data);
