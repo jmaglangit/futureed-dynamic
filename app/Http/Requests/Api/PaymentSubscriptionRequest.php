9fd52df70bdce39b0dc1b01be7e4aafeea19a924
jmaglangit <jmaglangit@nerubia.com>
2016-05-27 21:04:55 +0800

    updated request for subscription variables

--- app/Http/Requests/Api/PaymentSubscriptionRequest.php
+++ app/Http/Requests/Api/PaymentSubscriptionRequest.php
@@ -2,7 +2,7 @@
 
 use FutureEd\Http\Requests\Request;
 
-class RenewSubscriptionRequest extends ApiRequest {
+class PaymentSubscriptionRequest extends ApiRequest {
 
 	/**
 	 * Determine if the user is authorized to make this request.
@@ -24,17 +24,50 @@ class RenewSubscriptionRequest extends ApiRequest {
 		switch($this->method()){
 
 			case 'POST':
+				switch($this->route()->getName()){
 
-				return [
-					'order_date' => 'required|date_format:Ymd',
-					'date_start' => 'required|date_format:Ymd',
-					'date_end' => 'required|date_format:Ymd',
-					'discount' => 'required|numeric|between:0,999.99',
-					'total_amount' => 'required|numeric|between:1,999999.99'
+					case '':
+						return [
+							'subject_id' => 'required|numeric',
+							'order_date' => 'required|date_format:Ymd',
+							'student_id' => 'required|numeric',
+							'subscription_id' => 'required|numeric',
+							'date_start' => 'required|date_format:Ymd',
+							'date_end' => 'required|date_format:Ymd',
+							'seats_total' => 'required|numeric|between:1,999999',
+							'seats_taken' => 'numeric',
+							'total_amount' => 'required|numeric|between:1,999999.99',
+							'payment_status' => 'required|in:Pending,Paid,Cancelled',
+						];
+						break;
+					default:
+						return [
+							'order_date' => 'required|date_format:Ymd',
+							'date_start' => 'required|date_format:Ymd',
+							'date_end' => 'required|date_format:Ymd',
+							'discount' => 'required|numeric|between:0,999.99',
+							'total_amount' => 'required|numeric|between:1,999999.99'
 
-				];
+						];
+						break;
+				}
+
+
 
 		}
+	}
+
+	public function messages(){
+		return [
+			'subject_id.required' => trans('errors.1003',['attribute' => trans('errors.2155')]),
+			'subject_id.numeric' => trans('errors.1004',['attribute' => trans('errors.2155')]),
+			'student_id.required' => trans('errors.1003',['attribute' => trans('errors.2192')]),
+			'student_id.numeric' => trans('errors.1004',['attribute' => trans('errors.2192')]),
+			'subscription_id.required' => trans('errors.1003',['attribute' => trans('errors.2199')]),
+			'subscription_id.numeric' => trans('errors.1004',['attribute' => trans('errors.2199')]),
+
+
+		];
 	}
 
 
