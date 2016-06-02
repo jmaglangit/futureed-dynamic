b478b85fb394dee41b988d0d862b29a1eeb5c787
jmaglangit <jmaglangit@nerubia.com>
2016-06-02 20:31:29 +0800

    added added functions on client controller

--- app/Http/Controllers/Api/v1/ClientController.php
+++ app/Http/Controllers/Api/v1/ClientController.php
@@ -1,11 +1,33 @@
 <?php namespace FutureEd\Http\Controllers\Api\v1;
 
 use FutureEd\Http\Requests\Api\ClientRequest;
-use FutureEd\Models\Repository\Student\ClientRepositoryInterface;
-
+use FutureEd\Models\Repository\Client\ClientRepositoryInterface;
+use FutureEd\Services\ClientServices;
+use FutureEd\Services\SchoolServices;
+use FutureEd\Services\UserServices;
 use Illuminate\Support\Facades\Input;
 
 class ClientController extends ApiController {
+
+	protected $client_service;
+
+	protected $user_service;
+
+	protected $client;
+
+	protected $school_service;
+
+	public function __construct(
+		ClientServices $clientServices,
+		UserServices $userServices,
+		SchoolServices $schoolServices,
+		ClientRepositoryInterface $clientRepositoryInterface
+	){
+		$this->client_service = $clientServices;
+		$this->user_service = $userServices;
+		$this->client = $clientRepositoryInterface;
+		$this->school_service = $schoolServices;
+	}
 
 	/**
 	 * Display a listing of the resource.
@@ -50,7 +72,7 @@ class ClientController extends ApiController {
 			$offset = intval(Input::get('offset'));
 		}
 
-		$clients = $this->client->getClients($criteria, $limit, $offset);
+		$clients = $this->client_service->getClients($criteria, $limit, $offset);
 
 		return $this->respondWithData($clients);
 	}
@@ -74,14 +96,14 @@ class ClientController extends ApiController {
 
 		} else {
 
-			$return = $this->client->verifyClientId($id);
+			$return = $this->client_service->verifyClientId($id);
 
 			if ($return) {
 
 				//TODO: make use of relationships.
-				$userDetails = $this->user->getUserDetail($return['user_id'], $client)->toArray();
-				$clienDetails = $this->client->getClientDetails($id)->toArray();
-				$formResponse = $this->client->formResponse($userDetails, $clienDetails);
+				$userDetails = $this->user_service->getUserDetail($return['user_id'], $client)->toArray();
+				$clientDetails = $this->client_service->getClientDetails($id)->toArray();
+				$formResponse = $this->client_service->formResponse($userDetails, $clientDetails);
 
 				return $this->respondWithData($formResponse);
 
@@ -101,11 +123,11 @@ class ClientController extends ApiController {
 	 */
 	public function update($id, ClientRequest $clientRequest)
 	{
-		$return = $this->client->verifyClientId($id);
+		$return = $this->client_service->verifyClientId($id);
 
 		if($return){
 
-			$clientDetails = $this->client->getClientDetails($id)->toArray();
+			$clientDetails = $this->client_service->getClientDetails($id)->toArray();
 
 			$user = $clientRequest->only('username','email');
 			$client = $clientRequest->only('first_name','last_name','street_address',
@@ -114,7 +136,7 @@ class ClientController extends ApiController {
 			$school = $clientRequest->only('school_name','school_code','school_street_address','school_city',
 									  'school_state','school_country','school_country_id','school_zip','school_contact_name','school_contact_number');
 
-			$checkUsername = $this->user->checkUsername($user['username'],'Client');
+			$checkUsername = $this->user_service->checkUsername($user['username'],'Client');
 			$user['name'] = $client['first_name'].' '.$client['last_name'];
 
 			//add default value for country_id
@@ -129,13 +151,13 @@ class ClientController extends ApiController {
 				if($clientDetails['client_role'] === config('futureed.parent') ||
 				   $clientDetails['client_role'] === config('futureed.teacher')){
 
-					$this->user->updateUsername($return['user_id'],$user);
-					$this->client->updateClientDetails($return['id'],$client);
+					$this->user_service->updateUsername($return['user_id'],$user);
+					$this->client_service->updateClientDetails($return['id'],$client);
 
 				}else{
 
-					$school_code = $this->school->checkSchoolNameExist($school);
-					$school_name = $this->school->getSchoolName($school['school_code']);
+					$school_code = $this->school_service->checkSchoolNameExist($school);
+					$school_name = $this->school_service->getSchoolName($school['school_code']);
 
 					if(empty($school_name)){
 
@@ -147,18 +169,18 @@ class ClientController extends ApiController {
 
 					}else{
 
-					    $this->user->updateUsername($return['user_id'],$user);
-						$this->client->updateClientDetails($return['id'],$client);
-						$this->school->updateSchoolDetails($school);
+					    $this->user_service->updateUsername($return['user_id'],$user);
+						$this->client_service->updateClientDetails($return['id'],$client);
+						$this->school_service->updateSchoolDetails($school);
 
 					}
 
 				}
 
-				$clientDetails = $this->client->getClientDetails($id)->toArray();
-				$userDetails = $this->user->getUserDetail($return['user_id'],'Client')->toArray();
+				$clientDetails = $this->client_service->getClientDetails($id)->toArray();
+				$userDetails = $this->user_service->getUserDetail($return['user_id'],'Client')->toArray();
 
-				$response = $this->client->formResponse($userDetails,$clientDetails);
+				$response = $this->client_service->formResponse($userDetails,$clientDetails);
 
 				return $this->respondWithData($response);
 
@@ -196,15 +218,15 @@ class ClientController extends ApiController {
 
 		$input = $clientRequest->only('callback_uri');
 
-		$check_username = $this->user->checkUsername($user['username'], $user_type);
-		$check_email = $this->user->checkEmail($user['email'], $user_type);
+		$check_username = $this->user_service->checkUsername($user['username'], $user_type);
+		$check_email = $this->user_service->checkEmail($user['email'], $user_type);
 		$school['school_street_address'] = $school['school_address'];
 
 		//for teacher get school_code via school name if exist
-		$check_school = $this->school->getSchoolCode($school['school_name']);
+		$check_school = $this->school_service->getSchoolCode($school['school_name']);
 
 		//for principal check if school is unique
-		$school_exist = $this->school->checkSchoolNameExist($school);
+		$school_exist = $this->school_service->checkSchoolNameExist($school);
 
 		if ($check_username) {
 			
@@ -230,22 +252,22 @@ class ClientController extends ApiController {
 			$client['account_status'] = config('futureed.client_account_status_accepted');
 
 			//add user to db
-			$user_response = $this->user->addUser($user, $client);
+			$user_response = $this->user_service->addUser($user, $client);
 
 			$client['user_id'] = $user_response['id'];
 
 			if (strcasecmp($client['client_role'], config('futureed.principal')) == 0) {
 
 				//add school to db
-				$school_response = $this->school->addSchool($school);
+				$school_response = $this->school_service->addSchool($school);
 
 				$client['school_code'] = $school_response;
 
 			}
 
-			$client_response = $this->client->addClient($client);
-			$data = $this->user->getUser($user_response['id'], 'Client');
-			$code = $this->user->getConfirmationCode($user_response['id']);
+			$client_response = $this->client_service->addClient($client);
+			$data = $this->user_service->getUser($user_response['id'], 'Client');
+			$code = $this->user_service->getConfirmationCode($user_response['id']);
 			$data['client_role'] = $client['client_role'];
 
 			// send email to user
@@ -275,7 +297,7 @@ class ClientController extends ApiController {
 
 		}
 
-		$return = $this->client->getClientDetails($id);
+		$return = $this->client_service->getClientDetails($id);
 
 		if (!$return) {
 
@@ -289,7 +311,7 @@ class ClientController extends ApiController {
 
 		}
 		//check relation of teacher to classroom
-		$teacher_classroom = $this->client->getClassroom($id);
+		$teacher_classroom = $this->client_service->getClassroom($id);
 
 		if ($teacher_classroom['classroom']->toArray() && $return['client_role'] === config('futureed.teacher')) {
 
@@ -297,7 +319,7 @@ class ClientController extends ApiController {
 		}
 
 		//check parent to student
-		$parent_student = $this->client->getStudent($id);
+		$parent_student = $this->client_service->getStudent($id);
 
 		if ($parent_student['student']->toArray() && $return['client_role'] === config('futureed.parent')) {
 
@@ -305,7 +327,7 @@ class ClientController extends ApiController {
 
 		}
 
-		return $this->respondWithData([$this->client->deleteClient($id)]);
+		return $this->respondWithData([$this->client_service->deleteClient($id)]);
 
 
 	}
@@ -318,7 +340,7 @@ class ClientController extends ApiController {
 	 */
 	public function checkBillingAddress($id)
 	{
-		$client_details = $this->client->getClientDetails($id);
+		$client_details = $this->client_service->getClientDetails($id);
 
 		if($client_details->street_address == null
 				|| $client_details->city == null
@@ -329,5 +351,21 @@ class ClientController extends ApiController {
 			return $this->respondWithData(['status' => 1]);
 		}
 		return $this->respondWithData(['status' => 0]);
+	}
+
+	/**
+	 * Update billing address of the student.
+	 * @param ClientRequest $request
+	 * @param $id
+	 * @return
+	 */
+	public function updateBillingAddress(ClientRequest $request, $id){
+
+		$client_billing_information = $request->only('city','state','country_id');
+
+		$client = $this->client->updateClientDetails($id,$client_billing_information);
+
+		return $this->respondWithData($client);
+
 	}
 }
