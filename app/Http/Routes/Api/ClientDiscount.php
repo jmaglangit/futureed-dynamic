280270247a3991b3c0aa2269a8d49b7830726c0f
jmaglangit <jmaglangit@nerubia.com>
2016-06-07 15:19:16 +0800

    removed unnecessary lines

--- app/Http/Routes/Api/ClientDiscount.php
+++ app/Http/Routes/Api/ClientDiscount.php
@@ -4,7 +4,7 @@
  * Client discount resource.
  */
 Routes::group([
-//    'middleware' => ['api_user','api_after'],
+    'middleware' => ['api_user','api_after'],
     'permission' => ['admin','client','student'],
     'role' => ['principal','teacher','parent','admin','super admin']
 ],function(){
