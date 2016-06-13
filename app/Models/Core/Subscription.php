3159247aa926df4b32a8efe17d36fc538775b0b9
jmaglangit <jmaglangit@nerubia.com>
2016-06-13 14:23:26 +0800

    updated into necessary columns

--- app/Models/Core/Subscription.php
+++ app/Models/Core/Subscription.php
@@ -14,7 +14,7 @@ class Subscription extends Model {
 	
 	protected $dates = ['deleted_at'];
 	
-	protected $fillable = ['name', 'price', 'description','days', 'status','created_by','updated_by', 'has_lsp'];
+	protected $fillable = ['name', 'description', 'status','created_by','updated_by', 'has_lsp'];
 	
     protected $hidden = ['created_by','updated_by','created_at','updated_at','deleted_at'];
 
