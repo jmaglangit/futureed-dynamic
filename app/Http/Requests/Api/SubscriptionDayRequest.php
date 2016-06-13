4aa4d8401116ac28c8602f695d668775082dd7b8
jmaglangit <jmaglangit@nerubia.com>
2016-06-13 18:52:11 +0800

    added new request for subscription days

---
+++ app/Http/Requests/Api/SubscriptionDayRequest.php
@@ -0,0 +1,39 @@
+<?php namespace FutureEd\Http\Requests\Api;
+
+class SubscriptionDayRequest extends ApiRequest {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		switch($this->method()){
+
+			case 'POST':
+				return [
+					'days'		=> 'required|integer',
+					'status'	=> 'required|in:'.config('futureed.enabled').','.config('futureed.disabled')
+				];
+				break;
+			case 'PUT':
+				return [
+					'days'		=> 'required|integer',
+					'status'	=> 'required|in:'.config('futureed.enabled').','.config('futureed.disabled')
+				];
+				break;
+		}
+	}
+
+}
