23fdc6173a6d714f663e3c4a60fc767d32090387
jmaglangit <jmaglangit@nerubia.com>
2016-06-13 18:48:49 +0800

    added function interfaces for subscription days

--- app/Models/Repository/SubscriptionDay/SubscriptionDayRepositoryInterface.php
+++ app/Models/Repository/SubscriptionDay/SubscriptionDayRepositoryInterface.php
@@ -1,11 +1,14 @@
-<?php
+<?php namespace FutureEd\Models\Repository\SubscriptionDay;
 
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 6/13/16
- * Time: 3:51 PM
- */
 interface SubscriptionDayRepositoryInterface {
 
+	public function getSubscriptionDays($criteria = [] ,$limit = 0, $offset = 0);
+
+	public function getSubscriptionDay($id);
+
+	public function addSubscriptionDay($data);
+
+	public function updateSubscriptionDay($id, $data);
+
+	public function deleteSubscriptionDay($id);
 }
