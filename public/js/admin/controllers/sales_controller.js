48bd405428c57ba7765c001c4a232ccfad09f1f5
jmaglangit <jmaglangit@nerubia.com>
2016-06-13 14:25:02 +0800

    updated switch controller on views

--- public/js/admin/controllers/sales_controller.js
+++ public/js/admin/controllers/sales_controller.js
@@ -12,17 +12,28 @@ function SalesController($scope) {
 		self.active_price_settings = Constants.FALSE;
 		self.active_client_discount = Constants.FALSE;
 		self.active_bulk_settings = Constants.FALSE;
+		self.active_subscription = Constants.FALSE;
+		self.active_subscription_days = Constants.FALSE;
+		self.active_subscription_packages = Constants.FALSE;
 
 		switch(active) {
-			case 'bulk_settings':
+			case Constants.BULK_SETTINGS:
 				self.active_bulk_settings = Constants.TRUE;
 				break;
 
-			case 'client_discount':
+			case Constants.CLIENT_DISCOUNT:
 				self.active_client_discount = Constants.TRUE;
 				break;
 
-			case 'price_settings':
+			case Constants.SUBSCRIPTION_DAYS:
+				self.active_subscription_days = Constants.TRUE;
+				break;
+
+			case Constants.SUBSCRIPTION_PACKAGES:
+				self.active_subscription_packages = Constants.TRUE;
+				break;
+
+			case Constants.SUBSCRIPTION:
 			default:
 				self.active_price_settings = Constants.TRUE;
 				break;
