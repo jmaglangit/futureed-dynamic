8457c456d5c889feb3fb389b50077fe71c88401a
jmaglangit <jmaglangit@nerubia.com>
2016-06-13 18:45:40 +0800

    added necessary files for CRUD on subscriptions days

---
+++ app/Models/Repository/SubscriptionDay/SubscriptionDayRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 6/13/16
+ * Time: 3:54 PM
+ */
+
+namespace FutureEd\Models\Repository\SubscriptionDay;
+
+
+class SubscriptionDayRepository {
+
+}
---
+++ app/Models/Repository/SubscriptionDay/SubscriptionDayRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 6/13/16
+ * Time: 3:51 PM
+ */
+interface SubscriptionDayRepositoryInterface {
+
+}
