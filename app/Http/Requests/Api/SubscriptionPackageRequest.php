b0a1aaa82670430ac4a80d9f04349f39e9a618a0
jmaglangit <jmaglangit@nerubia.com>
2016-06-15 13:58:11 +0800

    update request condition into If

--- app/Http/Requests/Api/SubscriptionPackageRequest.php
+++ app/Http/Requests/Api/SubscriptionPackageRequest.php
@@ -19,28 +19,15 @@ class SubscriptionPackageRequest extends ApiRequest {
 	 */
 	public function rules()
 	{
-		switch($this->method()){
-
-			case 'POST':
-				return [
-					'days_id'	=> 'required|integer|exists:subscription_days,id,deleted_at,NULL',
-					'subscription_id'	=> 'required|integer|exists:subscription,id,deleted_at,NULL',
-					'country_id'	=> 'required|integer|exists:countries,id',
-					'subject_id'	=> 'required|integer|exists:subjects,id,deleted_at,NULL',
-					'price'	=> 'required|numeric|between:1,999999.99',
-					'status'	=> 'required|in:'.config('futureed.enabled').','.config('futureed.disabled')
-				];
-				break;
-			case 'PUT':
-				return [
-					'days_id'	=> 'required|integer|exists:subscription_days,id,deleted_at,NULL',
-					'subscription_id'	=> 'required|integer|exists:subscription,id,deleted_at,NULL',
-					'country_id'	=> 'required|integer|exists:countries,id',
-					'subject_id'	=> 'required|integer|exists:subjects,id,deleted_at,NULL',
-					'price'	=> 'required|numeric|between:1,999999.99',
-					'status'	=> 'required|in:'.config('futureed.enabled').','.config('futureed.disabled')
-				];
-				break;
+		if($this->method() == 'POST' || $this->method() == 'PUT'){
+			return [
+				'days_id'	=> 'required|integer|exists:subscription_days,id,deleted_at,NULL',
+				'subscription_id'	=> 'required|integer|exists:subscription,id,deleted_at,NULL',
+				'country_id'	=> 'required|integer|exists:countries,id',
+				'subject_id'	=> 'required|integer|exists:subjects,id,deleted_at,NULL',
+				'price'	=> 'required|numeric|between:1,999999.99',
+				'status'	=> 'required|in:'.config('futureed.enabled').','.config('futureed.disabled')
+			];
 		}
 	}
 
