0c17f46ee03619fa0ae5dac4500c830c5039f53c
jmaglangit <jmaglangit@nerubia.com>
2016-06-17 17:49:46 +0800

    updated query inserting data seeders

--- database/seeds/GameTableSeeder.php
+++ database/seeds/GameTableSeeder.php
@@ -11,23 +11,25 @@ class GameTableSeeder extends Seeder
     {
         $reader = Reader::createFromPath( storage_path('seeders') . '/games.csv');
 
-        DB::table('games')->truncate();
+        $seeds = [];
         foreach($reader as $index => $row) {
 
-            DB::table('games')->insert([
-                [
-                    'code' => $row[1],
-                    'name' => $row[2],
-                    'game_url' => $row[3],
-                    'game_image' => $row[4],
-                    'points_price' => $row[5],
-                    'description' => $row[6],
-                    'created_by' => 1,
-                    'updated_by' => 1,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now()
-                ]
+            array_push($seeds,[
+                'code' => $row[1],
+                'name' => $row[2],
+                'game_url' => $row[3],
+                'game_image' => $row[4],
+                'points_price' => $row[5],
+                'description' => $row[6],
+                'created_by' => 1,
+                'updated_by' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ]);
         }
+
+        DB::table('games')->truncate();
+
+        DB::table('games')->insert($seeds);
     }
 }
