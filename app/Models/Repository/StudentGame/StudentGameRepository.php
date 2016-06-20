b87b889f45d816a0d387cec3834a7743d3fd9aa4
jmaglangit <jmaglangit@nerubia.com>
2016-06-20 18:18:42 +0800

    implement model interface

--- app/Models/Repository/StudentGame/StudentGameRepository.php
+++ app/Models/Repository/StudentGame/StudentGameRepository.php
@@ -1,14 +1,76 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 6/20/16
- * Time: 5:43 PM
- */
-
-namespace FutureEd\Models\Repository\StudentGame;
+<?php namespace FutureEd\Models\Repository\StudentGame;
 
 
-class StudentGameRepository {
+use FutureEd\Models\Core\StudentGame;
+use FutureEd\Models\Traits\LoggerTrait;
+
+class StudentGameRepository implements StudentGameRepositoryInterface{
+
+	use LoggerTrait;
+
+	/**
+	 * Get bought games
+	 * @param array $criteria
+	 * @param int $limit
+	 * @param int $offset
+	 * @return array|bool
+	 */
+	public function getStudentGames($criteria = [], $limit = 0, $offset = 0){
+
+		try{
+
+			$student_games = new StudentGame();
+
+			if(isset($criteria['user_id'])){
+
+				$student_games = $student_games->userId($criteria['user_id']);
+			}
+
+			if(isset($criteria['game_id'])){
+
+				$student_games = $student_games->gameId($criteria['game_id']);
+			}
+
+			$count = $student_games->count();
+
+			if($limit > 0 && $offset >= 0) {
+
+				$student_games = $student_games->limit($limit)->offset($offset);
+			}
+
+			$response = [
+				'total' => $count,
+				'records' => $student_games->get()->toArray()
+			];
+
+		} catch (\Exception $e) {
+
+			$this->errorLog($e->getMessage());
+
+			return false;
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Get bought game
+	 * @param $id
+	 * @return \Illuminate\Support\Collection|null|static
+	 */
+	public function getStudentGame($id){
+
+		return StudentGame::find($id);
+	}
+
+	/**
+	 * Get bought game(s) by student
+	 * @param $user_id
+	 * @return mixed
+	 */
+	public function getStudentsGame($user_id){
+
+		return StudentGame::userId($user_id)->get();
+	}
 
 }
