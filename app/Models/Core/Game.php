12daea680812293309a50c6270dda5dfbc72e5f9
jmaglangit <jmaglangit@nerubia.com>
2016-06-20 17:22:56 +0800

    added game model

--- app/Models/Core/Game.php
+++ app/Models/Core/Game.php
@@ -1,14 +1,54 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 6/20/16
- * Time: 3:51 PM
- */
-
-namespace FutureEd\Models\Core;
+<?php namespace FutureEd\Models\Core;
 
 
-class Game {
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Game extends Model{
+
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'games';
+
+	protected $dates = ['created_at','updated_at','deleted_at'];
+
+	protected $fillable = [
+		'code',
+		'name',
+		'game_url',
+		'game_image',
+		'points_price',
+		'description'
+	];
+
+	protected $hidden = ['created_by', 'updated_by', 'updated_at','created_at', 'deleted_at'];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1,
+	];
+
+	//scopes
+
+	public function scopeCode($query,$code){
+
+		return $query->where('code',$code);
+	}
+
+	public function scopeName($query,$name){
+
+		return $query->where('name',$name);
+	}
+
+	public function scopePointsPrice($query,$points_price){
+
+		return $query->where('points_price',$points_price);
+	}
+
+
+
 
 }
