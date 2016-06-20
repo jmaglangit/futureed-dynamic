aca825e7de992c0ef3fff6a594939f41446b8230
jmaglangit <jmaglangit@nerubia.com>
2016-06-20 18:17:43 +0800

    added student games

--- app/Http/Routes/Api/StudentGame.php
+++ app/Http/Routes/Api/StudentGame.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 6/20/16
- * Time: 6:11 PM
- */
+
+
+Routes::group([
+	'middleware' => ['api_user', 'api_after'],
+	'permission' => ['admin', 'client', 'student'],
+	'role' => ['principal', 'teacher', 'parent', 'admin', 'super admin']
+],function(){
+
+	Routes::resource('/student-game','Api\v1\StudentGameController',[
+		'only' => ['index','show']
+	]);
+
+	Routes::get('/students-game/',[
+		'uses' => 'Api\v1\StudentGameController@getStudentsGame',
+		'as' => 'students.game'
+	]);
+});
