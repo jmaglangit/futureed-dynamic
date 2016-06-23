07b4fa0f5b03f0bfa076bf944f54fa4ce672704a
jmaglangit <jmaglangit@nerubia.com>
2016-06-23 11:06:36 +0800

    added play game view

---
+++ resources/views/student/profile/play_game.blade.php
@@ -0,0 +1,9 @@
+<div ng-if="profile.active_play_game">
+    <div class="col-md-12">
+        <div class="form-class">
+            {{--// Game here--}}
+            <iframe src="demo_iframe.htm" width="200" height="200"></iframe>
+
+        </div>
+    </div>
+</div>
