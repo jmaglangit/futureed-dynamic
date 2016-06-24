6bc875cb635dc7ddfadf6feeabe75e7807879400
jmaglangit <jmaglangit@nerubia.com>
2016-06-24 15:44:12 +0800

    updated message modal

--- resources/views/student/partials/message.blade.php
+++ resources/views/student/partials/message.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
 
             <div class="modal-body message-container modal-reward">
-                <div class="h1"><span class="col-xs-12">{!! ucfirst('rewards to buy') !!}</span></div>
+                <div class="h1"><span class="col-xs-12">{!! ucfirst(trans('messages.rewards_to_buy')) !!}</span></div>
 
                 <div class="row modal-rewards-items">
                     <div class="col-xs-6">
@@ -12,19 +12,19 @@
                             <img src="/images/shopping_bag.png">
                         </a>
                         <br>
-                        <span class="h4">Accessories for your Avatar</span></div>
+                        <span class="h4">{!! trans('messages.accessories_for_avatar') !!}</span></div>
                     <div class="col-xs-6 reward-games">
                         <a ng-click="redirectRewards('{!! route('student.profile.index') !!}', futureed.GAMES)" class="btn">
                             <img src="/images/input_gaming.png">
                         </a>
                         <br>
-                        <span class="h4">Games</span>
+                        <span class="h4">{!! trans_choice('messages.game',2) !!}</span>
                     </div>
                 </div>
             </div>
 
             <div class="modal-footer modal-reward">
-                <div class="btn btn-maroon btn-medium" data-dismiss="modal" aria-label="Close">Close</div>
+                <div class="btn btn-maroon btn-medium" data-dismiss="modal" aria-label="Close">{!! trans('messages.close') !!}</div>
             </div>
         </div>
     </div>
