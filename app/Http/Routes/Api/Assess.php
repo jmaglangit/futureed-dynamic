0b46262f77e6e2798edd0ecc76512a77fa60f4f5
jmaglangit <jmaglangit@nerubia.com>
2016-06-28 17:32:34 +0800

    added new routes for download link on iassess

--- app/Http/Routes/Api/Assess.php
+++ app/Http/Routes/Api/Assess.php
@@ -1,5 +1,4 @@
 <?php 
-#TODO: Add Middleware api_user 
 Routes::group(['prefix' => '/assess'], function() {
 
 	Routes::get('/get-test',[
@@ -13,6 +12,14 @@ Routes::group(['prefix' => '/assess'], function() {
 	Routes::post('/save-test',[
 		'uses' => 'Api\v1\AssessController@saveTest',
 		'as' => 'api.v1.assess.save-test',
+		'middleware' => ['api_user','api_after'],
+		'permission' => ['student','admin'],
+		'role' => ['admin','super admin']
+	]);
+
+	Routes::get('/download-report/{student_id}',[
+		'uses' => 'Api\v1\AssessController@getReport',
+		'as' => 'api.v1.assess.get-report',
 		'middleware' => ['api_user','api_after'],
 		'permission' => ['student','admin'],
 		'role' => ['admin','super admin']
