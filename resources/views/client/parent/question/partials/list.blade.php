a88589b21d0fcf69f60e5a307e2bff67b9a941cb
jmaglangit <jmaglangit@nerubia.com>
2016-06-29 17:10:04 +0800

    updated function call on views

--- resources/views/client/parent/question/partials/list.blade.php
+++ resources/views/client/parent/question/partials/list.blade.php
@@ -12,15 +12,15 @@
 			</div>
 		</div>
 	</div>
-	<div class="col-xs-12 btn-content margin-20-bot" ng-if="question.hide_difficulty">
+	<div class="col-xs-12 btn-content margin-20-bot">
 		<div class="col-xs-4">
-			<button class="btn btn-gold btn-medium" ng-click="question.viewQuestion(question.details.seq_no, futureed.BACK)" ng-if="question.details.seq_no != futureed.TRUE">{!! trans('messages.previous') !!}</button>
+			<button class="btn btn-gold btn-medium" ng-click="question.viewQuestion(futureed.BACK)" ng-if="question.details.seq_no != futureed.TRUE">{!! trans('messages.previous') !!}</button>
 		</div>
 		<div class="col-xs-4">
 			<center><h2 class="title-content">{! question.details.teaching_module !}</h2></center>
 		</div>
 		<div class="col-xs-4">
-			<button class="btn btn-maroon btn-medium pull-right" ng-click="question.viewQuestion(question.details.seq_no, futureed.NEXT)" ng-if="question.details.seq_no != question.question_total">{!! trans('messages.next') !!}</button>
+			<button class="btn btn-maroon btn-medium pull-right" ng-click="question.viewQuestion(futureed.NEXT)" ng-if="question.details.seq_no != question.question_total">{!! trans('messages.next') !!}</button>
 		</div>
 	</div>
 </div>
