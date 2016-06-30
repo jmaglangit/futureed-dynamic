50986cf21ec075efaaedb78d97c57209b6950b7b
jmaglangit <jmaglangit@nerubia.com>
2016-06-30 16:46:15 +0800

    added comments

--- public/js/common/tooltip_converter.js
+++ public/js/common/tooltip_converter.js
@@ -1,5 +1,9 @@
 /**
  * Created by jason on 6/30/16.
+ *
+ * Created converter for jquery and bootstrap tooltip conflict.
+ * this converter should be place between jquery and bootstrap.
+ * Jquery should be called first then bootstrap.
  */
 
 $.widget.bridge('uibutton', $.ui.button);
