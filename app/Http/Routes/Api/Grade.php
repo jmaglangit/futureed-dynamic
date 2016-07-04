b3d1147518086aa3e4097be23c8cc4eef2160258
Roy <rreynes@nerubia.com>
2016-07-04 10:58:09 +0800

    removed student grade validation upon sign up

--- app/Http/Routes/Api/Grade.php
+++ app/Http/Routes/Api/Grade.php
@@ -3,19 +3,18 @@
 /**
  * Get predefined types of grades.
  */
-Routes::group([
-	'middleware' => ['api_user', 'api_after'],
-	'permission' => ['admin', 'client', 'student'],
-	'role' => ['principal', 'teacher', 'parent', 'admin', 'super admin']
-], function()
-{
-	Routes::resource('/grade','Api\v1\GradeController',
-		['except' => ['create','edit']]);
+Routes::group([],
+	function()
+	{
+		Routes::resource('/grade','Api\v1\GradeController',
+			['except' => ['create','edit']]);
 
-    Routes::put('grade/update/{id}',[
-            'uses' => 'Api\v1\GradeCustomController@update',
-            'as' =>'api.v1.grade.update' ]);
-});
+		Routes::put('grade/update/{id}',[
+				'uses' => 'Api\v1\GradeCustomController@update',
+				'as' =>'api.v1.grade.update' ]);
+
+	}
+);
 
 
 
