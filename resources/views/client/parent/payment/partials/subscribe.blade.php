fa619fff4408dcde2b3c6aa768eb53fcf664e563
jmaglangit <jmaglangit@nerubia.com>
2016-08-03 17:23:02 +0800

    added tmeplate directive for invoice

--- resources/views/client/parent/payment/partials/subscribe.blade.php
+++ resources/views/client/parent/payment/partials/subscribe.blade.php
@@ -276,18 +276,7 @@
                         </div>
                         <div class="tab-pane" role="tabpanel" id="complete">
                             <h3>{!! ucfirst(trans('messages.billing_invoice')) !!}</h3>
-
-                            <div>
-                                Ref : KCGA {! payment.invoice.client_name !} {! payment.invoice.id !}/{!! date('Y') !!}<br>
-                                {!! trans('messages.date') !!} : {! payment.subscription_invoice.order_date_string !}<br><br>
-                                {! futureed.BILL_COMPANY !}.<br>
-                                {! futureed.BILL_STREET !}<br>
-                                {! futureed.BILL_ADDRESS !}<br>
-                                {! futureed.BILL_COUNTRY !}<br><br>
-                                {!! trans('messages.bill_to') !!} : {! payment.invoice.client_name !}<br>
-                                {! payment.invoice.client.street_address !}{! ',' + payment.invoice.client.city  !}{! ',' + payment.invoice.client.state !}{! ',' + payment.invoice.client.country.name !}<br>
-                                Attention : {! payment.invoice.client.school.contact_name !}<br>
-                            </div>
+                            <div template-directive template-url="{!! route('client.parent.payment.partial.invoice-header') !!}"></div>
                             <div class="row">
                                 <div class="col-xs-12 invoice-form">
                                     {{--subscription summary--}}
@@ -368,14 +357,7 @@
                                             <label class="col-lg-4 h5 form-label">{! payment.subscription_invoice.total_amount !} USD</label>
                                         </div>
                                     </div>
-                                    <div><br>
-                                        {!! trans('messages.payment_method') !!} :<br/>
-                                        {!! trans('messages.direct_credit_to') !!} : {! futureed.CC_NAME !}<br/>
-                                        {!! trans('messages.bank_name') !!} : {! futureed.BANK_NAME !}<br/>
-                                        {!! trans('messages.bank_account_number') !!} : {! futureed.BANK_ACCT_NO_SGD !} {! ';' + futureed.BANK_ACCT_NO_USD !}<br/>
-                                        {!! trans('messages.bank_address') !!} : {! futureed.BANK_ADDRESS !}<br/>
-                                        {!! trans('messages.bank_code') !!} : {! futureed.BANK_CODE !}
-                                    </div>
+                                    <div template-directive template-url="{!! route('client.parent.payment.partial.invoice-footer') !!}"></div>
                                 </div>
                             </div>
                             <ul class="list-inline pull-right">
