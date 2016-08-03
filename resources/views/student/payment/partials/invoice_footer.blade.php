244fa3762bc67bcc233eb34773f4374119c95a83
jmaglangit <jmaglangit@nerubia.com>
2016-08-03 17:26:05 +0800

    add elements for invoice footer

---
+++ resources/views/student/payment/partials/invoice_footer.blade.php
@@ -0,0 +1,8 @@
+<div><br>
+    {!! trans('messages.payment_method') !!} :<br/>
+    {!! trans('messages.direct_credit_to') !!} : {! futureed.CC_NAME !}<br/>
+    {!! trans('messages.bank_name') !!} : {! futureed.BANK_NAME !}<br/>
+    {!! trans('messages.bank_account_number') !!} : {! futureed.BANK_ACCT_NO_SGD !} ; {! futureed.BANK_ACCT_NO_USD !}<br/>
+    {!! trans('messages.bank_address') !!} : {! futureed.BANK_ADDRESS !}<br/>
+    {!! trans('messages.bank_code') !!} : {! futureed.BANK_CODE !}
+</div>
