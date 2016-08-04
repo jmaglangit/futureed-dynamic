6b5a0666a6c16483a1f0df30c27ab951d6d2771f
jmaglangit <jmaglangit@nerubia.com>
2016-08-04 18:05:16 +0800

    updated view calls

--- resources/views/student/payment/partials/subscribe.blade.php
+++ resources/views/student/payment/partials/subscribe.blade.php
@@ -255,7 +255,7 @@
 
                             <div class="row">
                                 <div class="col-xs-12 invoice-form">
-                                    @include('student.payment.partials.invoice_header')
+                                    @include('common.invoice_header')
                                     {{--subscription summary--}}
                                     <div class="form-search">
                                         <div>
@@ -312,7 +312,7 @@
                                             <label class="col-lg-4 h5 form-label">{! payment.subscription_invoice.total_amount !} USD</label>
                                         </div>
                                     </div>
-                                    @include('client.partials.invoice_footer')
+                                    @include('common.invoice_footer')
                                 </div>
                             </div>
                             <ul class="list-inline pull-right">
