b16c94fc73544d5fe0dd48dbefc3fd448e666ec3
jmaglangit <jmaglangit@nerubia.com>
2016-08-04 17:43:33 +0800

    commented languages that are not ready

--- resources/views/common/language-option.blade.php
+++ resources/views/common/language-option.blade.php
@@ -5,11 +5,11 @@
             <span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
-            <li><a href="/lang/ar"><img border="0" alt="" src="/images/flags/ar.png" width="50" height="30"></a></li>
+            {{--<li><a href="/lang/ar"><img border="0" alt="" src="/images/flags/ar.png" width="50" height="30"></a></li>--}}
             <li><a href="/lang/id"><img border="0" alt="" src="/images/flags/id.png" width="50" height="30"></a></li>
-            <li><a href="/lang/ms"><img border="0" alt="" src="/images/flags/ms.png" width="50" height="30"></a></li>
+            {{--<li><a href="/lang/ms"><img border="0" alt="" src="/images/flags/ms.png" width="50" height="30"></a></li>--}}
             <li><a href="/lang/my"><img border="0" alt="" src="/images/flags/my.png" width="50" height="30"></a></li>
-            <li><a href="/lang/es"><img border="0" alt="" src="/images/flags/es.png" width="50" height="30"></a></li>
+            {{--<li><a href="/lang/es"><img border="0" alt="" src="/images/flags/es.png" width="50" height="30"></a></li>--}}
             <li><a href="/lang/en"><img border="0" alt="" src="/images/flags/en.png" width="50" height="30"></a></li>
             <li><a href="/lang/pt"><img border="0" alt="" src="/images/flags/pt.png" width="50" height="30"></a></li>
             <li><a href="/lang/th"><img border="0" alt="" src="/images/flags/th.png" width="50" height="30"></a></li>
