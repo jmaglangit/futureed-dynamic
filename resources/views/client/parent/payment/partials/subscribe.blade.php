6d01f9216f65d29c3e5cec16cbd7cc48970c1f00
jmaglangit <jmaglangit@nerubia.com>
2016-08-04 11:12:49 +0800

    updated header and footer into blade view partials

--- resources/views/client/parent/payment/partials/subscribe.blade.php
+++ resources/views/client/parent/payment/partials/subscribe.blade.php
@@ -276,7 +276,7 @@
                         </div>
                         <div class="tab-pane" role="tabpanel" id="complete">
                             <h3>{!! ucfirst(trans('messages.billing_invoice')) !!}</h3>
-                            <div template-directive template-url="{!! route('client.parent.payment.partial.invoice-header') !!}"></div>
+                            @include('client.partials.invoice_header')
                             <div class="row">
                                 <div class="col-xs-12 invoice-form">
                                     {{--subscription summary--}}
@@ -357,7 +357,7 @@
                                             <label class="col-lg-4 h5 form-label">{! payment.subscription_invoice.total_amount !} USD</label>
                                         </div>
                                     </div>
-                                    <div template-directive template-url="{!! route('client.parent.payment.partial.invoice-footer') !!}"></div>
+                                    @include('client.partials.invoice_footer')
                                 </div>
                             </div>
                             <ul class="list-inline pull-right">
