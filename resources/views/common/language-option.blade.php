313df8065aa9ae57c1d13a1ebf37ce2a7c94ba6f
jmaglangit <jmaglangit@nerubia.com>
2016-08-05 19:06:48 +0800

    updated language options

--- resources/views/common/language-option.blade.php
+++ resources/views/common/language-option.blade.php
@@ -6,14 +6,14 @@
         </button>
         <ul class="dropdown-menu">
             {{--<li><a href="/lang/ar"><img border="0" alt="" src="/images/flags/ar.png" width="50" height="30"></a></li>--}}
-            <li><a href="/lang/id"><img border="0" alt="" src="/images/flags/id.png" width="50" height="30"></a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'id') }}"><a href="/lang/id"><img alt="" src="/images/flags/id.png"> {!! trans('messages.bahasa_indonesia') !!}</a></li>
             {{--<li><a href="/lang/ms"><img border="0" alt="" src="/images/flags/ms.png" width="50" height="30"></a></li>--}}
-            <li><a href="/lang/my"><img border="0" alt="" src="/images/flags/my.png" width="50" height="30"></a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'my') }}"><a href="/lang/my"><img alt="" src="/images/flags/my.png"> {!! trans('messages.burmese') !!}</a></li>
             {{--<li><a href="/lang/es"><img border="0" alt="" src="/images/flags/es.png" width="50" height="30"></a></li>--}}
-            <li><a href="/lang/en"><img border="0" alt="" src="/images/flags/en.png" width="50" height="30"></a></li>
-            <li><a href="/lang/pt"><img border="0" alt="" src="/images/flags/pt.png" width="50" height="30"></a></li>
-            <li><a href="/lang/th"><img border="0" alt="" src="/images/flags/th.png" width="50" height="30"></a></li>
-            <li><a href="/lang/vi"><img border="0" alt="" src="/images/flags/vi.png" width="50" height="30"></a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'en') }}"><a href="/lang/en"><img alt="" src="/images/flags/en.png"> {!! trans('messages.english_uk') !!}</a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'pt') }}"><a href="/lang/pt"><img alt="" src="/images/flags/pt.png"> {!! trans('messages.portuguese') !!}</a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'th') }}"><a href="/lang/th"><img alt="" src="/images/flags/th.png"> {!! trans('messages.thai') !!}</a></li>
+            <li ng-hide="{{ (session('appLanguage') == 'vi') }}"><a href="/lang/vi"><img alt="" src="/images/flags/vi.png"> {!! trans('messages.vietnamese') !!}</a></li>
         </ul>
     </div>
 </div>
