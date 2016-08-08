b40c621e6f58535114bac73591ef14590606e193
jmaglangit <jmaglangit@nerubia.com>
2016-08-08 14:25:18 +0800

    updated lanuage view

--- resources/views/common/language-option.blade.php
+++ resources/views/common/language-option.blade.php
@@ -5,15 +5,9 @@
             <span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
-            {{--<li><a href="/lang/ar"><img border="0" alt="" src="/images/flags/ar.png" width="50" height="30"></a></li>--}}
-            <li ng-hide="{{ (session('appLanguage') == 'id') }}"><a href="/lang/id"><img alt="" src="/images/flags/id.png"> {!! trans('messages.bahasa_indonesia') !!}</a></li>
-            {{--<li><a href="/lang/ms"><img border="0" alt="" src="/images/flags/ms.png" width="50" height="30"></a></li>--}}
-            <li ng-hide="{{ (session('appLanguage') == 'my') }}"><a href="/lang/my"><img alt="" src="/images/flags/my.png"> {!! trans('messages.burmese') !!}</a></li>
-            {{--<li><a href="/lang/es"><img border="0" alt="" src="/images/flags/es.png" width="50" height="30"></a></li>--}}
-            <li ng-hide="{{ (session('appLanguage') == 'en') }}"><a href="/lang/en"><img alt="" src="/images/flags/en.png"> {!! trans('messages.english_uk') !!}</a></li>
-            <li ng-hide="{{ (session('appLanguage') == 'pt') }}"><a href="/lang/pt"><img alt="" src="/images/flags/pt.png"> {!! trans('messages.portuguese') !!}</a></li>
-            <li ng-hide="{{ (session('appLanguage') == 'th') }}"><a href="/lang/th"><img alt="" src="/images/flags/th.png"> {!! trans('messages.thai') !!}</a></li>
-            <li ng-hide="{{ (session('appLanguage') == 'vi') }}"><a href="/lang/vi"><img alt="" src="/images/flags/vi.png"> {!! trans('messages.vietnamese') !!}</a></li>
+            @foreach(config('futureed.language_options' ) as $lang)
+             <li ng-hide="{!! session('appLanguage') == $lang !!}"><a href="{!! "/lang/" . $lang !!}"><img alt="" src="{!! "/images/flags/" . $lang . ".png" !!}"> {!! trans('messages.' . $lang) !!}</a></li>
+            @endforeach
         </ul>
     </div>
 </div>
