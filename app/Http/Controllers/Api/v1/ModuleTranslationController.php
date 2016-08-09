07522b284ec2385f7cdc2fe6f4ba90c84fa3b10d
jmaglangit <jmaglangit@nerubia.com>
2016-08-09 10:21:21 +0800

    created ModuleTranslation controller

---
+++ app/Http/Controllers/Api/v1/ModuleTranslationController.php
@@ -0,0 +1,12 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ModuleTranslationController extends ApiController {
+
+
+
+}
