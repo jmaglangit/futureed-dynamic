0f4c636dac1ba88eac5d0ed89d693de26d3244bd
jmaglangit <jmaglangit@nerubia.com>
2016-08-09 10:21:03 +0800

    created ModuleTranslation model

---
+++ app/Models/Core/ModuleTranslation.php
@@ -0,0 +1,21 @@
+<?php namespace FutureEd\Models\Core;
+
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ModuleTranslation  extends Model {
+
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'module_translations';
+
+	protected $date = [
+		'created_at','updated_at','deleted_at'
+	];
+
+	protected $attribute = '';
+
+}
