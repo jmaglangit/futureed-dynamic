bca1763894d7536f0a5f83476ead26603dc0bb60
jmaglangit <jmaglangit@nerubia.com>
2016-08-16 18:05:57 +0800

    created index for localization

--- resources/views/admin/manage/localization/index.blade.php
+++ resources/views/admin/manage/localization/index.blade.php
@@ -1,7 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 8/15/16
- * Time: 3:11 PM
- */
+@extends('admin.app')
+
+@section('navbar')
+    @include('admin.partials.main-nav')
+@stop
+
+@section('content')
+    <div class="container dshbrd-con" ng-controller="ManageLocalizationController as localization"
+         ng-controller="ManageLocalizationController as localization" ng-cloak>
+
+        <div template-directive template-url="{!! route('admin.partials.base_url') !!}"></div>
+
+        <div class="wrapr" >
+            <div class="client-nav side-nav">
+                @include('admin.partials.dshbrd-side-nav')
+            </div>
+
+            <div class="client-content" template-directive template-url="{!! route('admin.manage.localization.translations') !!}"></div>
+
+        </div>
+    </div>
+@stop
+
+@section('scripts')
+    {!! Html::script('/js/admin/controllers/manage_localization_controller.js') !!}
+    {!! Html::script('/js/admin/services/manage_localization_service.js')!!}
+
+
+
+    {!! Html::script('/js/common/search_service.js') !!}
+    {!! Html::script('/js/common/table_service.js')!!}
+    {!! Html::script('/js/common/FileSaver.min.js') !!}
+@stop
