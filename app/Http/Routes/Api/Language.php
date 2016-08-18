a6645ba6aa476d2bde99c494ca46ea9f33a0c439
jmaglangit <jmaglangit@nerubia.com>
2016-08-18 16:34:59 +0800

    created files to initialization of language

---
+++ app/Http/Routes/Api/Language.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/18/16
+ * Time: 11:23 AM
+ */
---
+++ resources/views/admin/manage/localization/partials/main_settings.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/17/16
+ * Time: 4:54 PM
+ */
