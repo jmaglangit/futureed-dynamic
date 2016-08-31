080586d30d58321da67d435a1d3df52620291ee7
jmaglangit <jmaglangit@nerubia.com>
2016-08-31 17:43:44 +0800

    created translation models

---
+++ app/Models/Core/AnswerExplanationTranslation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/31/16
+ * Time: 5:20 PM
+ */
+
+namespace FutureEd\Models\Core;
+
+
+class AnswerExplanationTranslation {
+
+}
---
+++ app/Models/Core/QuestionAnswerTranslation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 8/31/16
+ * Time: 5:10 PM
+ */
+
+namespace FutureEd\Models\Core;
+
+
+class QuestionAnswerTranslation {
+
+}
