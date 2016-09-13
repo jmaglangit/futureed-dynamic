2f7169f9b1f19c03b1cbad244301b43c1da0fffd
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 15:25:00 +0800

    added translation model for quote

--- app/Models/Core/QuoteTranslation.php
+++ app/Models/Core/QuoteTranslation.php
@@ -8,7 +8,25 @@
 
 namespace FutureEd\Models\Core;
 
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class QuoteTranslation {
+class QuoteTranslation extends Model{
 
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'quote_translations';
+
+	protected $date = [
+		'created_at','updated_at','deleted_at'
+	];
+
+	protected $fillable = [
+		'quote_id',
+		'quote',
+		'locale',
+	];
 }
