fe92298d0796e4e9e45e60355fcb9f5e9a78bcbf
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 15:29:42 +0800

    created question translation controller

---
+++ app/Http/Controllers/Api/v1/QuestionTranslationController.php
@@ -0,0 +1,26 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class QuestionTranslationController extends Controller {
+
+	protected $question_translation;
+
+	public function __construct(){
+
+	}
+	//generate new language
+
+
+	//checkLanguageAvailability
+
+	//getLanguageTranslation
+
+	//getTranslatedAttributes
+
+	//googleTranslate
+
+}
