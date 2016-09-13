cba4f03a2754cf82c2d2e5dae5621226e7c46d28
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 15:27:54 +0800

    created repo for question translation

--- app/Models/Repository/QuestionTranslation/QuestionTranslationRepository.php
+++ app/Models/Repository/QuestionTranslation/QuestionTranslationRepository.php
@@ -9,6 +9,37 @@
 namespace FutureEd\Models\Repository\QuestionTranslation;
 
 
+use Illuminate\Support\Facades\DB;
+
 class QuestionTranslationRepository {
 
+	public function generateInitialLanguageTranslation($locale){
+
+		DB::beginTransaction();
+
+		try{
+
+
+		}catch (\Exception $e){
+			DB::rollback();
+
+			return false;
+		}
+
+		DB::commit();
+
+		return true;
+	}
+
+	public function checkLanguageAvailability($locale){}
+
+	public function updatedTranslation($data,$target_lang,$field){}
+
+	public function getModuleTranslations($locale){}
+
+	public function getTranslatedAttributes(){}
+
+	public function getQuestions($criteria,$limit,$offset){}
+
+	public function questionCount(){}
 }
