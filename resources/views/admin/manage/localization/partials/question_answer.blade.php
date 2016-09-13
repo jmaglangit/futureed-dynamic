f389f5f87ce339d57d435dbf35bf32e46a354728
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 14:04:15 +0800

    added question answer blade iew

--- resources/views/admin/manage/localization/partials/question_answer.blade.php
+++ resources/views/admin/manage/localization/partials/question_answer.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/5/16
- * Time: 5:51 PM
- */
+<div class="panel-group col-xs-12 search-container" id="accordion">
+    <div class="panel panel-default">
+        <div id="detail_heading" class="panel-heading" data-toggle="collapse" data-parent="#accordion"
+             href="#question_answer_translation" aria-expanded="true" aria-controls="question_answer_translation" close-others="true">
+            <h4 class="panel-title">
+                {!! trans('messages.answer') !!}
+
+                <span class="pull-right"><i class="fa fa-angle-double-down"></i></span>
+            </h4>
+        </div>
+        <div id="question_answer_translation" class="panel-collapse collapse">
+            <div class="panel-body">
+                {{--translation activities--}}
+            </div>
+
+        </div>
+    </div>
+
+</div>
