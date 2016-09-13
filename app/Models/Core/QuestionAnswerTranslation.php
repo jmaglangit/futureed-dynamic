d19af91ea309d49658e9d919338bb82921276cbe
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 15:24:11 +0800

    added translable model for question answer

--- app/Models/Core/QuestionAnswerTranslation.php
+++ app/Models/Core/QuestionAnswerTranslation.php
@@ -9,6 +9,26 @@
 namespace FutureEd\Models\Core;
 
 
-class QuestionAnswerTranslation {
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class QuestionAnswerTranslation extends Model{
+
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'question_answer_translations';
+
+	protected $date = [
+		'created_at','updated_at','deleted_at'
+	];
+
+	protected $fillable = [
+		'question_answer_id',
+		'answer_text',
+		'locale',
+	];
 
 }
