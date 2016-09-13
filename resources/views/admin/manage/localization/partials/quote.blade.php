b32e9b8d37b14b0672266101fa5cca1c6372d76d
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 14:04:47 +0800

    added quote view translation

--- resources/views/admin/manage/localization/partials/quote.blade.php
+++ resources/views/admin/manage/localization/partials/quote.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/6/16
- * Time: 3:08 PM
- */
+<div class="panel-group col-xs-12 search-container" id="accordion">
+    <div class="panel panel-default">
+        <div id="detail_heading" class="panel-heading" data-toggle="collapse" data-parent="#accordion"
+             href="#quote_translation" aria-expanded="true" aria-controls="quote_translation" close-others="true">
+            <h4 class="panel-title">
+                {!! trans_choice('messages.quote',1) !!}
+
+                <span class="pull-right"><i class="fa fa-angle-double-down"></i></span>
+            </h4>
+        </div>
+        <div id="quote_translation" class="panel-collapse collapse" >
+            <div class="panel-body">
+                {{--translation activities--}}
+            </div>
+
+        </div>
+    </div>
+
+</div>
