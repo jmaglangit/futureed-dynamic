7e2462ff0a64346a55c17f63bd208b9f091b94bf
jmaglangit <jmaglangit@nerubia.com>
2016-09-13 15:23:21 +0800

    created translation model for answer explanation

--- app/Models/Core/AnswerExplanationTranslation.php
+++ app/Models/Core/AnswerExplanationTranslation.php
@@ -9,6 +9,25 @@
 namespace FutureEd\Models\Core;
 
 
-class AnswerExplanationTranslation {
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+class AnswerExplanationTranslation extends Model{
+
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'answer_explanation_translations';
+
+	protected $date = [
+		'created_at','updated_at','deleted_at'
+	];
+
+	protected $fillable = [
+		'answer_explanation_id',
+		'answer_explanation',
+		'locale',
+	];
 }
