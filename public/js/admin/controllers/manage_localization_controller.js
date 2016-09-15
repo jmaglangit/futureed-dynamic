c42fece175cad214fa89d7fc541747d8c2a84afb
jmaglangit <jmaglangit@nerubia.com>
2016-09-15 20:13:39 +0800

    added functions and api call for localization

--- public/js/admin/controllers/manage_localization_controller.js
+++ public/js/admin/controllers/manage_localization_controller.js
@@ -18,9 +18,19 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
 
         self.active_translation = Constants.FALSE;
         self.active_setting = Constants.FALSE;
+        self.module_locale_code = Constants.NONE;
         self.locale_code = Constants.NONE;
         self.module_google_translate = Constants.FALSE;
         self.module_field = Constants.FALSE;
+        self.translate_tag = Constants.TRUE;
+        self.question_google_translate = Constants.FALSE;
+        self.question_field = Constants.FALSE;
+        self.question_answer_google_translate = Constants.FALSE;
+        self.question_answer_field = Constants.FALSE;
+        self.answer_explanation_google_translate = Constants.FALSE;
+        self.answer_explanation_field = Constants.FALSE;
+        self.quote_google_translate = Constants.FALSE;
+        self.quote_field = Constants.FALSE;
 
         switch(active) {
             case Constants.LOCALIZATION_TRANSLATION:
@@ -134,11 +144,11 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
     }
 
 
-    //get languages available
-    self.getLanguages = function(){
+    //get module languages available
+    self.getModuleLanguages = function(){
         self.errors = Constants.FALSE;
 
-        self.locale_code = Constants.NONE;
+        self.module_locale_code = Constants.NONE;
 
         ManageLocalizationService.getLanguages().success(function(response){
             if(angular.equals(response.status, Constants.STATUS_OK)) {
@@ -146,7 +156,7 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
                     self.success = Constants.FALSE;
                     self.errors = $scope.errorHandler(response.errors);
                 } else if(response.data) {
-                    self.languages = response.data;
+                    self.module_languages = response.data;
                 }
             }
         }).error(function (response) {
@@ -155,7 +165,7 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
         });
     }
 
-    //get all languages enabled by the
+    //get all languages enabled by the app
     self.getAllLanguages = function(){
         self.errors = Constants.FALSE;
 
@@ -201,10 +211,10 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
     }
 
     //download translation per language
-    self.downloadTranslation = function(){
+    self.moduleDownloadTranslation = function(){
         self.errors = Constants.FALSE;
 
-        ManageLocalizationService.downloadTranslation(self.locale_code).success(function(data,status,headers,config){
+        ManageLocalizationService.downloadTranslation(self.module_locale_code).success(function(data,status,headers,config){
             if (data.errors) {
                 self.success = Constants.FALSE;
                 self.errors = $scope.errorHandler(data.errors);
@@ -221,7 +231,7 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
     }
 
     //upload translation
-    self.uploadTranslation = function(file){
+    self.moduleUploadTranslation = function(file){
 
         self.uploaded = Constants.FALSE;
         self.base_url = $("#base_url_form input[name='base_url']").val();
@@ -229,7 +239,7 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
         if(file.length) {
             $scope.ui_block();
             Upload.upload({
-                url: '/api/v1/module-translation/upload?target_lang=' + self.locale_code
+                url: '/api/v1/module-translation/upload?target_lang=' + self.module_locale_code
                 , file: file[0]
             }).success(function(response) {
                 if(angular.equals(response.status, Constants.STATUS_OK)) {
@@ -250,4 +260,312 @@ function ManageLocalizationController($scope, ManageLocalizationService, apiServ
             });
         }
     }
+
+    //google translate
+    self.moduleGoogleTranslate = function(){
+
+        var data = {
+            target_lang : self.module_locale_code,
+            field   : self.module_field.field,
+            tagged  : self.translate_tag
+        };
+
+        $scope.ui_block();
+        ManageLocalizationService.googleTranslateField(data).success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                }else if(response.data){
+                    var data = response.data;
+                    self.errors = Constants.FALSE;
+                    self.success = data;
+                }
+            }
+            $scope.ui_unblock();
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    //set translate tag
+    self.moduleActiveTag = function(val){
+        self.translate_tag = val;
+    }
+
+    //Questions
+
+    //get question languages
+    self.getQuestionLanguages = function(){
+        self.errors = Constants.FALSE;
+
+        self.question_locale_code = Constants.NONE;
+
+        ManageLocalizationService.getQuestionLanguages().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.question_languages = response.data;
+                }
+            }
+        }).error(function (response) {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.getTranslatableQuestionField = function(){
+        ManageLocalizationService.getQuestionTranslatableFields().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.question_translated_field = response.data;
+                }
+            }
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.questionActiveTag = function(val){
+        self.translate_tag = val;
+    }
+
+    self.questionGoogleTranslate = function(){
+        var data = {
+            target_lang : self.question_locale_code,
+            field   : self.question_field.field,
+            tagged  : self.translate_tag
+        };
+
+        $scope.ui_block();
+        ManageLocalizationService.googleQuestionTranslateField(data).success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                }else if(response.data){
+                    var data = response.data;
+                    self.errors = Constants.FALSE;
+                    self.success = data;
+                }
+            }
+            $scope.ui_unblock();
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    //Question Answer
+
+    //get question answer languages
+    self.getQuestionAnswerLanguages = function(){
+        self.errors = Constants.FALSE;
+
+        self.question_answer_locale_code = Constants.NONE;
+
+        ManageLocalizationService.getQuestionAnswerLanguages().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.question_answer_languages = response.data;
+                }
+            }
+        }).error(function (response) {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.getTranslatableQuestionAnswerField = function(){
+        ManageLocalizationService.getQuestionAnswerTranslatableFields().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.question_answer_translated_field = response.data;
+                }
+            }
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.questionAnswerActiveTag = function(val){
+        self.translate_tag = val;
+    }
+
+    self.questionAnswerGoogleTranslate = function(){
+        var data = {
+            target_lang : self.question_answer_locale_code,
+            field   : self.question_answer_field.field,
+            tagged  : self.translate_tag
+        };
+
+        $scope.ui_block();
+        ManageLocalizationService.googleQuestionAnswerTranslateField(data).success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                }else if(response.data){
+                    var data = response.data;
+                    self.errors = Constants.FALSE;
+                    self.success = data;
+                }
+            }
+            $scope.ui_unblock();
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    //Answer Explanation
+
+    self.getAnswerExplanationLanguages = function(){
+        self.errors = Constants.FALSE;
+
+        self.answer_explanation_locale_code = Constants.NONE;
+
+        ManageLocalizationService.getAnswerExplanationLanguages().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.answer_explanation_languages = response.data;
+                }
+            }
+        }).error(function (response) {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.getTranslatableAnswerExplanationField = function(){
+        ManageLocalizationService.getAnswerExplanationTranslatableFields().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.answer_explanation_translated_field = response.data;
+                }
+            }
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.answerExplanationActiveTag = function(val){
+        self.translate_tag = val;
+    }
+
+    self.answerExplanationGoogleTranslate = function(){
+        var data = {
+            target_lang : self.answer_explanation_locale_code,
+            field   : self.answer_explanation_field.field,
+            tagged  : self.translate_tag
+        };
+
+        $scope.ui_block();
+        ManageLocalizationService.googleAnswerExplanationTranslateField(data).success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                }else if(response.data){
+                    var data = response.data;
+                    self.errors = Constants.FALSE;
+                    self.success = data;
+                }
+            }
+            $scope.ui_unblock();
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    //Quote
+
+    self.getQuoteLanguages = function(){
+        self.errors = Constants.FALSE;
+
+        self.quote_locale_code = Constants.NONE;
+
+        ManageLocalizationService.getQuoteLanguages().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.quote_languages = response.data;
+                }
+            }
+        }).error(function (response) {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.getTranslatableQuoteField = function(){
+        ManageLocalizationService.getQuoteTranslatableFields().success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                } else if(response.data) {
+                    self.quote_translated_field = response.data;
+                }
+            }
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
+    self.quoteActiveTag = function(val){
+        self.translate_tag = val;
+    }
+
+    self.quoteGoogleTranslate = function(){
+        var data = {
+            target_lang : self.quote_locale_code,
+            field   : self.quote_field.field,
+            tagged  : self.translate_tag
+        };
+
+        $scope.ui_block();
+        ManageLocalizationService.googleQuoteTranslateField(data).success(function(response){
+            if(angular.equals(response.status, Constants.STATUS_OK)) {
+                if(response.errors) {
+                    self.success = Constants.FALSE;
+                    self.errors = $scope.errorHandler(response.errors);
+                }else if(response.data){
+                    var data = response.data;
+                    self.errors = Constants.FALSE;
+                    self.success = data;
+                }
+            }
+            $scope.ui_unblock();
+        }).error(function () {
+            self.errors = $scope.internalError();
+            $scope.ui_unblock();
+        });
+    }
+
 }
--- public/js/admin/services/manage_localization_service.js
+++ public/js/admin/services/manage_localization_service.js
@@ -50,5 +50,123 @@ function ManageLocalizationService($http) {
         });
     }
 
+    //google translate field
+    api.googleTranslateField = function(data){
+        return $http({
+            method  :   Constants.METHOD_POST,
+            data    :   data,
+            url     :   apiUrl + 'module-translation/google-translate'
+        });
+    }
+
+    //Question
+
+    //get all languages defined by question translation
+    api.getQuestionLanguages = function(){
+        return $http({
+            method  :   Constants.METHOD_GET
+            , url   :   apiUrl + 'question-translation/languages'
+        });
+    }
+
+    //get question translatable fields
+    api.getQuestionTranslatableFields = function(){
+        return $http({
+            method  :   Constants.METHOD_GET,
+            url     :   apiUrl + 'question-translation/attributes'
+        });
+    }
+
+    // google translate question field
+    api.googleQuestionTranslateField = function(data){
+        return $http({
+            method  :   Constants.METHOD_POST,
+            data    :   data,
+            url     :   apiUrl + 'question-translation/google-translate'
+        });
+    }
+
+    //Question Answer
+
+    //get all languages defined by question answer translation
+    api.getQuestionAnswerLanguages = function(){
+        return $http({
+            method  :   Constants.METHOD_GET
+            , url   :   apiUrl + 'question-answer-translation/languages'
+        });
+    }
+
+    //get question answer translatable fields
+    api.getQuestionAnswerTranslatableFields = function(){
+        return $http({
+            method  :   Constants.METHOD_GET,
+            url     :   apiUrl + 'question-answer-translation/attributes'
+        });
+    }
+
+    // google translate question answer field
+    api.googleQuestionAnswerTranslateField = function(data){
+        return $http({
+            method  :   Constants.METHOD_POST,
+            data    :   data,
+            url     :   apiUrl + 'question-answer-translation/google-translate'
+        });
+    }
+
+    //Answer Explanation
+
+    //get all languages defined by answer explanation translation
+    api.getAnswerExplanationLanguages = function(){
+        return $http({
+            method  :   Constants.METHOD_GET
+            , url   :   apiUrl + 'answer-explanation-translation/languages'
+        });
+    }
+
+    //get answer explanation translatable fields
+    api.getAnswerExplanationTranslatableFields = function(){
+        return $http({
+            method  :   Constants.METHOD_GET,
+            url     :   apiUrl + 'answer-explanation-translation/attributes'
+        });
+    }
+
+    // google translate answer explanation field
+    api.googleAnswerExplanationTranslateField = function(data){
+        return $http({
+            method  :   Constants.METHOD_POST,
+            data    :   data,
+            url     :   apiUrl + 'answer-explanation-translation/google-translate'
+        });
+    }
+
+    //Quote
+
+    //get all languages defined by Quote translation
+    api.getQuoteLanguages = function(){
+        return $http({
+            method  :   Constants.METHOD_GET
+            , url   :   apiUrl + 'quote-translation/languages'
+        });
+    }
+
+    //get quote translatable fields
+    api.getQuoteTranslatableFields = function(){
+        return $http({
+            method  :   Constants.METHOD_GET,
+            url     :   apiUrl + 'quote-translation/attributes'
+        });
+    }
+
+    // google translate quote field
+    api.googleQuoteTranslateField = function(data){
+        return $http({
+            method  :   Constants.METHOD_POST,
+            data    :   data,
+            url     :   apiUrl + 'quote-translation/google-translate'
+        });
+    }
+
+
     return api;
 }
