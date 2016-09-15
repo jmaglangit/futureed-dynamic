c877e0dbefda41e4bd84925e74c20373876cb1aa
jmaglangit <jmaglangit@nerubia.com>
2016-09-15 20:36:48 +0800

    added routes involve in localization

--- app/Http/Routes/Api/AnswerExplanationTranslation.php
+++ app/Http/Routes/Api/AnswerExplanationTranslation.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/15/16
- * Time: 5:51 PM
- */
+Routes::group([
+//	'middleware' => ['api_user','api_after'],
+//	'permission' => ['admin'],
+//	'role' => ['admin','super admin']
+], function(){
+
+	Routes::group([
+		'prefix' => '/answer-explanation-translation',
+	], function (){
+
+		Routes::get('/generate-language/{locale}',[
+			'as' => 'answer-explanation-translate.generate-language',
+			'uses' => 'Api\v1\AnswerExplanationTranslationController@generateNewLanguage'
+		]);
+
+		Routes::get('/check-language/{locale}',[
+			'as' => 'answer-explanation-translate.check-language',
+			'uses' => 'Api\v1\AnswerExplanationTranslationController@checkLanguageAvailability'
+		]);
+
+		Routes::get('/languages',[
+			'as' => 'answer-explanation-translate.languages',
+			'uses' => 'Api\v1\AnswerExplanationTranslationController@getLanguageTranslation'
+		]);
+
+		Routes::get('/attributes',[
+			'as' => 'answer-explanation-translate.attributes',
+			'uses' => 'Api\v1\AnswerExplanationTranslationController@getTranslatedAttributes'
+		]);
+
+		Routes::post('/google-translate',[
+			'as' => 'answer-explanation-translate.google-translate',
+			'uses' => 'Api\v1\AnswerExplanationTranslationController@googleTranslate'
+		]);
+	});
+});
