6b9b4212b1e852f1c567cb9f06b315f674b65849
jmaglangit <jmaglangit@nerubia.com>
2016-09-19 17:15:43 +0800

    added route definition for jobs

--- app/Http/Routes/Api/job.php
+++ app/Http/Routes/Api/job.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/19/16
- * Time: 2:18 PM
- */
+Routes::group([
+	'prefix' => '/job',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin'],
+	'role' => ['admin','super admin']
+], function(){
+
+	Routes::get('/queue',[
+		'as' => 'api.v1.job.queue',
+		'uses' => 'Api\v1\JobController@getQueueList'
+	]);
+});
