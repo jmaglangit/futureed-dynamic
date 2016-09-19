aff3f7f78c9ef9a6ab2356d69479fd6f93170390
jmaglangit <jmaglangit@nerubia.com>
2016-09-19 17:13:50 +0800

    implemented interface function for job repository

--- app/Models/Repository/Job/JobRepository.php
+++ app/Models/Repository/Job/JobRepository.php
@@ -9,6 +9,16 @@
 namespace FutureEd\Models\Repository\Job;
 
 
-class JobRepository {
+use FutureEd\Models\Core\Job;
 
+class JobRepository implements JobRepositoryInterface {
+
+	/**
+	 * Get all queue list.
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+	 */
+	public function getQueueList(){
+
+		return Job::all();
+	}
 }
