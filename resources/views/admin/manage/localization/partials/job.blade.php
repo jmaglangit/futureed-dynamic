fdc60586d6764a90dc24cd5eb853b17f45a9b6f7
jmaglangit <jmaglangit@nerubia.com>
2016-09-19 17:07:52 +0800

    created job view for job lists

--- resources/views/admin/manage/localization/partials/job.blade.php
+++ resources/views/admin/manage/localization/partials/job.blade.php
@@ -1,16 +1,16 @@
 <div>
-    <div class="col-xs-12 search-container">
-        <div class="title-mid">{!! 'Job on Queue' !!}</div>
+    <div class="col-xs-12 search-container" ng-init="localization.jobList()">
+        <div class="title-mid">{!! trans('messages.job_on_queue') !!}</div>
         <div class="form-group">
             <div>
-                <div>refresh button here...</div>
-                <div class="panel panel-default">
+                <div class="btn btn-blue btn-small pull-right" ng-click="localization.jobList()"><i class="fa fa-refresh" aria-hidden="true"></i> Refresh Queue</div>
+                <div class="panel panel-default col-xs-12">
                     <div class="panel-body">
-                        Job list here...
+                        <p ng-if="localization.job_list.length > futureed.TRUE" ng-repeat="job in localization.job_list">{!  job.message !}</p>
+                        <p ng-if="localization.job_list.length == futureed.FALSE">{!! trans('messages.job_on_queue_none') !!}</p>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </div>
