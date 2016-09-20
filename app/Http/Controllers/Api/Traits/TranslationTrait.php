af32f1f90abb1b122584c2eb00eaf4503c6c11dc
jmaglangit <jmaglangit@nerubia.com>
2016-09-20 17:38:01 +0800

    updated with google translate function

--- app/Http/Controllers/Api/Traits/TranslationTrait.php
+++ app/Http/Controllers/Api/Traits/TranslationTrait.php
@@ -1,9 +1,13 @@
 <?php namespace FutureEd\Http\Controllers\Api\Traits;
 
+use FutureEd\Http\Requests\Api\TranslatableRequest;
+use Illuminate\Support\Facades\Artisan;
 
 trait TranslationTrait {
 
 	protected $model;
+
+	protected $translatable_model;
 
 	/**
 	 * Check if language exist.
@@ -71,5 +75,31 @@ trait TranslationTrait {
 		}
 
 		return $this->respondWithData($data);
+	}
+
+	/**
+	 * googleTranslate
+	 * @param TranslatableRequest $request
+	 * @return
+	 */
+	public function googleTranslate(TranslatableRequest $request){
+
+		//check if input has translate only flagged or all.
+		$input = $request->only(
+			'target_lang',
+			'field',
+			'tagged'
+		);
+
+		//Queue translation
+		// -m question --language {lang} -f {field} --tagged {tagged|boolean}
+		Artisan::queue('fl:google-translate',[
+			'--model' => $this->translatable_model,
+			'--language' => $input['target_lang'],
+			'--field' => $input['field'],
+			'--tagged' => $input['tagged']
+		]);
+
+		return $this->respondWithData(trans('messages.queue_trans_google'));
 	}
 }
