fe60873c1d1dd07b927b5b9abc2705281adced0d
jmaglangit <jmaglangit@nerubia.com>
2016-10-03 17:52:15 +0800

    added function to get all records

--- app/Models/Repository/ModuleCountry/ModuleCountryRepository.php
+++ app/Models/Repository/ModuleCountry/ModuleCountryRepository.php
@@ -6,9 +6,41 @@
  * Time: 3:17 PM
  */
 
-namespace FutureEd\Models\ModuleCountry;
+namespace FutureEd\Models\Repository\ModuleCountry;
 
+use FutureEd\Models\Core\ModuleCountry;
 
-class ModuleCountryRepository {
+class ModuleCountryRepository implements ModuleCountryRepositoryInterface {
+
+	public function getModuleCountries($criteria,$limit,$offset){
+
+		$module_country = new ModuleCountry();
+
+		//get by criteria
+		if(isset($criteria['module_id'])){
+			$module_country = $module_country->whereModuleId($criteria['module_id']);
+		}
+
+		if(isset($criteria['country_id'])){
+			$module_country = $module_country->whereCountryId($criteria['country_id']);
+		}
+
+		if(isset($criteria['grade_id'])){
+			$module_country = $module_country->whereGradeId($criteria['grade_id']);
+		}
+
+		$count = $module_country->count();
+
+		if ($limit > 0 && $offset >= 0) {
+			$module_country = $module_country->offset($offset)->limit($limit);
+		}
+
+		$response = [
+			'total' => $count,
+			'records' => $module_country->with('grade','country')->get()->toArray()
+		];
+
+		return $response;
+	}
 
 }
