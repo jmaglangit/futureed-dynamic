be0e45e6f0b8f4599e6e1edb67e99a8ad2712bed
jmaglangit <jmaglangit@nerubia.com>
2016-10-03 17:55:31 +0800

    created controller

---
+++ app/Http/Controllers/Api/v1/ModuleCountryController.php
@@ -0,0 +1,44 @@
+<?php namespace FutureEd\Http\Controllers\Api\v1;
+
+use FutureEd\Http\Requests;
+use FutureEd\Models\Repository\ModuleCountry\ModuleCountryRepositoryInterface;
+use Illuminate\Support\Facades\Input;
+
+class ModuleCountryController extends ApiController {
+
+	protected $module_country;
+
+	public function __construct(
+		ModuleCountryRepositoryInterface $moduleCountryRepositoryInterface
+	){
+		$this->module_country = $moduleCountryRepositoryInterface;
+	}
+
+	/**
+	 * Get module country collection.
+	 */
+	public function index()
+	{
+		$category = [];
+
+		if(Input::get('module_id')){
+			$category['module_id'] = Input::get('module_id');
+		}
+
+		if(Input::get('grade_id')){
+			$category['grade_id'] = Input::get('grade_id');
+		}
+
+		if(Input::get('country_id')){
+			$category['country_id'] = Input::get('country_id');
+		}
+
+		$limit = (Input::get('limit')) ? Input::get('limit') : 0;
+
+		$offset = (Input::get('offset')) ? Input::get('offset') : 0;
+
+		return $this->respondWithData($this->module_country->getModuleCountries($category,$limit,$offset));
+	}
+
+
+}
