83f3a27eca4d1d96e450448d27e3b57b40a334c6
jmaglangit <jmaglangit@nerubia.com>
2016-10-03 17:48:00 +0800

    added relationships

--- app/Models/Core/ModuleCountry.php
+++ app/Models/Core/ModuleCountry.php
@@ -1,14 +1,45 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 9/29/16
- * Time: 5:32 PM
- */
+<?php namespace FutureEd\Models\Core;
 
-namespace FutureEd\Models\Core;
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+class ModuleCountry extends Model{
 
-class ModuleCountry {
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'module_countries';
+
+	protected $dates = ['created_at','updated_at','deleted_at'];
+
+	protected $fillable = [
+		'module_id',
+		'country_id',
+		'grade_id'
+	];
+
+	protected $hidden = [
+		'created_by',
+		'updated_by',
+		'created_at',
+		'updated_at',
+		'deleted_at'
+	];
+
+	//relationships
+
+	public function grade(){
+		return $this->belongsTo('FutureEd\Models\Core\Grade');
+	}
+
+	public function module(){
+		return $this->belongsTo('FutureEd\Models\Core\Module');
+	}
+
+	public function country(){
+		return $this->belongsTo('Webpatser\Countries\Countries');
+	}
 
 }
