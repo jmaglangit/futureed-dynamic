c458e8ad1c0351297e9381621b2e5c4a01ecff86
jmaglangit <jmaglangit@nerubia.com>
2016-10-03 17:44:13 +0800

    created routes for module country

--- app/Http/Routes/Api/ModuleCountry.php
+++ app/Http/Routes/Api/ModuleCountry.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jason
- * Date: 10/3/16
- * Time: 4:16 PM
- */
+
+Routes::group([
+	'prefix' => '/module-country',
+	'middleware' => ['api_user','api_after'],
+	'permission' => ['admin','client','student'],
+	'role' => ['principal','teacher','parent','admin','super admin']
+],function(){
+
+	Routes::get('/',[
+		'uses' => 'Api\v1\ModuleCountryController@index',
+		'as' => 'api.v1.module-country.index'
+	]);
+});
