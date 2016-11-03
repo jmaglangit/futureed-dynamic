851f15d0948eb36568d868a685adfd57e317e00b
jmaglangit <jmaglangit@nerubia.com>
2016-11-03 15:49:06 +0800

    added progress view graph for subject area

--- resources/views/student/reports/partials/charts/platform-chart-subject-area.blade.php
+++ resources/views/student/reports/partials/charts/platform-chart-subject-area.blade.php
@@ -1,25 +1,30 @@
-<div>
+<div class="thumbnail">
+    <div><a href="javascript:void(0)" ng-click="reports.setActive(futureed.SUBJECT_AREA)">
+            {!! trans('messages.graph_link_message') !!}
+        </a></div>
     {{--drop down here--}}
-    <label class="col-xs-2 question-analysis-filter">{!! trans('messages.subject') !!}</label>
-    <div class="col-xs-2">
-        <select ng-model="reports.search.subject_id"
-                ng-change="reports.getModuleList()"
-                ng-disabled="!reports.subjects.length"
-                class="form-control">
-            <option value="">{!! trans('messages.select_subject') !!}</option>
-            <option ng-selected="reports.search.subject_id == subject.id"
-                    ng-repeat="subject in reports.subjects" ng-value="subject.id">{! subject.name !}
-            </option>
-        </select>
-    </div>
-    <label class="col-xs-2 question-analysis-filter">{!! trans('messages.school_level') !!}</label>
-    <div class="col-xs-2">
-        <select ng-model="reports.search.grade_id"
-                ng-change="reports.getModuleList()"
-                class="form-control">
-            <option value="">{!! trans('messages.select_level') !!}</option>
-            <option ng-repeat="grade in grades" ng-value="grade.id">{! grade.name !}</option>
-        </select>
+    <div>
+        <label class="col-xs-2">{!! trans('messages.subject') !!}</label>
+        <div class="col-xs-4">
+            <select ng-model="reports.subject_area_subject_id"
+                    ng-change="reports.getStudentChartSubjectArea(reports.subject_area_subject_id,reports.subject_area_grade_id)"
+                    ng-disabled="!reports.subjects.length"
+                    class="form-control">
+                <option value="">{!! trans('messages.select_subject') !!}</option>
+                <option ng-selected="reports.search.subject_id == subject.id"
+                        ng-repeat="subject in reports.subjects" ng-value="subject.id">{! subject.name !}
+                </option>
+            </select>
+        </div>
+        <label class="col-xs-2">{!! trans('messages.school_level') !!}</label>
+        <div class="col-xs-4">
+            <select ng-model="reports.subject_area_grade_id"
+                    ng-change="reports.getStudentChartSubjectArea(reports.subject_area_subject_id,reports.subject_area_grade_id)"
+                    class="form-control">
+                <option value="">{!! trans('messages.select_level') !!}</option>
+                <option ng-repeat="grade in grades" ng-value="grade.id">{! grade.name !}</option>
+            </select>
+        </div>
     </div>
     <svg class="chart-subject-area" width="450" height="300" ng-init="reports.getStudentChartSubjectArea()"></svg>
 </div>
