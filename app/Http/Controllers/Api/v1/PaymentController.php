75ac2d9068dc8bf0ab8fcbddb881e781335848fd
Mark Joseph Cabuquit <mcabuquit@nerubia.com>
2016-11-17 17:50:15 +0800

    updated paypal payment when retrieving

--- app/Http/Controllers/Api/v1/PaymentController.php
+++ app/Http/Controllers/Api/v1/PaymentController.php
@@ -198,7 +198,7 @@ class PaymentController extends ApiController
                                 $data['school_name'] = $client['school']['name'];
                                 $data['login_link'] = URL::to('/client/login');
 
-                                if(!$is_renewal && $res['client']->client_role == config('futureed.teacher')){
+                                if(!$is_renewal && $res['client']['client_role'] == config('futureed.teacher')){
                                     $this->email->sendTeacherAddClass($data);
                                 }
                             }
