866ab3491afae62f1a35cb9651f67e75e16a55bc
Mark Joseph Cabuquit <mcabuquit@nerubia.com>
2016-12-08 14:59:59 +0800

    update validation

--- public/js/client/controllers/manage_principal_payment_controller.js
+++ public/js/client/controllers/manage_principal_payment_controller.js
@@ -1449,11 +1449,15 @@ function ManagePrincipalPaymentController(
 	self.validateNumber =  function(){
 		if(self.new_classroom.seats != Constants.NULL) {
 			if(self.new_classroom.seats.length <= 5){
-				var temp = (self.new_classroom.seats.toString()).split(".");
+				var temp = (self.new_classroom.seats.toString()).split('.');
 				if(temp.length > 1){
-					self.new_classroom.seats = parseInt(temp[0].replace("-",""));
+					self.new_classroom.seats = parseInt(temp[0].replace('-',''));
 				} else {
-					self.new_classroom.seats = parseInt(self.new_classroom.seats.toString().replace(".",""));
+					if(self.new_classroom.seats == '0'){
+						self.new_classroom.seats = Constants.EMPTY_STR;
+					} else {
+						self.new_classroom.seats = self.new_classroom.seats.toString().replace('.','');
+					}
 				}
 				self.temp_number = self.new_classroom.seats;
 			} else {
