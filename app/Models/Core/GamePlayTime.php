eb6ad0424cb004ff36726c100ffe6bc0df83590f
jmaglangit <jmaglangit@nerubia.com>
2016-12-19 15:02:57 +0800

    added migration for game time

---
+++ app/Models/Core/GamePlayTime.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 12/19/16
+ * Time: 11:16 AM
+ */
+
+namespace FutureEd\Models\Core;
+
+
+class GamePlayTime {
+
+}
---
+++ app/Models/Repository/GamePlayTime/GamePlayTimeRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 12/19/16
+ * Time: 11:29 AM
+ */
+
+namespace FutureEd\Models\Repository\GamePlayTime;
+
+
+interface GamePlayTimeInterface {
+
+}
