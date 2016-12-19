b0c628c34d9db95d268a18860ef23ed52166182d
jmaglangit <jmaglangit@nerubia.com>
2016-12-19 15:11:14 +0800

    added model for game time

--- app/Models/Core/GamePlayTime.php
+++ app/Models/Core/GamePlayTime.php
@@ -9,6 +9,30 @@
 namespace FutureEd\Models\Core;
 
 
-class GamePlayTime {
+use FutureEd\Models\Traits\TransactionTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GamePlayTime extends Model {
+
+	use SoftDeletes;
+
+	use TransactionTrait;
+
+	protected $table = 'game_play_times';
+
+	protected $fillable = [
+		'game_id',
+		'student_id',
+		'countdown_time_played',
+		'date_played'
+	];
+
+	protected $hidden = ['created_by', 'updated_by', 'updated_at','created_at', 'deleted_at'];
+
+	protected $attributes = [
+		'created_by' => 1,
+		'updated_by' => 1,
+	];
 
 }
