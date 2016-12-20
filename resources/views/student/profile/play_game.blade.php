f9f64643c00d6cedc016ebfef4fb1b9c456b4694
jmaglangit <jmaglangit@nerubia.com>
2016-12-20 18:11:39 +0800

    added function on playing game

--- resources/views/student/profile/play_game.blade.php
+++ resources/views/student/profile/play_game.blade.php
@@ -1,6 +1,9 @@
 <div ng-if="profile.active_play_game">
     <div class="col-md-12">
         <div class="form-class">
+            {{-- TODO: pop-up modal--}}
+            {{-- TODO: display timer--}}
+            <div class="pull-right h3">Game Time Left : <span id="countdown"> 00:00 </span></div>
             <iframe class="play-game-box" src="{! profile.selected_game.game_url !}" ></iframe>
         </div>
     </div>
