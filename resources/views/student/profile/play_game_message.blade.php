87a497fee53e17120e375597a7e8a573c4554a40
jmaglangit <jmaglangit@nerubia.com>
2016-12-21 22:26:21 +0800

    added blade or game messages

--- resources/views/student/profile/play_game_message.blade.php
+++ resources/views/student/profile/play_game_message.blade.php
@@ -4,19 +4,28 @@
         <div class="modal-content">
 
             <div class="modal-body message-container modal-reward">
-                <div class="h1"><span class="col-xs-12">{!! trans_choice('messages.game',1) !!}</span></div>
+                <div class="h1" ng-if="!time_up"><span class="col-xs-12">{!! trans_choice('messages.game',1) !!}</span></div>
+                <div class="h1" ng-if="time_up">
+                    <span class="col-xs-12">{!! trans_choice('messages.game_lock',1) !!}</span></div>
 
-                <div class="row">
+                <div ng-if="!profile.errors" class="row">
                     <div class="h4">{!! trans('messages.game_time_message',
                     ['BUTTON' => '<b>' . strtoupper(trans('messages.play')) . '</b>', 'TIME' => config('futureed.game_time')]) !!} </div>
                     <div class="h3">{!! trans('messages.game_proceed') !!}</div>
                 </div>
+                <div ng-if="profile.errors" class="row">
+                    <div ng-repeat="error in profile.errors" class="h3">
+                        {! error !}
+                    </div>
+                </div>
+
             </div>
 
             <div class="modal-footer modal-reward">
                 {{--TODO: on Play, set timer and inform api --}}
-                <div class="btn btn-maroon btn-medium" data-dismiss="modal" aria-label="Play"
-                     ng-click="">{!! trans('messages.play') !!}</div>
+                <div class="btn btn-maroon btn-medium" aria-label="Play"
+                     ng-if="!profile.errors"
+                     ng-click="profile.getGameTime()">{!! trans('messages.play') !!}</div>
                 <div class="btn btn-maroon btn-medium" data-dismiss="modal" aria-label="Back"
                      ng-click="profile.setStudentProfileActive(futureed.GAMES)">{!! trans('messages.back') !!}</div>
             </div>
