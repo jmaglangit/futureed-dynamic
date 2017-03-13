5ec595bb75dfd995acaa504461e1aea495ec66e5
jmaglangit <jmaglangit@nerubia.com>
2017-03-13 16:14:07 +0800

    added migration tables

---
+++ app/Models/Observers/ModuleObserver.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/3/17
+ * Time: 10:54 AM
+ */
+
+namespace FutureEd\Models\Observers;
+
+
+class ModuleObserver {
+
+}
---
+++ resources/views/admin/manage/module/partials/dynamic_setup.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 3/3/17
+ * Time: 2:50 PM
+ */
