5b7febd25f0d22cc73c44e235462c974f1e100d9
Cris <Cris>
2017-04-25 18:25:14 +0800

    additional logic for diff error message

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -45,7 +45,7 @@ return [
 		2016 => 'Confirmation code should be number only.',
 		2017 => 'Picture Password is required.',
 		2018 => 'Username does not exist.',
-		2019 => 'Your Account has been locked. Please click forgot password to reset your password.',
+		2019 => ['Your Account has been locked.', 'Please click forgot password to reset your password.'],
 		2020 => 'Account is deleted.',
 		2021 => 'Username is invalid.',
 		2022 => 'The school level field is required.',
