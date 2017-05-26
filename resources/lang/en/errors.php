48b6568da6c19b8f20c9e4aee54527f2d7e5a22c
Cris <Cris>
2017-05-26 09:42:46 +0800

    fix conflict on error messages

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -102,9 +102,8 @@ return [
 
 		2072 => 'Picture password is incorrect. You have :remaining_attempts remaining attempt(s).',
 		2073 => 'Please click forgot password to reset your password.',
-		2074 => 'Username/Email or password is invalid. You have :remaining_attempts remaining attempt(s).',
-
-        2074 => 'Unable to add Student. Country curriculum is not the same.',
+		2074 => 'Unable to add Student. Country curriculum is not the same.',
+		2075 => 'Username/Email or password is invalid. You have :remaining_attempts remaining attempt(s).',
 
 		2100 => 'Reset code is invalid.',
 		2101 => 'Password image is invalid.',
