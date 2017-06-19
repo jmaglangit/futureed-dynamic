21ecc1196f88e6f6bea6744b5bd6843091c0eeae
jmaglangit <jmaglangit@nerubia.com>
2017-06-19 15:16:47 +0800

    fixed alignment on text fields

--- resources/views/admin/manage/question_template/partials/add_question_template.blade.php
+++ resources/views/admin/manage/question_template/partials/add_question_template.blade.php
@@ -21,7 +21,7 @@
 		{!! Form::open(array('class' => 'form-horizontal')) !!}
 		<fieldset>
 			<div class="form-group">
-				<label class="control-label col-xs-2">{!! trans('messages.admin_question_type') !!} <span class="required">*</span></label>
+				<label class="control-label col-xs-3">{!! trans('messages.admin_question_type') !!} <span class="required">*</span></label>
 				<div class="col-xs-4">
 					{!! Form::select('search_question_type'
 						, array(
@@ -42,7 +42,7 @@
 			</div>
 
 			<div class="form-group">
-				<label class="control-label col-xs-2">{!! trans('messages.admin_operation') !!} <span class="required">*</span></label>
+				<label class="control-label col-xs-3">{!! trans('messages.admin_operation') !!} <span class="required">*</span></label>
 				<div class="col-xs-4">
 					{!! Form::select('search_operation'
 						, array(
@@ -66,7 +66,7 @@
 			</div>
 
 			<div class="form-group">
-				<label class="control-label col-xs-2">{!! trans('messages.admin_question_form') !!} <span class="required">*</span></label>
+				<label class="control-label col-xs-3">{!! trans('messages.admin_question_form') !!} <span class="required">*</span></label>
 				<div class="col-xs-4">
 					{!! Form::select('search_question_form'
 						, array(
@@ -94,7 +94,7 @@
 
 			<div class="form-group">				
 				<div class="col-xs-8">
-					<div class="col-xs-2 admin-search-module">
+					<div class="col-xs-3 admin-search-module">
 						{!! Form::button(trans('messages.admin_template_text_num')
 							,array(
 								'class' => 'btn btn-blue'
@@ -102,15 +102,15 @@
 							)
 						)!!}
 					</div>
-					<div class="col-xs-2 admin-search-module">
-						{!! Form::button(trans('messages.admin_template_text_object')
+					<div class="col-xs-3 admin-search-module">
+						{!! Form::button('object'
 							,array(
 								'class' => 'btn btn-blue'
 								, 'ng-click' => 'template.actionButtons(futureed.OBJECT)'
 							)
 						)!!}
 					</div>
-					<div class="col-xs-2 admin-search-module">
+					<div class="col-xs-3 admin-search-module">
 						{!! Form::button(trans('messages.admin_template_text_name')
 							,array(
 								'class' => 'btn btn-blue'
@@ -118,7 +118,7 @@
 							)
 						)!!}
 					</div>
-					<div class="col-xs-2 admin-search-module" ng-if="template.record.operation == futureed.ADDITION">
+					<div class="col-xs-3 admin-search-module" ng-if="template.record.operation == futureed.ADDITION">
 						{!! Form::button(trans('messages.addition_symbol')
 							,array(
 								'class' => 'btn btn-blue'
@@ -126,7 +126,7 @@
 							)
 						)!!}
 					</div>
-					<div class="col-xs-2 admin-search-module" ng-if="template.record.operation == futureed.SUBTRACTION">
+					<div class="col-xs-3 admin-search-module" ng-if="template.record.operation == futureed.SUBTRACTION">
 						{!! Form::button(trans('messages.subtraction_symbol')
 							,array(
 								'class' => 'btn btn-blue'
@@ -134,7 +134,7 @@
 							)
 						)!!}
 					</div>
-					<div class="col-xs-2 admin-search-module" ng-if="template.record.operation == futureed.MULTIPLICATION">
+					<div class="col-xs-3 admin-search-module" ng-if="template.record.operation == futureed.MULTIPLICATION">
 						{!! Form::button(trans('messages.multiply_symbol')
 							,array(
 								'class' => 'btn btn-blue'
