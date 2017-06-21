73e5f39a920559e42227749eb2c39362c0aa5a66
jmaglangit <jmaglangit@nerubia.com>
2017-06-21 11:03:20 +0800

    fix message on adding question template

--- public/js/admin/controllers/manage_question_temp_controller.js
+++ public/js/admin/controllers/manage_question_temp_controller.js
@@ -170,7 +170,7 @@ function ManageQuestionTempController($scope, ManageQuestionTempService, TableSe
 					});
 				} else if(response.data) {
 					self.setActive();
-					self.success = Constants.MSG_CREATED("Module");
+					self.success = Constants.MSG_CREATED("Question Template");
 				}
 			}
 			
