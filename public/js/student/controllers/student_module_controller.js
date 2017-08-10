67df5d5cca182290c29fe3b0cb71e144737dd492
jmaglangit <jmaglangit@nerubia.com>
2017-08-10 15:32:13 +0800

    remove comment logs

--- public/js/student/controllers/student_module_controller.js
+++ public/js/student/controllers/student_module_controller.js
@@ -1855,14 +1855,12 @@ function StudentModuleController($scope, $window, $interval, $filter, apiService
 				break;
 			case Constants.FRACTION_ADDITION_WHOLE:
 
-				console.log('new whole');
 				self.date_start = new Date();
 
 				setRandomDigits(self.question_grade_condition.max_number.toString().length);
 				randomDigitsOnclick();
 
 				question_text = question_text.replace("{fraction_addition_whole_number}","");
-
 
                 self.current_question.questions_text = question_text;
                 btncalculateOnclick();
