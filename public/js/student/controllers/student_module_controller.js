7efb3037696614733aa1494e0e280dd83445e2e5
Paul_Dongallo <pdongallo@nerubia.com>
2017-08-22 17:44:47 +0800

    removed console.log

--- public/js/student/controllers/student_module_controller.js
+++ public/js/student/controllers/student_module_controller.js
@@ -1725,7 +1725,7 @@ function StudentModuleController($scope, $window, $interval, $filter, apiService
 	}
 
 	self.dynamicQuestionSetup = function(question){
-		console.log("sort teststs small");
+
 		var question_text = question.question_template.question_template_format;
 		var module_map = module_mapper[self.current_question.module_id];
 
@@ -1892,7 +1892,7 @@ function StudentModuleController($scope, $window, $interval, $filter, apiService
 				break;
 
 			case Constants.INTEGER_SORT_SMALL:
-				console.log("sort small page");
+
 				self.date_start = new Date();
 
 				setRandomDigits(self.question_grade_condition.max_number.toString().length);
